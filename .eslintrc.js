248cee68243d7b4cd2e5ab725f1f83c79cdddc2f
Jacob Henry <jchenry@mitre.org>
2019-06-24 13:59:21 -0400

    Added eslint rules so it would compile in heimdall-lite

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  globals: {
+    "$": true,
+    "jQuery": true
+  },
+  parserOptions: {
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
+  },
+  rules: {
+    'no-console': 0,
+  },
+  plugins: ['vue'],
+};
