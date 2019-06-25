eb3b90b89a4525f7ed2e7caf067bb18a89647aa4
Jacob Henry <jchenry@mitre.org>
2019-06-25 14:11:01 -0400

    Synchronized method name change in test

--- src/store.test.ts
+++ src/store.test.ts
@@ -5,7 +5,7 @@ it ('Should parse JSON examples for flat profiles', () => {
     let json = fs.readFileSync("tst/examples/profile/profile.json","utf-8");
     let state = new HeimdallState;
     state.parseFile(json, "profile.json");
-    expect(state.getStatus()).toEqual([
+    expect(state.getStatusCount()).toEqual([
         ['Passed',         17],
         ['Failed',         28],
         ['Not Applicable', 2],
