cab1ee8cf200d93a6ae138f76630d666c44eb5e8
Jacob Henry <jchenry@mitre.org>
2019-07-25 18:13:43 -0400

    Added initial versions of generated file parsers

---
+++ src/generated-parsers/exec-json.ts
@@ -0,0 +1,377 @@
+// To parse this data:
+//
+//   import { Convert, ExecJSON } from "./file";
+//
+//   const execJSON = Convert.toExecJSON(json);
+//
+// These functions will throw an error if the JSON doesn't
+// match the expected interface, even if the JSON is valid.
+
+export interface ExecJSON {
+    platform:   Platform;
+    profiles:   ExecJSONProfile[];
+    statistics: Statistics;
+    version:    string;
+}
+
+export interface Platform {
+    /**
+     * The name of the platform this was run on.
+     */
+    name: string;
+    /**
+     * The version of the platform this was run on.
+     */
+    release: string;
+    /**
+     * TODO: Document this property
+     */
+    target_id?: string;
+}
+
+export interface ExecJSONProfile {
+    attributes:       { [key: string]: any }[];
+    controls:         ExecJSONControl[];
+    copyright?:       string;
+    copyright_email?: string;
+    groups:           ControlGroup[];
+    license?:         string;
+    maintainer?:      string;
+    name:             string;
+    sha256:           string;
+    skip_message?:    string;
+    status:           string;
+    summary?:         string;
+    supports:         SupportedPlatform[];
+    title?:           string;
+    version?:         string;
+}
+
+export interface ExecJSONControl {
+    code:            string;
+    desc:            null | string;
+    descriptions:    ControlDescription[];
+    id:              string;
+    impact:          number;
+    refs:            Reference[];
+    results:         ControlResult[];
+    source_location: SourceLocation;
+    tags:            { [key: string]: any };
+    title:           null | string;
+}
+
+export interface ControlDescription {
+    data:  string;
+    label: string;
+}
+
+export interface Reference {
+    ref?: string;
+    url?: string;
+    uri?: string;
+}
+
+export interface ControlResult {
+    backtrace?:    { [key: string]: any };
+    code_desc:     string;
+    exception?:    string;
+    message?:      string;
+    resource?:     string;
+    run_time:      number;
+    skip_message?: string;
+    start_time:    string;
+    status:        string;
+}
+
+export interface SourceLocation {
+    /**
+     * The line at which this statement is located in the file
+     */
+    line: number;
+    /**
+     * Path to the file that this statement originates from
+     */
+    ref: string;
+}
+
+export interface ControlGroup {
+    /**
+     * The control IDs in this group
+     */
+    controls: string[];
+    /**
+     * The unique identifier of the group
+     */
+    id: string;
+    /**
+     * The name of the group
+     */
+    title?: string;
+}
+
+export interface SupportedPlatform {
+    "os-family"?:      string;
+    "os-name"?:        string;
+    platform?:         string;
+    "platform-family": string;
+    "platform-name"?:  string;
+}
+
+export interface Statistics {
+    /**
+     * Breakdowns of control statistics by result
+     */
+    controls?: StatisticHash;
+    /**
+     * How long (in seconds) this inspec exec ran for.
+     */
+    duration: number;
+}
+
+/**
+ * Breakdowns of control statistics by result
+ */
+export interface StatisticHash {
+    failed?:  StatisticBlock;
+    passed?:  StatisticBlock;
+    skipped?: StatisticBlock;
+}
+
+export interface StatisticBlock {
+    /**
+     * Total number of controls (in this category) for this inspec execution.
+     */
+    total: number;
+}
+
+// Converts JSON strings to/from your types
+// and asserts the results of JSON.parse at runtime
+export class Convert {
+    public static toExecJSON(json: string): ExecJSON {
+        return cast(JSON.parse(json), r("ExecJSON"));
+    }
+
+    public static execJSONToJson(value: ExecJSON): string {
+        return JSON.stringify(uncast(value, r("ExecJSON")), null, 2);
+    }
+}
+
+function invalidValue(typ: any, val: any): never {
+    throw Error(`Invalid value ${JSON.stringify(val)} for type ${JSON.stringify(typ)}`);
+}
+
+function jsonToJSProps(typ: any): any {
+    if (typ.jsonToJS === undefined) {
+        var map: any = {};
+        typ.props.forEach((p: any) => map[p.json] = { key: p.js, typ: p.typ });
+        typ.jsonToJS = map;
+    }
+    return typ.jsonToJS;
+}
+
+function jsToJSONProps(typ: any): any {
+    if (typ.jsToJSON === undefined) {
+        var map: any = {};
+        typ.props.forEach((p: any) => map[p.js] = { key: p.json, typ: p.typ });
+        typ.jsToJSON = map;
+    }
+    return typ.jsToJSON;
+}
+
+function transform(val: any, typ: any, getProps: any): any {
+    function transformPrimitive(typ: string, val: any): any {
+        if (typeof typ === typeof val) return val;
+        return invalidValue(typ, val);
+    }
+
+    function transformUnion(typs: any[], val: any): any {
+        // val must validate against one typ in typs
+        var l = typs.length;
+        for (var i = 0; i < l; i++) {
+            var typ = typs[i];
+            try {
+                return transform(val, typ, getProps);
+            } catch (_) {}
+        }
+        return invalidValue(typs, val);
+    }
+
+    function transformEnum(cases: string[], val: any): any {
+        if (cases.indexOf(val) !== -1) return val;
+        return invalidValue(cases, val);
+    }
+
+    function transformArray(typ: any, val: any): any {
+        // val must be an array with no invalid elements
+        if (!Array.isArray(val)) return invalidValue("array", val);
+        return val.map(el => transform(el, typ, getProps));
+    }
+
+    function transformDate(typ: any, val: any): any {
+        if (val === null) {
+            return null;
+        }
+        const d = new Date(val);
+        if (isNaN(d.valueOf())) {
+            return invalidValue("Date", val);
+        }
+        return d;
+    }
+
+    function transformObject(props: { [k: string]: any }, additional: any, val: any): any {
+        if (val === null || typeof val !== "object" || Array.isArray(val)) {
+            return invalidValue("object", val);
+        }
+        var result: any = {};
+        Object.getOwnPropertyNames(props).forEach(key => {
+            const prop = props[key];
+            const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
+            result[prop.key] = transform(v, prop.typ, getProps);
+        });
+        Object.getOwnPropertyNames(val).forEach(key => {
+            if (!Object.prototype.hasOwnProperty.call(props, key)) {
+                result[key] = transform(val[key], additional, getProps);
+            }
+        });
+        return result;
+    }
+
+    if (typ === "any") return val;
+    if (typ === null) {
+        if (val === null) return val;
+        return invalidValue(typ, val);
+    }
+    if (typ === false) return invalidValue(typ, val);
+    while (typeof typ === "object" && typ.ref !== undefined) {
+        typ = typeMap[typ.ref];
+    }
+    if (Array.isArray(typ)) return transformEnum(typ, val);
+    if (typeof typ === "object") {
+        return typ.hasOwnProperty("unionMembers") ? transformUnion(typ.unionMembers, val)
+            : typ.hasOwnProperty("arrayItems")    ? transformArray(typ.arrayItems, val)
+            : typ.hasOwnProperty("props")         ? transformObject(getProps(typ), typ.additional, val)
+            : invalidValue(typ, val);
+    }
+    // Numbers can be parsed by Date but shouldn't be.
+    if (typ === Date && typeof val !== "number") return transformDate(typ, val);
+    return transformPrimitive(typ, val);
+}
+
+function cast<T>(val: any, typ: any): T {
+    return transform(val, typ, jsonToJSProps);
+}
+
+function uncast<T>(val: T, typ: any): any {
+    return transform(val, typ, jsToJSONProps);
+}
+
+function a(typ: any) {
+    return { arrayItems: typ };
+}
+
+function u(...typs: any[]) {
+    return { unionMembers: typs };
+}
+
+function o(props: any[], additional: any) {
+    return { props, additional };
+}
+
+function m(additional: any) {
+    return { props: [], additional };
+}
+
+function r(name: string) {
+    return { ref: name };
+}
+
+const typeMap: any = {
+    "ExecJSON": o([
+        { json: "platform", js: "platform", typ: r("Platform") },
+        { json: "profiles", js: "profiles", typ: a(r("ExecJSONProfile")) },
+        { json: "statistics", js: "statistics", typ: r("Statistics") },
+        { json: "version", js: "version", typ: "" },
+    ], false),
+    "Platform": o([
+        { json: "name", js: "name", typ: "" },
+        { json: "release", js: "release", typ: "" },
+        { json: "target_id", js: "target_id", typ: u(undefined, "") },
+    ], false),
+    "ExecJSONProfile": o([
+        { json: "attributes", js: "attributes", typ: a(m("any")) },
+        { json: "controls", js: "controls", typ: a(r("ExecJSONControl")) },
+        { json: "copyright", js: "copyright", typ: u(undefined, "") },
+        { json: "copyright_email", js: "copyright_email", typ: u(undefined, "") },
+        { json: "groups", js: "groups", typ: a(r("ControlGroup")) },
+        { json: "license", js: "license", typ: u(undefined, "") },
+        { json: "maintainer", js: "maintainer", typ: u(undefined, "") },
+        { json: "name", js: "name", typ: "" },
+        { json: "sha256", js: "sha256", typ: "" },
+        { json: "skip_message", js: "skip_message", typ: u(undefined, "") },
+        { json: "status", js: "status", typ: "" },
+        { json: "summary", js: "summary", typ: u(undefined, "") },
+        { json: "supports", js: "supports", typ: a(r("SupportedPlatform")) },
+        { json: "title", js: "title", typ: u(undefined, "") },
+        { json: "version", js: "version", typ: u(undefined, "") },
+    ], false),
+    "ExecJSONControl": o([
+        { json: "code", js: "code", typ: "" },
+        { json: "desc", js: "desc", typ: u(null, "") },
+        { json: "descriptions", js: "descriptions", typ: a(r("ControlDescription")) },
+        { json: "id", js: "id", typ: "" },
+        { json: "impact", js: "impact", typ: 3.14 },
+        { json: "refs", js: "refs", typ: a(r("Reference")) },
+        { json: "results", js: "results", typ: a(r("ControlResult")) },
+        { json: "source_location", js: "source_location", typ: r("SourceLocation") },
+        { json: "tags", js: "tags", typ: m("any") },
+        { json: "title", js: "title", typ: u(null, "") },
+    ], false),
+    "ControlDescription": o([
+        { json: "data", js: "data", typ: "" },
+        { json: "label", js: "label", typ: "" },
+    ], false),
+    "Reference": o([
+        { json: "ref", js: "ref", typ: u(undefined, "") },
+        { json: "url", js: "url", typ: u(undefined, "") },
+        { json: "uri", js: "uri", typ: u(undefined, "") },
+    ], "any"),
+    "ControlResult": o([
+        { json: "backtrace", js: "backtrace", typ: u(undefined, m("any")) },
+        { json: "code_desc", js: "code_desc", typ: "" },
+        { json: "exception", js: "exception", typ: u(undefined, "") },
+        { json: "message", js: "message", typ: u(undefined, "") },
+        { json: "resource", js: "resource", typ: u(undefined, "") },
+        { json: "run_time", js: "run_time", typ: 3.14 },
+        { json: "skip_message", js: "skip_message", typ: u(undefined, "") },
+        { json: "start_time", js: "start_time", typ: "" },
+        { json: "status", js: "status", typ: "" },
+    ], false),
+    "SourceLocation": o([
+        { json: "line", js: "line", typ: 3.14 },
+        { json: "ref", js: "ref", typ: "" },
+    ], false),
+    "ControlGroup": o([
+        { json: "controls", js: "controls", typ: a("") },
+        { json: "id", js: "id", typ: "" },
+        { json: "title", js: "title", typ: u(undefined, "") },
+    ], false),
+    "SupportedPlatform": o([
+        { json: "os-family", js: "os-family", typ: u(undefined, "") },
+        { json: "os-name", js: "os-name", typ: u(undefined, "") },
+        { json: "platform", js: "platform", typ: u(undefined, "") },
+        { json: "platform-family", js: "platform-family", typ: "" },
+        { json: "platform-name", js: "platform-name", typ: u(undefined, "") },
+    ], false),
+    "Statistics": o([
+        { json: "controls", js: "controls", typ: u(undefined, r("StatisticHash")) },
+        { json: "duration", js: "duration", typ: 3.14 },
+    ], false),
+    "StatisticHash": o([
+        { json: "failed", js: "failed", typ: u(undefined, r("StatisticBlock")) },
+        { json: "passed", js: "passed", typ: u(undefined, r("StatisticBlock")) },
+        { json: "skipped", js: "skipped", typ: u(undefined, r("StatisticBlock")) },
+    ], false),
+    "StatisticBlock": o([
+        { json: "total", js: "total", typ: 3.14 },
+    ], false),
+};
---
+++ src/generated-parsers/profile-json.ts
@@ -0,0 +1,280 @@
+// To parse this data:
+//
+//   import { Convert, ProfileJSON } from "./file";
+//
+//   const profileJSON = Convert.toProfileJSON(json);
+//
+// These functions will throw an error if the JSON doesn't
+// match the expected interface, even if the JSON is valid.
+
+export interface ProfileJSON {
+    controls:         ProfileJSONControl[];
+    copyright?:       string;
+    copyright_email?: string;
+    generator?:       Generator;
+    groups:           ControlGroup[];
+    inputs:           { [key: string]: any }[];
+    maintainer?:      string;
+    name:             string;
+    sha256:           string;
+    status:           string;
+    supports:         SupportedPlatform[];
+    title?:           string;
+}
+
+export interface ProfileJSONControl {
+    code:            string;
+    desc:            null | string;
+    descriptions:    { [key: string]: any };
+    id:              string;
+    impact:          number;
+    refs:            Reference[];
+    source_location: SourceLocation;
+    tags:            { [key: string]: any };
+    title:           null | string;
+}
+
+export interface Reference {
+    ref?: string;
+    url?: string;
+    uri?: string;
+}
+
+export interface SourceLocation {
+    /**
+     * The line at which this statement is located in the file
+     */
+    line: number;
+    /**
+     * Path to the file that this statement originates from
+     */
+    ref: string;
+}
+
+export interface Generator {
+    /**
+     * The name of the software that generated this report.
+     */
+    name: string;
+    /**
+     * The version of the software that generated this report.
+     */
+    version: string;
+}
+
+export interface ControlGroup {
+    /**
+     * The control IDs in this group
+     */
+    controls: string[];
+    /**
+     * The unique identifier of the group
+     */
+    id: string;
+    /**
+     * The name of the group
+     */
+    title?: string;
+}
+
+export interface SupportedPlatform {
+    "os-family"?:      string;
+    "os-name"?:        string;
+    platform?:         string;
+    "platform-family": string;
+    "platform-name"?:  string;
+}
+
+// Converts JSON strings to/from your types
+// and asserts the results of JSON.parse at runtime
+export class Convert {
+    public static toProfileJSON(json: string): ProfileJSON {
+        return cast(JSON.parse(json), r("ProfileJSON"));
+    }
+
+    public static profileJSONToJson(value: ProfileJSON): string {
+        return JSON.stringify(uncast(value, r("ProfileJSON")), null, 2);
+    }
+}
+
+function invalidValue(typ: any, val: any): never {
+    throw Error(`Invalid value ${JSON.stringify(val)} for type ${JSON.stringify(typ)}`);
+}
+
+function jsonToJSProps(typ: any): any {
+    if (typ.jsonToJS === undefined) {
+        var map: any = {};
+        typ.props.forEach((p: any) => map[p.json] = { key: p.js, typ: p.typ });
+        typ.jsonToJS = map;
+    }
+    return typ.jsonToJS;
+}
+
+function jsToJSONProps(typ: any): any {
+    if (typ.jsToJSON === undefined) {
+        var map: any = {};
+        typ.props.forEach((p: any) => map[p.js] = { key: p.json, typ: p.typ });
+        typ.jsToJSON = map;
+    }
+    return typ.jsToJSON;
+}
+
+function transform(val: any, typ: any, getProps: any): any {
+    function transformPrimitive(typ: string, val: any): any {
+        if (typeof typ === typeof val) return val;
+        return invalidValue(typ, val);
+    }
+
+    function transformUnion(typs: any[], val: any): any {
+        // val must validate against one typ in typs
+        var l = typs.length;
+        for (var i = 0; i < l; i++) {
+            var typ = typs[i];
+            try {
+                return transform(val, typ, getProps);
+            } catch (_) {}
+        }
+        return invalidValue(typs, val);
+    }
+
+    function transformEnum(cases: string[], val: any): any {
+        if (cases.indexOf(val) !== -1) return val;
+        return invalidValue(cases, val);
+    }
+
+    function transformArray(typ: any, val: any): any {
+        // val must be an array with no invalid elements
+        if (!Array.isArray(val)) return invalidValue("array", val);
+        return val.map(el => transform(el, typ, getProps));
+    }
+
+    function transformDate(typ: any, val: any): any {
+        if (val === null) {
+            return null;
+        }
+        const d = new Date(val);
+        if (isNaN(d.valueOf())) {
+            return invalidValue("Date", val);
+        }
+        return d;
+    }
+
+    function transformObject(props: { [k: string]: any }, additional: any, val: any): any {
+        if (val === null || typeof val !== "object" || Array.isArray(val)) {
+            return invalidValue("object", val);
+        }
+        var result: any = {};
+        Object.getOwnPropertyNames(props).forEach(key => {
+            const prop = props[key];
+            const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
+            result[prop.key] = transform(v, prop.typ, getProps);
+        });
+        Object.getOwnPropertyNames(val).forEach(key => {
+            if (!Object.prototype.hasOwnProperty.call(props, key)) {
+                result[key] = transform(val[key], additional, getProps);
+            }
+        });
+        return result;
+    }
+
+    if (typ === "any") return val;
+    if (typ === null) {
+        if (val === null) return val;
+        return invalidValue(typ, val);
+    }
+    if (typ === false) return invalidValue(typ, val);
+    while (typeof typ === "object" && typ.ref !== undefined) {
+        typ = typeMap[typ.ref];
+    }
+    if (Array.isArray(typ)) return transformEnum(typ, val);
+    if (typeof typ === "object") {
+        return typ.hasOwnProperty("unionMembers") ? transformUnion(typ.unionMembers, val)
+            : typ.hasOwnProperty("arrayItems")    ? transformArray(typ.arrayItems, val)
+            : typ.hasOwnProperty("props")         ? transformObject(getProps(typ), typ.additional, val)
+            : invalidValue(typ, val);
+    }
+    // Numbers can be parsed by Date but shouldn't be.
+    if (typ === Date && typeof val !== "number") return transformDate(typ, val);
+    return transformPrimitive(typ, val);
+}
+
+function cast<T>(val: any, typ: any): T {
+    return transform(val, typ, jsonToJSProps);
+}
+
+function uncast<T>(val: T, typ: any): any {
+    return transform(val, typ, jsToJSONProps);
+}
+
+function a(typ: any) {
+    return { arrayItems: typ };
+}
+
+function u(...typs: any[]) {
+    return { unionMembers: typs };
+}
+
+function o(props: any[], additional: any) {
+    return { props, additional };
+}
+
+function m(additional: any) {
+    return { props: [], additional };
+}
+
+function r(name: string) {
+    return { ref: name };
+}
+
+const typeMap: any = {
+    "ProfileJSON": o([
+        { json: "controls", js: "controls", typ: a(r("ProfileJSONControl")) },
+        { json: "copyright", js: "copyright", typ: u(undefined, "") },
+        { json: "copyright_email", js: "copyright_email", typ: u(undefined, "") },
+        { json: "generator", js: "generator", typ: u(undefined, r("Generator")) },
+        { json: "groups", js: "groups", typ: a(r("ControlGroup")) },
+        { json: "inputs", js: "inputs", typ: a(m("any")) },
+        { json: "maintainer", js: "maintainer", typ: u(undefined, "") },
+        { json: "name", js: "name", typ: "" },
+        { json: "sha256", js: "sha256", typ: "" },
+        { json: "status", js: "status", typ: "" },
+        { json: "supports", js: "supports", typ: a(r("SupportedPlatform")) },
+        { json: "title", js: "title", typ: u(undefined, "") },
+    ], "any"),
+    "ProfileJSONControl": o([
+        { json: "code", js: "code", typ: "" },
+        { json: "desc", js: "desc", typ: u(null, "") },
+        { json: "descriptions", js: "descriptions", typ: m("any") },
+        { json: "id", js: "id", typ: "" },
+        { json: "impact", js: "impact", typ: 3.14 },
+        { json: "refs", js: "refs", typ: a(r("Reference")) },
+        { json: "source_location", js: "source_location", typ: r("SourceLocation") },
+        { json: "tags", js: "tags", typ: m("any") },
+        { json: "title", js: "title", typ: u(null, "") },
+    ], false),
+    "Reference": o([
+        { json: "ref", js: "ref", typ: u(undefined, "") },
+        { json: "url", js: "url", typ: u(undefined, "") },
+        { json: "uri", js: "uri", typ: u(undefined, "") },
+    ], "any"),
+    "SourceLocation": o([
+        { json: "line", js: "line", typ: 3.14 },
+        { json: "ref", js: "ref", typ: "" },
+    ], false),
+    "Generator": o([
+        { json: "name", js: "name", typ: "" },
+        { json: "version", js: "version", typ: "" },
+    ], false),
+    "ControlGroup": o([
+        { json: "controls", js: "controls", typ: a("") },
+        { json: "id", js: "id", typ: "" },
+        { json: "title", js: "title", typ: u(undefined, "") },
+    ], false),
+    "SupportedPlatform": o([
+        { json: "os-family", js: "os-family", typ: u(undefined, "") },
+        { json: "os-name", js: "os-name", typ: u(undefined, "") },
+        { json: "platform", js: "platform", typ: u(undefined, "") },
+        { json: "platform-family", js: "platform-family", typ: "" },
+        { json: "platform-name", js: "platform-name", typ: u(undefined, "") },
+    ], false),
+};
