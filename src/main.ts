8ddb182b0f92120e4688bd9f4bd6bcd49317742e
Kyle Fagan <kfagan@mitre.org>
2020-04-15 12:52:50 -0400

    Add helmet to app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet())
   await app.listen(3000);
 }
 bootstrap();
