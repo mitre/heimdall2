03d2e204264af72f15875e449e464c129a4b0b3e
Aaron Lippold <lippold@gmail.com>
2020-04-29 12:14:14 -0400

    added fibers to the package.json
    Signed-off-by: Aaron Lippold <lippold@gmail.com>

--- apps/frontend/src/views/Auth.vue
+++ apps/frontend/src/views/Auth.vue
@@ -1,109 +1,64 @@
 <template>
-  <v-container class="bar lighten-2">
-    <v-row justify="space-around" no-gutters>
-      <v-col center xl="8" md="8" sm="12" xs="12">
-        <v-tabs
-          :vertical="$vuetify.breakpoint.mdAndUp"
-          active
-          :value="active_tab"
-          @change="selected_tab"
-          color="primary-visible"
-          show-arrows
-        >
-          <v-tabs-slider></v-tabs-slider>
-          <!-- Define our tabs -->
-          <v-tab href="#login-tab">Login</v-tab>
-          <v-tab href="#register-tab">Sign Up</v-tab>
-
-          <!-- Include those components -->
-          <v-tab-item value="login-tab">
-            <v-card>
-              <v-card-title class="headline" primary-title>
-                Login to Heimdall Server
-              </v-card-title>
-
+  <v-app id="inspire">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="4">
+            <v-card class="elevation-12">
+              <v-toolbar color="primary" dark flat>
+                <v-toolbar-title>Login to Heimdall</v-toolbar-title>
+                <v-spacer />
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn :href="source" icon large target="_blank" v-on="on">
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      large
+                      href="https://codepen.io/johnjleider/pen/pMvGQO"
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-codepen</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
               <v-card-text>
-                <v-form ref="form" v-model="valid" lazy-validation>
-                  <v-container>
-                    <v-text-field
-                      v-model="username"
-                      label="email address"
-                      maxlength="70"
-                      required
-                    />
+                <v-form>
+                  <v-text-field
+                    label="Login"
+                    name="login"
+                    prepend-icon="person"
+                    type="text"
+                  />
 
-                    <v-text-field
-                      type="password"
-                      v-model="password"
-                      label="password"
-                      maxlength="70"
-                      required
-                    />
-                    <v-text-field
-                      v-model="host"
-                      label="host"
-                      maxlength="200"
-                      required
-                    />
-                  </v-container>
-                  <v-btn
-                    class="pink white--text"
-                    :disabled="!valid"
-                    @click="login"
-                    >Login</v-btn
-                  >
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                  />
                 </v-form>
               </v-card-text>
-              <v-divider></v-divider>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
             </v-card>
-          </v-tab-item>
-
-          <v-tab-item value="register-tab">
-            <v-card>
-              <v-card-title class="headline" primary-title>
-                Register with Heimdall Server
-              </v-card-title>
-
-              <v-card-text>
-                <v-form ref="form" v-model="valid" lazy-validation>
-                  <v-container>
-                    <v-text-field
-                      v-model="username"
-                      label="email address"
-                      maxlength="70"
-                      required
-                    />
-
-                    <v-text-field
-                      type="password"
-                      v-model="password"
-                      label="password"
-                      maxlength="70"
-                      required
-                    />
-                    <v-text-field
-                      v-model="host"
-                      label="host"
-                      maxlength="200"
-                      required
-                    />
-                  </v-container>
-                  <v-btn
-                    class="pink white--text"
-                    :disabled="!valid"
-                    @click="register"
-                    >Sign Up</v-btn
-                  >
-                </v-form>
-              </v-card-text>
-              <v-divider></v-divider>
-            </v-card>
-          </v-tab-item>
-        </v-tabs>
-      </v-col>
-    </v-row>
-    <div hidden>{{ watches }}</div>
-  </v-container>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -123,7 +78,9 @@ export interface LoginHash {
 // We declare the props separately
 // to make props types inferrable.
 const AuthProps = Vue.extend({
-  props: {}
+  props: {
+    source: String
+  }
 });
 
 @Component({
