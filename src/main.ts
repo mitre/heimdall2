9aeee79d676dbfe9bced227bb769ae968315249b
Kyle Fagan <kfagan@mitre.org>
2020-05-18 16:10:07 -0400

    Update helmet import to work with esModuleInterop

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as helmet from 'helmet';
+import helmet = require('helmet');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
