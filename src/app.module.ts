b4d165ecaaa900fe717fd4bf746be2c97e2fd247
Kyle Fagan <kfagan@mitre.org>
2020-05-19 16:07:11 -0400

    Add ConfigModule to App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
     UsersModule,
-    DatabaseModule
+    DatabaseModule,
+    ConfigModule
   ],
   controllers: [AppController],
   providers: [AppService],
