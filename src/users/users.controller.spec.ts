4bee3ea42473a3ce88d5f53e88ff7cdb9d783170
Sam Sunvold <ssunvold@mitre.org>
2020-05-26 11:50:10 -0400

    rebase with master
    Signed-off-by: Sam Sunvold <ssunvold@mitre.org>

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,9 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+<<<<<<< HEAD
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './user.model';
 import { UserDto } from './dto/user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
+=======
+import { SequelizeModule } from '@nestjs/sequelize';
+import { UsersController } from '../../src/users/users.controller';
+import { UsersService } from '../../src/users/users.service';
+import { UsersModule } from '../../src/users/users.module';
+import { User } from '../../src/users/user.model';
+import { UserDto } from '../../src/users/dto/user.dto';
+import { CreateUserDto } from '../../src/users/dto/create-user.dto';
+>>>>>>> 2a8b421... Update
 import { async } from 'rxjs/internal/scheduler/async';
 
 // Test suite for the UsersController
@@ -15,6 +25,7 @@ describe("UsersController", () => {
     beforeEach(async () => {
         module = await Test.createTestingModule({
             controllers: [UsersController],
+<<<<<<< HEAD
             providers: [
                 {
                     provide: UsersService,
@@ -24,10 +35,16 @@ describe("UsersController", () => {
                 }
             ],
         }).compile();
+=======
+            providers: [UsersService],
+        }).compile();
+
+>>>>>>> 2a8b421... Update
         usersService = module.get<UsersService>(UsersService);
         usersController = module.get<UsersController>(UsersController);
     });
 
+<<<<<<< HEAD
     // Test suite for findByID
     describe("UsersController", () => {
         it("should create", async () => {
@@ -48,4 +65,20 @@ describe("UsersController", () => {
             
         });
     });
+=======
+    it("should work", () => {
+        expect(5).toBe(5);
+    })
+
+    // // Test suite for findByID
+    // describe("findById", () => {
+    //     it("should ", async () => {
+    //         const user = new User();
+    //         user.id = 0;
+
+    //         jest.spyOn(usersService, "findById").mockResolvedValue(new UserDto(user));
+    //         expect(await usersController.findById(0)).toBe(0);
+    //     });
+    // });
+>>>>>>> 2a8b421... Update
 });
