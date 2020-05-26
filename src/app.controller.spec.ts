1a56fc61c7b40595c2459704dd2e9be2fcde1abc
Sam Sunvold <ssunvold@mitre.org>
2020-05-26 09:33:58 -0400

    Added first unit test for users controller
    Signed-off-by: Sam Sunvold <ssunvold@mitre.org>

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Sequelize } from 'sequelize-typescript';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,7 +9,13 @@ describe('AppController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+         {
+           provide: Sequelize,
+           useValue: {}
+         }
+      ],
     }).compile();
 
     appController = app.get<AppController>(AppController);
--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,11 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { SequelizeModule } from '@nestjs/sequelize';
-import { UsersController } from '../../src/users/users.controller';
-import { UsersService } from '../../src/users/users.service';
-import { UsersModule } from '../../src/users/users.module';
-import { User } from '../../src/users/user.model';
-import { UserDto } from '../../src/users/dto/user.dto';
-import { CreateUserDto } from '../../src/users/dto/create-user.dto';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './user.model';
+import { UserDto } from './dto/user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 import { async } from 'rxjs/internal/scheduler/async';
 
 // Test suite for the UsersController
@@ -17,25 +15,37 @@ describe("UsersController", () => {
     beforeEach(async () => {
         module = await Test.createTestingModule({
             controllers: [UsersController],
-            providers: [UsersService],
+            providers: [
+                {
+                    provide: UsersService,
+                    useFactory: () => ({
+                        create: jest.fn(() => true),
+                    })
+                }
+            ],
         }).compile();
-
         usersService = module.get<UsersService>(UsersService);
         usersController = module.get<UsersController>(UsersController);
     });
 
-    it("should work", () => {
-        expect(5).toBe(5);
-    })
+    // Test suite for findByID
+    describe("UsersController", () => {
+        it("should create", async () => {
+            const userDTO: CreateUserDto = {
+                email: "abc@fakeemail.com",
+                password: "Letmein123",
+                passwordConfirmation: "Letmein123",
+                firstName: "John",
+                lastName: "Doe",
+                title: "intern",
+                organization: "Fake Org",
+            };
+            usersController.create(userDTO);
+            expect(usersService.create).toHaveBeenCalledWith(userDTO);
+        });
 
-    // // Test suite for findByID
-    // describe("findById", () => {
-    //     it("should ", async () => {
-    //         const user = new User();
-    //         user.id = 0;
-
-    //         jest.spyOn(usersService, "findById").mockResolvedValue(new UserDto(user));
-    //         expect(await usersController.findById(0)).toBe(0);
-    //     });
-    // });
+        it("should find by ID", () => {
+            
+        });
+    });
 });
