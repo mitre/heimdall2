a789aba124687c43b8380dbe8b700349bb27a440
Sam Sunvold <ssunvold@mitre.org>
2020-05-26 11:57:17 -0400

    Rebased with master
    Signed-off-by: Sam Sunvold <ssunvold@mitre.org>

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,25 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c00e275... Added first unit test for users controller
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './user.model';
 import { UserDto } from './dto/user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
-<<<<<<< HEAD
-=======
-import { SequelizeModule } from '@nestjs/sequelize';
-import { UsersController } from '../../src/users/users.controller';
-import { UsersService } from '../../src/users/users.service';
-import { UsersModule } from '../../src/users/users.module';
-import { User } from '../../src/users/user.model';
-import { UserDto } from '../../src/users/dto/user.dto';
-import { CreateUserDto } from '../../src/users/dto/create-user.dto';
->>>>>>> 2a8b421... Update
-=======
->>>>>>> c00e275... Added first unit test for users controller
 import { async } from 'rxjs/internal/scheduler/async';
 
 // Test suite for the UsersController
@@ -31,10 +15,6 @@ describe("UsersController", () => {
     beforeEach(async () => {
         module = await Test.createTestingModule({
             controllers: [UsersController],
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c00e275... Added first unit test for users controller
             providers: [
                 {
                     provide: UsersService,
@@ -43,24 +23,11 @@ describe("UsersController", () => {
                     })
                 }
             ],
-<<<<<<< HEAD
         }).compile();
-=======
-            providers: [UsersService],
-        }).compile();
-
->>>>>>> 2a8b421... Update
-=======
-        }).compile();
->>>>>>> c00e275... Added first unit test for users controller
         usersService = module.get<UsersService>(UsersService);
         usersController = module.get<UsersController>(UsersController);
     });
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> c00e275... Added first unit test for users controller
     // Test suite for findByID
     describe("UsersController", () => {
         it("should create", async () => {
@@ -76,33 +43,9 @@ describe("UsersController", () => {
             usersController.create(userDTO);
             expect(usersService.create).toHaveBeenCalledWith(userDTO);
         });
-<<<<<<< HEAD
 
         it("should find by ID", () => {
             
         });
     });
-=======
-    it("should work", () => {
-        expect(5).toBe(5);
-    })
-
-    // // Test suite for findByID
-    // describe("findById", () => {
-    //     it("should ", async () => {
-    //         const user = new User();
-    //         user.id = 0;
-
-    //         jest.spyOn(usersService, "findById").mockResolvedValue(new UserDto(user));
-    //         expect(await usersController.findById(0)).toBe(0);
-    //     });
-    // });
->>>>>>> 2a8b421... Update
-=======
-
-        it("should find by ID", () => {
-            
-        });
-    });
->>>>>>> c00e275... Added first unit test for users controller
 });
