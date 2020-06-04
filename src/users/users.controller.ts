4ff143480062363d758847ed95bd257d0194e4cf
Kyle Fagan <kfagan@mitre.org>
2020-06-04 16:13:41 -0400

    Add JWT Auth Guard to appropriate users controller routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,10 +9,12 @@ import { PasswordsMatchPipe } from '../pipes/passwords-match.pipe';
 import { PasswordComplexityPipe } from '../pipes/password-complexity.pipe';
 import { PasswordChangePipe } from '../pipes/password-change.pipe';
 import { AbacGuard } from '../guards/abac.guard';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findById(@Param('id') id: number): Promise<UserDto> {
     return this.usersService.findById(id);
@@ -25,12 +27,13 @@ export class UsersController {
     return this.usersService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   async update(@Param('id') id: number, @Body(new PasswordsMatchPipe(), new PasswordChangePipe(), new PasswordComplexityPipe()) updateUserDto: UpdateUserDto) {
     return this.usersService.update(id, updateUserDto);
   }
 
-  @UseGuards(AbacGuard)
+  @UseGuards(JwtAuthGuard, AbacGuard)
   @Delete(':id')
   async remove(@Param('id') id: number, @Body() deleteUserDto: DeleteUserDto): Promise<UserDto> {
     return this.usersService.remove(id, deleteUserDto)
