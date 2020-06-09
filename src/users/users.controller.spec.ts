ed892fab3e72e71909bb21b55994c3068bfd6cf9
Sam Sunvold <sunvoldsj@vcu.edu>
2020-06-09 14:15:49 -0400

    Rebase

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -18,6 +18,7 @@ import {
 import { AbacGuard } from '../guards/abac.guard';
 
 // Test suite for the UsersController
+<<<<<<< HEAD
 describe('UsersController Unit Tests', () => {
   const mockAbacGuard: CanActivate = { canActivate: jest.fn(() => true)};
   let usersController: UsersController;
@@ -157,3 +158,50 @@ describe('UsersController Unit Tests', () => {
     });
   });
 });
+=======
+describe("UsersController", () => {
+    let usersController: UsersController;
+    let usersService: UsersService;
+    let module: TestingModule;
+
+    beforeEach(async () => {
+        module = await Test.createTestingModule({
+            controllers: [UsersController],
+            providers: [
+                {
+                    provide: UsersService,
+                    useFactory: () => ({
+                        create: jest.fn(),
+                        findByID: jest.fn(),
+                    })
+                }
+            ],
+        }).compile();
+        usersService = module.get<UsersService>(UsersService);
+        usersController = module.get<UsersController>(UsersController);
+    });
+
+    // Test suite for findByID
+    describe("UsersController", () => {
+        it("should create", async () => {
+            const userDTO: CreateUserDto = {
+                email: "abc@fakeemail.com",
+                password: "Letmein123",
+                passwordConfirmation: "Letmein123",
+                firstName: "John",
+                lastName: "Doe",
+                title: "intern",
+                organization: "Fake Org",
+            };
+            usersController.create(userDTO);
+            expect(usersService.create).toHaveBeenCalledWith(userDTO);
+        });
+
+        it("should find by ID", () => {
+            const id: number = 7;
+            usersController.findById(id);
+            expect(usersService.findById).toHaveBeenCalledWith(id);
+        });
+    });
+});
+>>>>>>> 28adb4d... More unit tests
