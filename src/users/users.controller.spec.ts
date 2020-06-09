46942505a92b2ddcb70cc41777fcfe22296bb96a
Sam Sunvold <sunvoldsj@vcu.edu>
2020-06-09 14:16:51 -0400

    Rebase

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -2,6 +2,7 @@ import { NotFoundException, BadRequestException, CanActivate } from '@nestjs/com
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+<<<<<<< HEAD
 import {
   ID,
   USER_ONE_DTO,
@@ -16,6 +17,11 @@ import {
   CREATE_USER_DTO_TEST_OBJ_WITH_MISSING_PASSWORD_CONFIRMATION_FIELD,
 } from '../../test/test.constants';
 import { AbacGuard } from '../guards/abac.guard';
+=======
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { DeleteUserDto } from './dto/delete-user.dto';
+>>>>>>> 49f931e... Finished UsersController unit tests
 
 // Test suite for the UsersController
 <<<<<<< HEAD
@@ -164,6 +170,8 @@ describe("UsersController", () => {
     let usersService: UsersService;
     let module: TestingModule;
 
+    const ID: number = 7;
+
     beforeEach(async () => {
         module = await Test.createTestingModule({
             controllers: [UsersController],
@@ -172,17 +180,27 @@ describe("UsersController", () => {
                     provide: UsersService,
                     useFactory: () => ({
                         create: jest.fn(),
-                        findByID: jest.fn(),
+                        findById: jest.fn(),
+                        update: jest.fn(),
+                        remove: jest.fn()
                     })
                 }
             ],
         }).compile();
+
         usersService = module.get<UsersService>(UsersService);
         usersController = module.get<UsersController>(UsersController);
     });
 
-    // Test suite for findByID
     describe("UsersController", () => {
+        // Tests the findById function
+        it("should findById", () => {
+            usersController.findById(ID);
+            expect(usersService.findById).toHaveBeenCalledWith(ID);
+            expect(usersService.findById).toBeCalledTimes(1);
+        });
+
+        // Tests the create function
         it("should create", async () => {
             const userDTO: CreateUserDto = {
                 email: "abc@fakeemail.com",
@@ -195,12 +213,34 @@ describe("UsersController", () => {
             };
             usersController.create(userDTO);
             expect(usersService.create).toHaveBeenCalledWith(userDTO);
+            expect(usersService.create).toBeCalledTimes(1);
         });
 
-        it("should find by ID", () => {
-            const id: number = 7;
-            usersController.findById(id);
-            expect(usersService.findById).toHaveBeenCalledWith(id);
+        // Tests the update function
+        it("should update", () => {
+            const updateUserDTO: UpdateUserDto = {
+                email: "updatedemail@yahoo.com",
+                firstName: "Jane",
+                lastName: "Doe",
+                organization: "Fake Org",
+                title: "Intern",
+                password: "newpassword",
+                passwordConfirmation: "newpassword",
+                currentPassword: "currentpassword"
+            };
+            usersController.update(ID, updateUserDTO);
+            expect(usersService.update).toHaveBeenCalledWith(ID, updateUserDTO);
+            expect(usersService.update).toBeCalledTimes(1);
+        });
+
+        // Tests the remove function
+        it("should remove", () => {
+            const deleteUserDTO : DeleteUserDto = {
+                password: "deleteuserdto"
+            };
+            usersController.remove(ID, deleteUserDTO);
+            expect(usersService.remove).toHaveBeenCalledWith(ID, deleteUserDTO);
+            expect(usersService.remove).toBeCalledTimes(1);
         });
     });
 });
