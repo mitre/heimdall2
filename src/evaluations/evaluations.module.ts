907849ea482f0bc89fe6d6139f62199b897b79a0
Sam Perry <sperry@mitre.org>
2020-06-17 10:33:53 -0400

    Created evaluation.model.ts file as it is referenced inside the service.ts file. module.ts was based off the user.module.ts file but updated for evaluations, same goes for the services.ts file.

---
+++ src/evaluations/evaluations.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { EvaluationsService } from "./evaluations.service";
+
+
+@Module({
+  providers: [EvaluationsService],
+  exports: [EvaluationsService]
+})
+export class EvaluationsModule {}
