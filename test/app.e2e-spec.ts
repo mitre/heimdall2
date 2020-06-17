1b1e6915fa192c0dd893a2c157e69336b80172ef
Sam Sunvold <sunvoldsj@vcu.edu>
2020-06-17 10:54:47 -0400

    added POST test
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,20 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { INestApplication, ValidationPipe, HttpStatus } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
+import { DatabaseService } from './../src/database/database.service';
+import { CREATE_USER_DTO_TEST_OBJ } from './test.constants';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  let databaseService: DatabaseService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
+    jest.setTimeout(10000);
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [DatabaseService]
     }).compile();
 
+    databaseService = moduleFixture.get<DatabaseService>(DatabaseService);
+
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        whitelist: true
+      })
+    );
     await app.init();
   });
 
-  afterAll(() => {
-    app.close();
+  beforeEach(async () => {
+    await databaseService.cleanAll();
+  });
+
+  describe('/users', () => {
+    it('should POST', async () => {
+      return request(app.getHttpServer()).post('/users').send(CREATE_USER_DTO_TEST_OBJ).expect(HttpStatus.CREATED).then(response => {
+        expect(response.body.email).toEqual('abc@yahoo.com')
+      });
+    });
+  });
+
+  afterAll(async () => {
+    await app.close();
+    await databaseService.closeConnection();
   });
 });
