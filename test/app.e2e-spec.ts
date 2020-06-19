033ba71a3984a34de2b21ff3cf77186abdff1e4a
Sam Sunvold <sunvoldsj@vcu.edu>
2020-06-19 11:06:15 -0400

    Added user POST e2e tests
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import request from 'supertest';
 import { INestApplication, ValidationPipe, HttpStatus } from '@nestjs/common';
 import { AppModule } from './../src/app.module';
 import { DatabaseService } from './../src/database/database.service';
-import { CREATE_USER_DTO_TEST_OBJ } from './test.constants';
+import { CREATE_USER_DTO_TEST_OBJ, CREATE_USER_DTO_TEST_OBJ_WITH_UNMATCHING_PASSWORDS, TEST_USER, CREATE_USER_DTO_TEST_OBJ_WITH_INVALID_EMAIL_FIELD, CREATE_USER_DTO_TEST_OBJ_WITH_MISSING_PASSWORD_FIELD, CREATE_USER_DTO_TEST_OBJ_WITH_MISSING_PASSWORD_CONFIRMATION_FIELD } from './test.constants';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -32,11 +32,71 @@ describe('AppController (e2e)', () => {
   });
 
   describe('/users', () => {
-    it('should POST', async () => {
-      return request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ).expect(HttpStatus.CREATED).then(response => {
-        expect(response.body.email).toEqual('abc@yahoo.com')
+    describe('GET', () => {
+      it('should GET', () => {
+        return request(app.getHttpServer()).get('/users').expect(HttpStatus.NOT_FOUND);
       });
     });
+
+    describe('POST', () => {
+      it('should return 201 status when user is created', async () => {
+        return await request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ).expect(HttpStatus.CREATED).then(response => {
+          expect(response.body.createdAt.valueOf()).not.toBe(TEST_USER.createdAt.valueOf());
+          expect(response.body.email).toEqual(TEST_USER.email);
+          expect(response.body.firstName).toEqual(TEST_USER.firstName);
+          expect(response.body.id).toBeDefined();
+          expect(response.body.lastLogin).toEqual(null);
+          expect(response.body.lastName).toEqual(TEST_USER.lastName);
+          expect(response.body.loginCount).toEqual(TEST_USER.loginCount.toString());
+          expect(response.body.organization).toEqual(TEST_USER.organization);
+          expect(response.body.role).toEqual(TEST_USER.role);
+          expect(response.body.title).toEqual(TEST_USER.title);
+          expect(response.body.updatedAt.valueOf()).not.toBe(TEST_USER.updatedAt.valueOf());
+        });
+      });
+
+      it('should return 400 status if passwords dont match', async () => {
+        return await request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ_WITH_UNMATCHING_PASSWORDS)
+          .expect(HttpStatus.BAD_REQUEST).then(response => {
+            expect(response.body.message).toEqual('Passwords do not match');
+            expect(response.body.error).toEqual('Bad Request');
+          });
+      });
+      
+      it('should return 400 status if password is not provided', async () => {
+        return await request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ_WITH_MISSING_PASSWORD_FIELD)
+          .expect(HttpStatus.BAD_REQUEST).then(response => {
+            expect(response.body.message[0]).toEqual('password should not be empty');
+            expect(response.body.error).toEqual('Bad Request');
+          });
+      });
+
+      it('should return 400 status if passwordConfirmation is not provided', async () => {
+        return await request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ_WITH_MISSING_PASSWORD_CONFIRMATION_FIELD)
+          .expect(HttpStatus.BAD_REQUEST).then(response => {
+            expect(response.body.message[0]).toEqual('passwordConfirmation should not be empty');
+            expect(response.body.error).toEqual('Bad Request');
+          });
+      });
+
+      it('should return 400 status if invalid email is provided', async () => {
+        return await request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ_WITH_INVALID_EMAIL_FIELD)
+          .expect(HttpStatus.BAD_REQUEST).then(response => {
+            expect(response.body.message[0]).toEqual('email must be an email');
+            expect(response.body.error).toEqual('Bad Request');
+          });
+      });
+
+      it('should return 400 status if already exisitng email is given', async () => {
+        await request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ).expect(HttpStatus.CREATED)
+        return await request(app.getHttpServer()).post('/users').set('Content-Type', 'application/json').send(CREATE_USER_DTO_TEST_OBJ)
+          .expect(HttpStatus.INTERNAL_SERVER_ERROR).then(response => {
+            expect(response.body.messages[0].email).toEqual('email must be unique');
+            expect(response.body.error).toEqual('Internal Server Error');
+          });
+      });
+    });
+
   });
 
   afterAll(async () => {
