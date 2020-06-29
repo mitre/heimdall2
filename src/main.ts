dd10a9cb256cc233eb559c5b89a75d60e12671a5
Sam Sunvold <sunvoldsj@vcu.edu>
2020-06-29 10:06:24 -0400

    Added configurable app port functionality
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet = require('helmet');
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
   app.use(helmet());
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,6 +14,6 @@ async function bootstrap() {
       whitelist: true
     })
   );
-  await app.listen(3000);
+  await app.listen(configService.get('HEIMDALL_SERVER_PORT') || 3000);
 }
 bootstrap();
