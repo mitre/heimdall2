3da4a39337acd28f16c62709b67335bf57d7c8fc
Kyle Fagan <kfagan@mitre.org>
2020-06-29 16:27:57 -0400

    Check if password reset is enabled when logging in, set 10m timeout for JWT if it is

--- src/authn/authn.service.ts
+++ src/authn/authn.service.ts
@@ -1,34 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
-import { UserDto } from '../users/dto/user.dto';
+import { User } from '../users/user.model';
 import { compare } from 'bcrypt';
+import { ConfigService } from '../config/config.service';
 
 @Injectable()
 export class AuthnService {
   constructor(
     private usersService: UsersService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private configService: ConfigService
   ) {}
 
   async validateUser(email: string, password: string): Promise<any> {
     const user = await this.usersService.findModelByEmail(email);
     if(user && (await compare(password, user.encryptedPassword))) {
       this.usersService.updateLoginMetadata(user);
-      return new UserDto(user);
+      return user;
     } else {
       return null;
     }
   }
 
-  async login(user: UserDto) {
+  async login(user: User): Promise<any> {
     const payload = {
       email: user.email,
       sub: user.id,
-      role: user.role
+      role: user.role,
+      forcePasswordChange: user.forcePasswordChange
     };
-    return {
-      accessToken: this.jwtService.sign(payload),
-    };
+    if(payload.forcePasswordChange) {
+      // Give the user 10 minutes to (hopefully) change their password.
+      return {
+        accessToken: this.jwtService.sign(payload, { expiresIn: '600s' })
+      }
+    } else {
+      return {
+        accessToken: this.jwtService.sign(payload)
+      }
+    }
   }
 }
