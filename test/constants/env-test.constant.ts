105f4b18cf597c227213bc1199c725e457b82072
Sam Sunvold <sunvoldsj@vcu.edu>
2020-07-01 12:11:23 -0400

    Added env-test.constants file
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

---
+++ test/constants/env-test.constant.ts
@@ -0,0 +1,8 @@
+export const ENV_MOCK_FILE = 'HEIMDALL_SERVER_PORT=8000\n' +
+  'DATABASE_HOST=localhost\n' +
+  'DATABASE_PORT=5432\n' +
+  'DATABASE_USERNAME=postgres\n' +
+  'DATABASE_PASSWORD=postgres\n' +
+  'DATABASE_NAME=heimdallts_jest_testing_service_db\n' +
+  'JWT_SECRET=abc123\n' +
+  'NODE_ENV=test\n';
