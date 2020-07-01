1f1531ca762eaafda543c65378a9846683cbcc3c
Robert Clark <rbclark@mitre.org>
2020-07-01 14:00:20 -0400

    Allow for serving heimdall-client as a static asset

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,15 @@ import {AuthzModule} from './authz/authz.module';
 import {AuthnModule} from './authn/authn.module';
 import {EvaluationTagModule} from './evaluation-tags/evaluation-tags.module';
 import {EvaluationsModule} from './evaluations/evaluations.module';
+import {ServeStaticModule} from '@nestjs/serve-static';
+import {join} from 'path';
 
 @Module({
-  imports: [UsersModule, DatabaseModule, ConfigModule, AuthzModule, AuthnModule, EvaluationTagModule, EvaluationsModule]
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'dist'),
+    }),
+    UsersModule, DatabaseModule, ConfigModule, AuthzModule, AuthnModule, EvaluationTagModule, EvaluationsModule
+  ]
 })
 export class AppModule {}
