b23b10383368e2324d6b6e4f93cb7bb78601d606
Kyle Fagan <kfagan@mitre.org>
2020-07-07 17:09:38 -0400

    Add evaluation tags model

---
+++ src/evaluation-tags/evaluation-tag.model.ts
@@ -0,0 +1,26 @@
+import {Table, Model, AutoIncrement, AllowNull, PrimaryKey, Column, DataType, BelongsTo, ForeignKey} from 'sequelize-typescript';
+import {Evaluation} from '../evaluations/evaluation.model';
+
+@Table
+export class EvaluationTag extends Model<EvaluationTag> {
+  @PrimaryKey
+  @AutoIncrement
+  @AllowNull(false)
+  @Column(DataType.BIGINT)
+  id: number;
+
+  @AllowNull(false)
+  @Column
+  key: string;
+
+  @AllowNull(false)
+  @Column
+  value: string;
+
+  @ForeignKey(() => Evaluation)
+  @Column(DataType.BIGINT)
+  evaluationId: number;
+
+  @BelongsTo(() => Evaluation)
+  evaluation: Evaluation;
+}
