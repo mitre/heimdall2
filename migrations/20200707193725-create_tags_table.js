71bd1ff483e55a8fdf62cf12aa5d8e9dd32ef7b6
Kyle Fagan <kfagan@mitre.org>
2020-07-07 16:49:48 -0400

    Migration for EvaluationTags and association with Evaluations

---
+++ migrations/20200707193725-create_tags_table.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('EvaluationTags', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT
+      },
+      key: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      evaluationId: {
+        type: Sequelize.BIGINT,
+        references: {
+          model: 'Evaluations',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      }
+    })
+    .then(() => {
+      return queryInterface.addColumn('Evaluations', 'evaluationTagId', {
+        type: Sequelize.BIGINT,
+        references: {
+          model: 'EvaluationTags',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      })
+    })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('EvaluationTags')
+    .then(() => {
+      return queryInterface.removeColumn('Evaluations', 'evaluationTagId')
+    });
+  }
+};
