ad96c6f45b854a01e511224c875d6a0b9f931597
Kyle Fagan <kfagan@mitre.org>
2020-07-08 09:57:28 -0400

    Add remove (destroy) method to EvaluationTags service

--- src/evaluation-tags/evaluation-tags.service.ts
+++ src/evaluation-tags/evaluation-tags.service.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import {InjectModel} from '@nestjs/sequelize';
 import {EvaluationTag} from './evaluation-tag.model';
 import {EvaluationTagDto} from './dto/evaluation-tag.dto';
@@ -11,5 +11,20 @@ export class EvaluationTagsService {
   async findAll(): Promise<EvaluationTagDto[]> {
     const evaluationTags = await this.evaluationTagModel.findAll<EvaluationTag>();
     return evaluationTags.map(evaluationTag => new EvaluationTagDto(evaluationTag));
+  }
+
+  async remove(id: number) {
+    const evaluationTag = await this.evaluationTagModel.findByPk<EvaluationTag>(id);
+    this.exists(evaluationTag);
+    await evaluationTag.destroy();
+    return new EvaluationTagDto(evaluationTag);
+  }
+
+  exists(evaluationTag: EvaluationTag): boolean {
+    if (!evaluationTag) {
+      throw new NotFoundException('EvaluationTag with given id not found');
+    } else {
+      return true;
+    }
   }
 }
