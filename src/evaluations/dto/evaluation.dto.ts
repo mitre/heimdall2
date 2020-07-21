d287319bf93463fa4191552bf0c61e3411d72ff7
Kyle Fagan <kfagan@mitre.org>
2020-07-21 08:54:11 -0400

    Update Evaluation DTOs with new data field and correct class-validator decorators on create

--- src/evaluations/dto/evaluation.dto.ts
+++ src/evaluations/dto/evaluation.dto.ts
@@ -2,13 +2,15 @@ import { Evaluation } from '../evaluation.model';
 
 export class EvaluationDto {
   id: number;
-  readonly version: string
+  readonly version: string;
+  readonly data: Object;
   readonly createdAt: Date;
   readonly updatedAt: Date;
 
   constructor(evaluation: Evaluation) {
     this.id = evaluation.id;
     this.version = evaluation.version;
+    this.data = evaluation.data;
     this.createdAt = evaluation.createdAt;
     this.updatedAt = evaluation.updatedAt;
   }
