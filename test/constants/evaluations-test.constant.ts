7e0a4d645afaaf87ed600d32a170ee0b7db260d9
Sam Sunvold <sunvoldsj@vcu.edu>
2020-07-28 11:43:33 -0400

    Removed tag from evaluation test constant so that test would pass again
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

--- test/constants/evaluations-test.constant.ts
+++ test/constants/evaluations-test.constant.ts
@@ -1,8 +1,7 @@
 import {CreateEvaluationDto} from '../../src/evaluations/dto/create-evaluation.dto';
-import {EVALUATION_TAG_1} from './evaluation-tags-test.contant';
 
 export const EVALUATION_1: CreateEvaluationDto = {
   data: {},
   version: '1.0',
-  evaluationTags: [EVALUATION_TAG_1]
+  evaluationTags: []
 };
