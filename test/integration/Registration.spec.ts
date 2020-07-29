746bdd051f6d08d4ee9bde80c182b4e0a92bb921
sjoshi10 <saurabjdc@gmail.com>
2020-07-29 16:17:28 -0400

    add registration tests

---
+++ test/integration/Registration.spec.ts
@@ -0,0 +1,63 @@
+import {Test, TestingModule} from '@nestjs/testing';
+import {AppModule} from '../../src/app.module';
+
+
+
+import {
+    CREATE_ADMIN_DTO, 
+    CREATE_USER_DTO_TEST_OBJ_WITH_UNMATCHING_PASSWORDS, 
+    CREATE_USER_DTO_TEST_OBJ_2
+  } from '../constants/users-test.constant';
+
+import { register} from "./pages/Registration";
+import {DatabaseService} from '../../src/database/database.service';
+
+
+
+describe("Registration", () => {
+    let databaseService: DatabaseService;
+
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule],
+            providers: [DatabaseService]
+          }).compile();
+      
+          databaseService = moduleFixture.get<DatabaseService>(DatabaseService);
+      });
+
+    beforeEach( async () => {
+   await page.goto("http:/localhost:3000/signup");
+    });
+
+
+     it("Registration Success", async () => {
+
+       const response =  await register(page, CREATE_ADMIN_DTO)
+       expect(response).toBe(201)
+    });
+
+    it("Registration failure because password doesn't match", async () => {
+
+        const response =  await register(page, CREATE_USER_DTO_TEST_OBJ_WITH_UNMATCHING_PASSWORDS)
+        expect(response).toBe(400)
+        
+    });
+
+    
+    it("Registration failure because email already exists", async () => {
+
+        const response =  await register(page, CREATE_USER_DTO_TEST_OBJ_2)
+        expect(response).toBe(201)
+        await page.goto("http:/localhost:3000/signup");
+        page.waitForNavigation();
+        const response2 =  await register(page, CREATE_USER_DTO_TEST_OBJ_2)
+        expect(response2).toBe(500)
+        
+    });
+    
+      afterAll(async () => {
+        await databaseService.cleanAll();
+  //      await databaseService.closeConnection();
+      });
+    });
---
+++ test/integration/pages/Registration.ts
@@ -0,0 +1,44 @@
+export async function register(page, USER) {
+    await expect(page).toFillForm('form[name="signup_form"]', {
+        email: USER.email,
+        password: USER.password,
+        password_confirmation: USER.passwordConfirmation,
+        role: USER.role
+      });
+       const wait =  page.waitForNavigation();
+       expect(page).toClick("button", { text: "Register" })
+       const response = await page.waitForResponse(res => res.url() === "http://localhost:3000/users" )
+      if(response.status() == 200){
+      await wait 
+      }
+      const data = await response.status()
+      return data; 
+}
+
+
+/*
+export async function login( USER) {
+
+
+       return  axios.post('http://localhost:3000/authn/login', {email: USER.email, password: USER.password}).then(({data}) => {
+          return data.accessToken
+        }).catch(error => {
+            console.log(error)
+        });
+}
+
+export async function unregister(USER , Response){
+    const token = await login(USER)
+
+
+    axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    await axios.delete('http://localhost:3000/users/'+Response.id,{ data: {password: USER.password}}).then(({data}) => {
+        return console.log(data)
+      }).catch(error => {
+          console.log(error)
+      }); 
+    return token; 
+    //console.log(USER_INFO.id)
+
+}
+*/
