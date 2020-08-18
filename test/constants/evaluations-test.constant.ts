695d36fdfb199a715b4e4da47be70294a090f694
Kyle Fagan <kfagan@mitre.org>
2020-08-18 14:07:59 -0400

    Evaluations e2e tests

--- test/constants/evaluations-test.constant.ts
+++ test/constants/evaluations-test.constant.ts
@@ -1,7 +1,22 @@
 import {CreateEvaluationDto} from '../../src/evaluations/dto/create-evaluation.dto';
-
+import {UpdateEvaluationDto} from '../../src/evaluations/dto/update-evaluation.dto';
+import {CREATE_EVALUATION_TAG_DTO} from './evaluation-tags-test.constant';
 export const EVALUATION_1: CreateEvaluationDto = {
   data: {},
   version: '1.0',
+  evaluationTags: []
+};
+
+export const EVALUATION_WITH_TAGS_1: CreateEvaluationDto = {
+  data: {},
+  version: '1.0',
+  evaluationTags: [CREATE_EVALUATION_TAG_DTO]
+}
+
+export const UPDATE_EVALUATION: UpdateEvaluationDto = {
+  data: {
+    version: '1.0'
+  },
+  version: '1.1',
   evaluationTags: []
 };
