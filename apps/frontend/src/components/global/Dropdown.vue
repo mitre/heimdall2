128ee5a380974ce445b17b64b1b8b1880bccaf43
Camden Moors <cmoors@mitre.org>
2020-09-08 11:23:35 -0400

    Add Help and About dropdown to Heimdall logo

--- apps/frontend/src/components/global/Dropdown.vue
+++ apps/frontend/src/components/global/Dropdown.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center">
-    <v-menu offset-y>
+    <v-menu offset-y :close-on-content-click="false">
       <template v-slot:activator="{on}">
         <div color="none" dark v-on="on">
           <v-avatar size="32px" item>
@@ -12,12 +12,35 @@
         </div>
       </template>
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i">
-          <v-list-item-title @click="showHelp = true">
-            <v-btn color="none" @click="showHelp = true">
-              Open Dialog
-            </v-btn></v-list-item-title
-          ><HelpModal v-show="showHelp"></HelpModal>
+        <v-list-item>
+          <v-list-item-title>
+            <HelpModal>
+              <template v-slot:clickable="{on}">
+                <LinkItem
+                  key="help"
+                  text="Help"
+                  icon="mdi-help-circle"
+                  v-on="on"
+                  >Help</LinkItem
+                >
+              </template>
+            </HelpModal>
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-title>
+            <AboutModal>
+              <template v-slot:clickable="{on}">
+                <LinkItem
+                  key="about"
+                  text="About"
+                  icon="mdi-information"
+                  v-on="on"
+                  >About</LinkItem
+                >
+              </template>
+            </AboutModal>
+          </v-list-item-title>
         </v-list-item>
       </v-list>
     </v-menu>
@@ -27,18 +50,12 @@
 <script lang="ts">
 import {AppInfoModule} from '@/store/app_info';
 import HelpModal from '@/components/global/HelpModal.vue';
+import AboutModal from '@/components/global/AboutModal.vue';
 
 export default {
   components: {
-    HelpModal
-  },
-  data: () => ({
-    showHelp: false,
-    items: [
-      {title: 'Help'},
-      {title: 'About'},
-      {title: 'Version: ' + AppInfoModule.version}
-    ]
-  })
+    HelpModal,
+    AboutModal
+  }
 };
 </script>
