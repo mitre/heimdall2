a8eb911ab2df7db7826b49231f2c85e9d73dfa6d
Kyle Fagan <kfagan@mitre.org>
2020-09-17 10:19:30 -0400

    Fix no unused vars backend

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -21,13 +21,12 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    "unused-imports/no-unused-imports-ts": ["error", {
-      vars: "all",
-      varsIgnorePattern: "^_^",
-      args: "after-used",
-      argsIgnorePattern: "^_"
-    }],
     'no-return-await': 'error',
-    'no-throw-literal': 'error'
+    'no-throw-literal': 'error',
+    "unused-imports/no-unused-imports-ts": "error",
+    "unused-imports/no-unused-vars-ts": [
+			"warn",
+			{ vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" },
+		],
   },
 };
