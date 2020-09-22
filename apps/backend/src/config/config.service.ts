c0327df51582d6d8687e2aa75c7bfdd41dab913a
Kyle Fagan <kfagan@mitre.org>
2020-09-22 18:39:08 -0400

    Parse DATABASE_URL into components for Nest. Allow DATABASE_URL to be provided for .sequelizerc

--- apps/backend/src/config/config.service.ts
+++ apps/backend/src/config/config.service.ts
@@ -2,13 +2,17 @@ import * as dotenv from 'dotenv';
 import * as fs from 'fs';
 
 export class ConfigService {
-  private readonly envConfig: {[key: string]: string};
+  private envConfig: {[key: string]: string};
 
   constructor() {
     console.log('Attempting to read configuration file `.env`!');
     try {
       this.envConfig = dotenv.parse(fs.readFileSync('.env'));
       console.log('Read config!');
+      if (this.parseDatabaseUrl()) {
+        console.log(
+          'DATABASE_URL parsed into smaller components (i.e. DATABASE_USER)'
+        );
     } catch (error) {
       if (error.code === 'ENOENT') {
         // File probably does not exist
@@ -21,6 +25,42 @@ export class ConfigService {
         throw error;
       }
     }
+  }
+
+  private parseDatabaseUrl(): boolean {
+    if (!this.envConfig['DATABASE_URL']) {
+      return false;
+    } else {
+      const url = this.get('DATABASE_URL');
+      const pattern = /^(?:([^:\/?#\s]+):\/{2})?(?:([^@\/?#\s]+)@)?([^\/?#\s]+)?(?:\/([^?#\s]*))?(?:[?]([^#\s]+))?\S*$/;
+      const matches = url.match(pattern);
+
+      this.set(
+        'DATABASE_USERNAME',
+        matches[2] != undefined ? matches[2].split(':')[0] : undefined
+      );
+      this.set(
+        'DATABASE_PASSWORD',
+        matches[2] != undefined ? matches[2].split(':')[1] : undefined
+      );
+      this.set(
+        'DATABASE_HOST',
+        matches[3] != undefined ? matches[3].split(/:(?=\d+$)/)[0] : undefined
+      );
+      this.set(
+        'DATABASE_NAME',
+        matches[4] != undefined ? matches[4].split('/')[0] : undefined
+      );
+      this.set(
+        'DATABASE_PORT',
+        matches[3] != undefined ? matches[3].split(/:(?=\d+$)/)[1] : undefined
+      );
+      return true;
+    }
+  }
+
+  set(key: string, value: string) {
+    this.envConfig[key] = value;
   }
 
   get(key: string): string | undefined {
