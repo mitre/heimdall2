a15704af63a7ff38f2f1e59432d7521b86c99930
Kyle Fagan <kfagan@mitre.org>
2020-10-14 09:42:09 -0400

    Lint interfaces

--- libs/interfaces/evaluation/update-evaluation.interface.ts
+++ libs/interfaces/evaluation/update-evaluation.interface.ts
@@ -7,9 +7,7 @@ import {
 export interface IUpdateEvaluation {
   readonly filename: string | undefined;
   readonly data: Record<string, any> | undefined;
-  readonly evaluationTags: (
-    | IUpdateEvaluationTag
-    | ICreateEvaluationTag
-    | IDeleteEvaluationTag
-  )[] | undefined;
+  readonly evaluationTags:
+    | (IUpdateEvaluationTag | ICreateEvaluationTag | IDeleteEvaluationTag)[]
+    | undefined;
 }
