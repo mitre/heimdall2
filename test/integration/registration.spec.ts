2f3e7a67aaae674fa5ab26d39d09e86a9fcd67cf
Camden Moors <cmoors@mitre.org>
2020-11-23 14:54:38 -0500

    Registration failure test

--- test/integration/registration.spec.ts
+++ test/integration/registration.spec.ts
@@ -3,7 +3,7 @@
 import RegistrationPage from '../support/pages/registration.page';
 import RegistrationVerifier from '../support/verifiers/registration.verifier';
 import ToastVerifier from '../support/verifiers/toast.verifier';
-import {CREATE_USER_DTO_TEST_OBJ} from '../../apps/backend/test/constants/users-test.constant';
+import {CREATE_USER_DTO_TEST_OBJ, CREATE_USER_DTO_TEST_OBJ_WITH_INVALID_PASSWORD} from '../../apps/backend/test/constants/users-test.constant';
 
 context('Registration', () => {
   // Pages, verifiers, and modules
@@ -20,9 +20,13 @@ context('Registration', () => {
   describe('Registration Form', () => {
     it('allows a user to create an account', () => {
       registrationVerifier.registerFormPresent();
-      registrationPage.registerSuccess(CREATE_USER_DTO_TEST_OBJ);
+      registrationPage.register(CREATE_USER_DTO_TEST_OBJ);
       toastVerifier.toastTextContains('You have successfully registered, please sign in')
-      
     });
+
+    it('fails to allow a user to create an account with a weak password', () => {
+      registrationPage.register(CREATE_USER_DTO_TEST_OBJ_WITH_INVALID_PASSWORD);
+      toastVerifier.toastTextContains('Password does not meet complexity requirements. Passwords are a minimum of 15 characters in length. Passwords must contain at least one special character, number, upper-case letter, and lower-case letter. Passwords cannot contain more than three consecutive repeating characters. Passwords cannot contain more than four repeating characters from the same character class.')
+    })
   });
 })
--- test/support/pages/registration.page.ts
+++ test/support/pages/registration.page.ts
@@ -1,22 +1,10 @@
 import {CreateUserDto} from "../../../apps/backend/src/users/dto/create-user.dto";
 
 export default class RegistrationPage {
-  
-  registerSuccess(user: CreateUserDto) {
+  register(user: CreateUserDto) {
     cy.get('input[name=email]').type(user.email);
     cy.get('input[name=password]').type(user.password);
     cy.get('input[name=passwordConfirmation]').type(user.passwordConfirmation);
     cy.get('#register').click();
   }
-
-  // registerFailure() {
-  //   // passwordConfirmation is the field being tested here, therefore it
-  //   // cannot be the last item filled in on the form, otherwise it will
-  //   // still be in focus and the error will never display.
-  //   await expect(page).toFillForm('form[name="signup_form"]', {
-  //     email: user.email,
-  //     passwordConfirmation: user.passwordConfirmation,
-  //     password: user.password
-  //   });
-  // }
 }
