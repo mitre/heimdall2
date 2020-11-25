4d306b67768c9b8d97441719b160bdb01ad7f0fc
Camden Moors <cmoors@mitre.org>
2020-11-25 12:41:05 -0500

    Registration existing email check, mismatch check

--- test/support/pages/registration.page.ts
+++ test/support/pages/registration.page.ts
@@ -7,4 +7,9 @@ export default class RegistrationPage {
     cy.get('input[name=passwordConfirmation]').type(user.passwordConfirmation);
     cy.get('#register').click();
   }
+  registerNoSubmit(user: CreateUserDto) {
+    cy.get('input[name=email]').type(user.email);
+    cy.get('input[name=password]').type(user.password);
+    cy.get('input[name=passwordConfirmation]').type(user.passwordConfirmation);
+  }
 }
--- test/support/verifiers/registration.verifier.ts
+++ test/support/verifiers/registration.verifier.ts
@@ -12,4 +12,8 @@ export default class RegistrationPageVerifier {
       'Confirm Password'
     );
   }
+
+  registerButtonDisabled() {
+    cy.get('#register').should('be.disabled');
+  }
 }
