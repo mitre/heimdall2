eb4f34500976ce6c34143b193a434aab3f9da2c6
Camden Moors <cmoors@mitre.org>
2020-12-02 10:42:58 -0500

    Try running all commands in one line

--- test/support/helpers/DatabaseHelper.ts
+++ test/support/helpers/DatabaseHelper.ts
@@ -2,12 +2,9 @@ export default class DatabaseHelper {
   createAdmin(): string {
     const re = /[\n\r].*New administrator password is:s*([^\n\r]*)/;
     let adminPassword = '';
-    cy.exec('sudo systemctl restart postgresql');
-    cy.exec('sleep 2');
-    cy.exec('yarn backend sequelize-cli db:drop');
-    cy.exec('yarn backend sequelize-cli db:create');
-    cy.exec('yarn backend sequelize-cli db:migrate');
-    cy.exec('yarn backend sequelize-cli db:seed:all').then((result) => {
+    cy.exec(
+      'sudo systemctl restart postgresql; sleep 2; yarn backend sequelize-cli db:drop; sleep 2; yarn backend sequelize-cli db:create; sleep 2; yarn backend sequelize-cli db:migrate; sleep 2; yarn backend sequelize-cli db:seed:all'
+    ).then((result) => {
       adminPassword = re.exec(result.stdout)[1];
     });
     return adminPassword;
