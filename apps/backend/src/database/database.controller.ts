51dcba6a6fabee7ee5aa8b8bcb97c50e5e6d0472
Camden Moors <cmoors@mitre.org>
2020-12-03 10:52:09 -0500

    Update import reference, mark service constructors as readonly

--- apps/backend/src/database/database.controller.ts
+++ apps/backend/src/database/database.controller.ts
@@ -1,16 +1,16 @@
 import {Controller, Post, UseGuards} from '@nestjs/common';
-import {ConfigService} from 'src/config/config.service';
-import {DevelopmentGuard} from 'src/guards/development.guard';
-import {UsersService} from 'src/users/users.service';
+import {ConfigService} from '../config/config.service';
+import {DevelopmentGuard} from '../guards/development.guard';
+import {UsersService} from '../users/users.service';
 import clear from './clear';
 import {DatabaseService} from './database.service';
 
 @Controller('database')
 export class DatabaseController {
   constructor(
-    private usersService: UsersService,
-    private configService: ConfigService,
-    private databaseService: DatabaseService
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+    private readonly databaseService: DatabaseService
   ) {}
 
   @Post('clear')
