a48da78541f1bfbcba1bb52e20c06ebd9397b8d6
Camden Moors <cmoors@mitre.org>
2020-12-03 09:48:36 -0500

    Remove duplicate ESLintrc and lint

--- test/support/helpers/DatabaseHelper.ts
+++ test/support/helpers/DatabaseHelper.ts
@@ -1,5 +1,5 @@
 export default class DatabaseHelper {
   clear(): void {
-    cy.request('http://127.0.0.1:3000/database/clear', { method: 'POST'});
+    cy.request('http://127.0.0.1:3000/database/clear', {method: 'POST'});
   }
 }
