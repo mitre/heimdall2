09adbc2fbfedac4dcf13e9121d2656264d205f08
Kyle Fagan <kfagan@mitre.org>
2021-02-23 14:37:25 -0800

    Add guards to evaluation tag routes

--- apps/backend/src/evaluation-tags/evaluation-tags.controller.spec.ts
+++ apps/backend/src/evaluation-tags/evaluation-tags.controller.spec.ts
@@ -1,33 +1,310 @@
+import {ForbiddenError} from '@casl/ability';
+import {SequelizeModule} from '@nestjs/sequelize';
 import {Test, TestingModule} from '@nestjs/testing';
-import {EVALUATION_TAG_DTO} from '../../test/constants/evaluation-tags-test.constant';
+import {CREATE_EVALUATION_TAG_DTO} from '../../test/constants/evaluation-tags-test.constant';
+import {EVALUATION_1} from '../../test/constants/evaluations-test.constant';
+import {PRIVATE_GROUP} from '../../test/constants/groups-test.constant';
+import {
+  CREATE_USER_DTO_TEST_OBJ,
+  CREATE_USER_DTO_TEST_OBJ_2
+} from '../../test/constants/users-test.constant';
+import {AuthzService} from '../authz/authz.service';
+import {DatabaseModule} from '../database/database.module';
+import {DatabaseService} from '../database/database.service';
+import {Evaluation} from '../evaluations/evaluation.model';
+import {EvaluationsService} from '../evaluations/evaluations.service';
+import {GroupEvaluation} from '../group-evaluations/group-evaluation.model';
+import {GroupUser} from '../group-users/group-user.model';
+import {Group} from '../groups/group.model';
+import {GroupsService} from '../groups/groups.service';
+import {User} from '../users/user.model';
+import {UsersService} from '../users/users.service';
+import {EvaluationTag} from './evaluation-tag.model';
 import {EvaluationTagsController} from './evaluation-tags.controller';
 import {EvaluationTagsService} from './evaluation-tags.service';
 
 describe('EvaluationTagsController', () => {
   let evaluationTagsController: EvaluationTagsController;
+  let evaluationTagsService: EvaluationTagsService;
   let module: TestingModule;
+  let usersService: UsersService;
+  let databaseService: DatabaseService;
+  let evaluationsService: EvaluationsService;
+  let groupsService: GroupsService;
+  let user: User;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
       controllers: [EvaluationTagsController],
+      imports: [
+        DatabaseModule,
+        SequelizeModule.forFeature([
+          Evaluation,
+          EvaluationTag,
+          User,
+          GroupEvaluation,
+          Group,
+          GroupUser
+        ])
+      ],
       providers: [
-        {
-          provide: EvaluationTagsService,
-          useFactory: () => ({
-            findAll: jest.fn(() => [EVALUATION_TAG_DTO])
-          })
-        }
+        AuthzService,
+        DatabaseService,
+        EvaluationTagsService,
+        UsersService,
+        EvaluationsService,
+        GroupsService
       ]
     }).compile();
+
     evaluationTagsController = module.get<EvaluationTagsController>(
       EvaluationTagsController
     );
+    evaluationTagsService = module.get<EvaluationTagsService>(
+      EvaluationTagsService
+    );
+    evaluationsService = module.get<EvaluationsService>(EvaluationsService);
+    usersService = module.get<UsersService>(UsersService);
+    databaseService = module.get<DatabaseService>(DatabaseService);
+    groupsService = module.get<GroupsService>(GroupsService);
+  });
+
+  beforeEach(async () => {
+    await databaseService.cleanAll();
+    user = await usersService.create(CREATE_USER_DTO_TEST_OBJ);
+  });
+
+  afterAll((done) => {
+    databaseService.closeConnection();
+    done();
   });
 
   describe('index', () => {
-    it('should return EvaluationTags', async () => {
-      const evaluationTags = await evaluationTagsController.index();
-      expect(evaluationTags).toContainEqual(EVALUATION_TAG_DTO);
+    it('should return EvaluationTags a User has ownership of', async () => {
+      // Evaluation 1 has no tags so a tag needs to be added manually
+      const evaluation = await evaluationsService.create(EVALUATION_1, user.id);
+      await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+      const foundEvaluationTags = await evaluationTagsController.index({
+        user: user
+      });
+      expect(foundEvaluationTags.length).toEqual(1);
+      expect(foundEvaluationTags[0].value).toEqual(
+        CREATE_EVALUATION_TAG_DTO.value
+      );
+    });
+
+    it('should return EvaluationTags a User has group ownership of', async () => {
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+      const group = await groupsService.create(PRIVATE_GROUP);
+      await groupsService.addUserToGroup(group, user, 'owner');
+      await groupsService.addEvaluationToGroup(group, evaluation);
+      await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+      const foundEvaluationTags = await evaluationTagsController.index({
+        user: user
+      });
+
+      expect(foundEvaluationTags.length).toEqual(1);
+      expect(foundEvaluationTags[0].value).toEqual(
+        CREATE_EVALUATION_TAG_DTO.value
+      );
+    });
+
+    it('should not return EvaluationTags associated with an Evaluation a User not authorized to view', async () => {
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+      await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+      const foundEvaluationTags = await evaluationTagsController.index({
+        user: user
+      });
+      expect(foundEvaluationTags.length).toEqual(0);
+    });
+  });
+
+  describe('findById', () => {
+    it('should return an EvaluationTag a User has ownership of', async () => {
+      const evaluation = await evaluationsService.create(EVALUATION_1, user.id);
+      const evaluationTag = await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+      const foundTag = await evaluationTagsController.findById(
+        evaluationTag.id,
+        {user: user}
+      );
+      expect(foundTag.value).toEqual(CREATE_EVALUATION_TAG_DTO.value);
+    });
+
+    it('should return an EvaluationTags a User has group ownership of', async () => {
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+      const group = await groupsService.create(PRIVATE_GROUP);
+      await groupsService.addUserToGroup(group, user, 'owner');
+      await groupsService.addEvaluationToGroup(group, evaluation);
+      const evaluationTag = await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+      const foundEvaluationTag = await evaluationTagsController.findById(
+        evaluationTag.id,
+        {user: user}
+      );
+
+      expect(foundEvaluationTag.value).toEqual(CREATE_EVALUATION_TAG_DTO.value);
+    });
+
+    it('should not return an EvaluationTags associated with an Evaluation a User not authorized to view', async () => {
+      expect.assertions(1);
+
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+      const evaluationTag = await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+
+      await expect(
+        evaluationTagsController.findById(evaluationTag.id, {user: user})
+      ).rejects.toBeInstanceOf(ForbiddenError);
+    });
+  });
+
+  describe('create', () => {
+    it('should create EvaluationTags on an Evaluation a User has ownership of', async () => {
+      const evaluation = await evaluationsService.create(EVALUATION_1, user.id);
+      const evaluationTag = await evaluationTagsController.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO,
+        {user: user}
+      );
+      expect(evaluationTag).toBeDefined();
+    });
+
+    it('should create EvaluationTags on an Evaluation a User has Group ownership of', async () => {
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+      const group = await groupsService.create(PRIVATE_GROUP);
+      await groupsService.addUserToGroup(group, user, 'owner');
+      await groupsService.addEvaluationToGroup(group, evaluation);
+      const evaluationTag = await evaluationTagsController.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO,
+        {user: user}
+      );
+
+      expect(evaluationTag).toBeDefined();
+    });
+
+    it('should throw ForbiddenError when adding EvaluationTags to an Evaluation a User is not authorized to manage', async () => {
+      expect.assertions(1);
+
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+
+      await expect(
+        evaluationTagsController.create(
+          evaluation.id,
+          CREATE_EVALUATION_TAG_DTO,
+          {user: user}
+        )
+      ).rejects.toBeInstanceOf(ForbiddenError);
+    });
+  });
+
+  describe('remove', () => {
+    it('should remove EvaluationTags from Evaluations a User has ownership of', async () => {
+      const evaluation = await evaluationsService.create(EVALUATION_1, user.id);
+      const evaluationTag = await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+      const removedTag = await evaluationTagsController.remove(
+        evaluationTag.id,
+        {user: user}
+      );
+      expect(removedTag.value).toEqual(CREATE_EVALUATION_TAG_DTO.value);
+    });
+
+    it('should remove EvaluationTags on an Evaluation a User has Group ownership of', async () => {
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+      const group = await groupsService.create(PRIVATE_GROUP);
+      await groupsService.addUserToGroup(group, user, 'owner');
+      await groupsService.addEvaluationToGroup(group, evaluation);
+      const evaluationTag = await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+      const removedTag = await evaluationTagsController.remove(
+        evaluationTag.id,
+        {user: user}
+      );
+
+      expect(removedTag.value).toEqual(CREATE_EVALUATION_TAG_DTO.value);
+    });
+
+    it('should throw ForbiddenError when removing EvaluationTags from an Evaluation a User is not authorized to manage', async () => {
+      expect.assertions(1);
+
+      const evaluationOwner = await usersService.create(
+        CREATE_USER_DTO_TEST_OBJ_2
+      );
+      const evaluation = await evaluationsService.create(
+        EVALUATION_1,
+        evaluationOwner.id
+      );
+      const evaluationTag = await evaluationTagsService.create(
+        evaluation.id,
+        CREATE_EVALUATION_TAG_DTO
+      );
+
+      await expect(
+        evaluationTagsController.remove(evaluationTag.id, {user: user})
+      ).rejects.toBeInstanceOf(ForbiddenError);
     });
   });
 });
--- apps/backend/src/evaluation-tags/evaluation-tags.controller.ts
+++ apps/backend/src/evaluation-tags/evaluation-tags.controller.ts
@@ -1,3 +1,4 @@
+import {ForbiddenError} from '@casl/ability';
 import {
   Body,
   Controller,
@@ -5,9 +6,14 @@ import {
   Get,
   Param,
   Post,
+  Request,
   UseGuards
 } from '@nestjs/common';
+import {AuthzService} from '../authz/authz.service';
+import {Action} from '../casl/casl-ability.factory';
+import {EvaluationsService} from '../evaluations/evaluations.service';
 import {JwtAuthGuard} from '../guards/jwt-auth.guard';
+import {User} from '../users/user.model';
 import {CreateEvaluationTagDto} from './dto/create-evaluation-tag.dto';
 import {EvaluationTagDto} from './dto/evaluation-tag.dto';
 import {EvaluationTagsService} from './evaluation-tags.service';
@@ -15,25 +21,50 @@ import {EvaluationTagsService} from './evaluation-tags.service';
 @Controller('evaluation-tags')
 @UseGuards(JwtAuthGuard)
 export class EvaluationTagsController {
-  constructor(private evaluationTagsService: EvaluationTagsService) {}
+  constructor(
+    private evaluationTagsService: EvaluationTagsService,
+    private evaluationsService: EvaluationsService,
+    private readonly authz: AuthzService
+  ) {}
+
   @Get()
-  async index(): Promise<EvaluationTagDto[]> {
-    const evaluationTags = await this.evaluationTagsService.findAll();
+  async index(@Request() request: {user: User}): Promise<EvaluationTagDto[]> {
+    const abac = this.authz.abac.createForUser(request.user);
+    let evaluationTags = await this.evaluationTagsService.findAll();
+    evaluationTags = evaluationTags.filter((evaluationTag) =>
+      abac.can(Action.Read, evaluationTag.evaluation)
+    );
     return evaluationTags.map(
       (evaluationTag) => new EvaluationTagDto(evaluationTag)
     );
   }
 
   @Get(':id')
-  async findById(@Param('id') id: string): Promise<EvaluationTagDto> {
-    return new EvaluationTagDto(await this.evaluationTagsService.findById(id));
+  async findById(
+    @Param('id') id: string,
+    @Request() request: {user: User}
+  ): Promise<EvaluationTagDto> {
+    const abac = this.authz.abac.createForUser(request.user);
+    const evaluationTag = await this.evaluationTagsService.findById(id);
+    ForbiddenError.from(abac).throwUnlessCan(
+      Action.Read,
+      evaluationTag.evaluation
+    );
+    return new EvaluationTagDto(evaluationTag);
   }
 
   @Post(':evaluationId')
   async create(
     @Param('evaluationId') evaluationId: string,
-    @Body() createEvaluationTagDto: CreateEvaluationTagDto
+    @Body() createEvaluationTagDto: CreateEvaluationTagDto,
+    @Request() request: {user: User}
   ): Promise<EvaluationTagDto> {
+    const abac = this.authz.abac.createForUser(request.user);
+    const evaluation = await this.evaluationsService.findById(evaluationId);
+    // Use Action.Update here because any authenticated user can create an evaluation
+    // and we wouldn't want anyone to be able to add any tag to any evaluation.
+    ForbiddenError.from(abac).throwUnlessCan(Action.Update, evaluation);
+
     return new EvaluationTagDto(
       await this.evaluationTagsService.create(
         evaluationId,
@@ -43,7 +74,16 @@ export class EvaluationTagsController {
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string): Promise<EvaluationTagDto> {
+  async remove(
+    @Param('id') id: string,
+    @Request() request: {user: User}
+  ): Promise<EvaluationTagDto> {
+    const abac = this.authz.abac.createForUser(request.user);
+    const evaluationTag = await this.evaluationTagsService.findById(id);
+    ForbiddenError.from(abac).throwUnlessCan(
+      Action.Delete,
+      evaluationTag.evaluation
+    );
     return new EvaluationTagDto(await this.evaluationTagsService.remove(id));
   }
 }
