764051c314185bf310451784169bd83808c18f78
Robert Clark <rbclark@mitre.org>
2021-03-18 12:06:22 -0400

    Enable no-console eslint rule and fix or make targeted exceptions for all current instances

--- apps/backend/.eslintrc.js
+++ apps/backend/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'no-console': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': ["warn", { "argsIgnorePattern": "^_" }],
