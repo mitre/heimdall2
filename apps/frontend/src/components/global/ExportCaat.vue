bba2d9a514399f809beff25e76567bff4f7e0158
Camden Moors <cmoors@mitre.org>
2021-04-05 13:56:58 -0400

    Refactor ExportCAAT

--- apps/frontend/src/components/global/ExportCaat.vue
+++ apps/frontend/src/components/global/ExportCaat.vue
@@ -14,18 +14,33 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-import {FilteredDataModule, Filter} from '@/store/data_filters';
+import Vue from 'vue'
 import XLSX from 'xlsx';
-import {saveAs} from 'file-saver';
-import {HDFControl, HDFControlSegment} from 'inspecjs';
+import Component from 'vue-class-component';
 import LinkItem from '@/components/global/sidebaritems/IconLinkItem.vue';
+import {HDFControlSegment} from 'inspecjs';
+import {Filter, FilteredDataModule} from '../../store/data_filters';
 import {Prop} from 'vue-property-decorator';
+import {ContextualizedControl} from 'inspecjs/dist/context';
 import _ from 'lodash';
 
-const MAX_CELL_SIZE = 32000; // Rounding a bit here.
-type CAATRow = string[];
+type CAATRow = {
+  [key: string]: string;
+};
+type ContextualizedControlImp = ContextualizedControl & {
+  sourced_from: {
+    from_file: {
+      filename: string;
+      unique_id: string;
+    }
+  }
+}
+type Files = {
+  [key: string]: {
+    filename: string;
+    rows: CAATRow[]
+  }
+}
 
 @Component({
   components: {
@@ -34,197 +49,154 @@ type CAATRow = string[];
 })
 export default class ExportCaat extends Vue {
   @Prop({type: Object, required: true}) readonly filter!: Filter;
-  /** Turns a control into a CAAT row.
-   *  Checks vuln_list first to see if this gid is already included
-   */
-  toRows(control: HDFControl): CAATRow[] {
-    // init rows
-    const allRows: CAATRow[] = [];
 
-    for (let formatted of control.canonized_nist({
+  MAX_CELL_SIZE = 32000;
+
+  header = [
+      'Control Number', 'Finding Title', 'Date Identified', 'Finding ID', 'Information System or Program Name',
+      'Repeat Findings', 'Repeat Finding Weakness ID', 'Finding Description', 'Weakness Description', 'Control Weakness Type',
+      'Source', 'Assessment/Audit Company', 'Test Method', 'Test Objective', 'Test Result Description',
+      'Test Result', 'Recommended Corrective Action(s)', 'Effect on Business', 'Likelihood', 'Impact'
+    ]
+
+  fileSettings = {
+    Title: 'Compliance Assessment/Audit Tracking (CAAT) Spreadsheet',
+    Subject: 'Assessment Data',
+    Author: 'Heimdall',
+    CreatedDate: new Date()
+  }
+
+  getRow(control: ContextualizedControlImp): CAATRow[] {
+    const hdf = control.hdf;
+    const allRows: CAATRow[] = [];
+    for (const formatted of hdf.canonized_nist({
       max_specifiers: 3,
       pad_zeros: true,
       allow_letters: false,
       add_spaces: false
     })) {
       // Establish our row
-      let row: CAATRow = [];
-
       if (!formatted) {
         continue;
       }
+      const row: CAATRow = {};
+      row['Control Number'] = formatted
+      row['Finding Title'] = 'Test ' + this.fix(hdf.wraps.id) + ' - ' + this.fix(hdf.wraps.title)
 
-      // Designate a helper to deal with null/undefined
-      let fix = (x: string | null | undefined) =>
-        (x || '').replace(/(\r\n|\n|\r)/gm, "\r\n").slice(0, MAX_CELL_SIZE);
-
-      // Build up the row
-      row.push(formatted); // Control Number
-      row.push(
-        'Test ' + fix(control.wraps.id) + ' - ' + fix(control.wraps.title)
-      ); // Finding Title
-      if (control.start_time) {
-        row.push(this.convertDate(new Date(control.start_time), '/')); // Date Identified
+      if (hdf.start_time) {
+        row['Date Identified'] = this.convertDate(new Date(hdf.start_time), '/')
       } else {
-        row.push('');
+        row['Date Identified'] = '';
       }
-      row.push(''); //row.push(fix(control.wraps.tags.stig_id)); // Finding ID
-      row.push(''); // Information System or Program Name
-      row.push(''); // Repeat Findings
-      row.push(''); // Repeat Finding CFACTS Weakness ID
-      row.push(fix(control.wraps.title)); // Finding Description
+
+      row['Finding ID'] = '';
+      row['Information System or Program Name'] = '';
+      row['Repeat Findings'] = '';
+      row['Repeat Finding Weakness ID'] = '';
+      row['Finding Description'] = this.fix(hdf.wraps.title)
+
       // Prepend the caveat to the Weakness Description if there is one
-      let caveat = control.descriptions.caveat ? '(Caveat: ' + fix(control.descriptions.caveat) + ')\n' : '';
-      row.push(caveat + fix(control.wraps.desc)); // Weakness Description
-      row.push('Security'); // Control Weakness Type
-      row.push('Self-Assessment '); // Source
-      row.push(''); //row.push("InSpec"); // Assessment/Audit Company
-      row.push('Test'); // Test Method
-      row.push(fix(control.descriptions.check || control.wraps.tags.check)); // Test Objective\
-      let testResult = `${control.status}:\r\n\r\n`;
-      _.get(control, 'wraps.results').forEach((result: HDFControlSegment) => {
+      let caveat = hdf.descriptions.caveat ? '(Caveat: ' + this.fix(hdf.descriptions.caveat) + ')\n' : '';
+      row['Weakness Description'] = caveat + this.fix(hdf.wraps.desc);
+
+      row['Control Weakness Type'] = 'Security';
+      row['Source'] = 'Self-Assessment';
+      row['Assessment/Audit Company'] = '';
+      row['Test Method'] = 'Test';
+      row['Test Objective'] = this.fix(hdf.descriptions.check || hdf.wraps.tags.check)
+
+      // Get the result description of the test
+      let testResult = `${hdf.status}:\r\n\r\n`;
+      _.get(hdf, 'wraps.results').forEach((result: HDFControlSegment) => {
         if(result.message) {
           testResult += `${result.status.toUpperCase()} -- Test: ${result.code_desc}\r\nMessage: ${result.message}\r\n\r\n`
         } else {
           testResult += `${result.status.toUpperCase()} -- Test: ${result.code_desc}\r\n\r\n`
         }
       })
-      row.push(fix(testResult)); // Test Result Description
-      // Test Result
-      if (control.status === 'Passed') {
-        row.push('Satisfied');
-      }
-      else if (_.get(control, 'wraps.results[0].status') === 'skipped'){
-        row.push('Other Than Satisfied');
-      } else {
-        row.push('Other Than Satisfied');
-      }
-      row.push(fix(control.descriptions.fix || control.wraps.tags.fix)); // Recommended Corrective Action(s)
-      row.push(''); // Effect on Business
-      row.push(''); // Likelihood
-      const controlSeverity = control.severity === 'medium' ? 'moderate' : control.severity
-      row.push(fix(control.wraps.impact === 0 ? 'none' : controlSeverity)); // Impact
+      row['Test Result Description'] = this.fix(testResult); // Test Result Description
 
-      if (row.length !== this.header().length) {
-        throw new Error('Row of wrong size');
+      // Test Result
+      if (hdf.status === 'Passed') {
+        row['Test Result'] = 'Satisfied';
+      } else {
+        row['Test Result'] =  'Other Than Satisfied';
       }
+      row['Recommended Corrective Action(s)'] = this.fix(hdf.descriptions.fix || hdf.wraps.tags.fix); // Recommended Corrective Action(s)
+      row['Effect on Business'] = '';
+      row['Likelihood'] = '';
+      const controlSeverity = hdf.severity === 'medium' ? 'moderate' : hdf.severity
+      row['Impact'] = this.fix(hdf.wraps.impact === 0 ? 'none' : controlSeverity); // Impact
+
+      // if (row.length !== ((this.header.length - (this.header.length / 2)))) {
+      //   console.log(row.length)
+      //   console.log(this.header.length)
+      //   throw new Error('Row of wrong size');
+      // }
       allRows.push(row);
     }
     return allRows;
   }
 
-  /** Gets the standardized CAAT header */
-  header(): CAATRow {
-    return [
-      'Control Number',
-      'Finding Title',
-      'Date Identified',
-      'Finding ID',
-      'Information System or Program Name',
-      'Repeat Findings',
-      'Repeat Finding Weakness ID',
-      'Finding Description',
-      'Weakness Description',
-      'Control Weakness Type',
-      'Source',
-      'Assessment/Audit Company',
-      'Test Method',
-      'Test Objective',
-      'Test Result Description',
-      'Test Result',
-      'Recommended Corrective Action(s)',
-      'Effect on Business',
-      'Likelihood',
-      'Impact'
-    ];
-  }
-
   exportCaat() {
-    // Get our data
-    let controls = FilteredDataModule.controls(this.filter);
+    // Create the workbook
+    const wb = XLSX.utils.book_new();
+    wb.Props = this.fileSettings;
+    // Get all of our controls
+    const controls = FilteredDataModule.controls(this.filter);
+    const files: Files = {}
 
-    // Initialize our data structures
-    let caat: CAATRow[] = [this.header()];
-
-    // Turn controls into rows
-    const nonDedupedRows: Array<CAATRow> = [];
-    const hitIds = new Set();
+    // Gets list of files
+    const foundFiles = new Set();
     for (const ctrl of controls) {
-      let root = ctrl.root.hdf;
-      if (hitIds.has(root.wraps.id)) {
+      const root = ctrl.root;
+      const file = (root.sourced_from as unknown as ContextualizedControlImp).sourced_from?.from_file
+      const fileId = file.unique_id
+      if (foundFiles.has(fileId)) {
         continue;
       } else {
-        hitIds.add(root.wraps.id);
-        nonDedupedRows.push(...this.toRows(root));
+        foundFiles.add(fileId);
+        files[fileId] = {
+          'filename': file.filename,
+          'rows': []
+        }
       }
     }
-
-    // Deduplicate controls
-    const hitControls = new Set();
-    let rows = [];
-    for (const r of nonDedupedRows) {
-      let ctrl = r[0];
-      if (!hitControls.has(ctrl)) {
-        hitControls.add(ctrl);
-        rows.push(r);
+    // Find controls for each file
+    Object.keys(files).forEach((fileId: string): void => {
+      wb.SheetNames.push(`${files[fileId].filename} - ${fileId}`.substr(0, 30));
+      const hitIds = new Set();
+      // Get controls from each file
+      for (const ctrl of controls) {
+        const root = (ctrl.root as unknown as ContextualizedControlImp);
+        const ctrlFileId = (root.sourced_from as unknown as ContextualizedControlImp).sourced_from?.from_file.unique_id
+        if (hitIds.has(root.hdf.wraps.id)) {
+          continue;
+        } else {
+          if(ctrlFileId === fileId){
+            hitIds.add(root.hdf.wraps.id);
+            files[fileId].rows.push(...this.getRow(root));
+          }
+        }
       }
-    }
-    // DEBUG
-    rows = nonDedupedRows;
-
-    // Sort them by id
-    rows = rows.sort((a, b) => {
-      // We sort by control (index 0), then by severity within
-      const aFam = a[0];
-      const aImp = a[19];
-      const bFam = b[0];
-      const bImp = b[19];
-      if (aFam !== bFam) {
-        return aFam.localeCompare(bFam);
-      } else {
-        return aImp.localeCompare(bImp);
-      }
+      const ws = XLSX.utils.json_to_sheet(files[fileId].rows, {header: this.header});
+      wb.Sheets[`${files[fileId].filename} - ${fileId}`.substr(0, 30)] = ws;
     });
 
-    // Append to caat
-    caat.push(...rows);
-
-    // Handle XLSX exporting
-    let wb = XLSX.utils.book_new();
-
-    wb.Props = {
-      Title: 'Compliance Assessment/Audit Tracking (CAAT) Spreadsheet',
-      Subject: 'Assessment Data',
-      Author: 'Heimdall',
-      CreatedDate: new Date()
-    };
-
-    wb.SheetNames.push('Assessment Data');
-
-    let ws = XLSX.utils.aoa_to_sheet(caat);
-    wb.Sheets['Assessment Data'] = ws;
-
-    let wbout = XLSX.write(wb, {bookType: 'xlsx', type: 'binary'});
+    const wbout = XLSX.write(wb, {bookType: 'xlsx', type: 'binary'});
     saveAs(
       new Blob([this.s2ab(wbout)], {type: 'application/octet-stream'}),
-      'CAAT-' + this.convertDate(new Date(), '-') + '.xlsx'
+        'CAAT-' + this.convertDate(new Date(), '-') + '.xlsx'
     );
+  }
+
+  fix(x: string | null | undefined) {
+    return (x || '').replace(/(\r\n|\n|\r)/gm, "\r\n").slice(0, this.MAX_CELL_SIZE)
   }
 
   /** Outputs the given number as a 2-digit string. Brittle **/
   padTwoDigits(s: number): string {
     return s < 10 ? `0${s}` : `${s}`;
-  }
-
-  removeTrailingQuotations(input: string): string {
-    let output = input;
-    if(input.endsWith('"')) {
-      output = output.slice(0, -1)
-    }
-    if(input.startsWith('"')) {
-      output = output.slice(1)
-    }
-    return output
   }
 
   convertDate(d: Date, delimiter: string): string {
@@ -237,8 +209,8 @@ export default class ExportCaat extends Vue {
 
   /** Converts a string to an array buffer */
   s2ab(s: string): ArrayBuffer {
-    let buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
-    let view = new Uint8Array(buf); //create uint8array as viewer
+    const buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    const view = new Uint8Array(buf); //create uint8array as viewer
     for (let i = 0; i < s.length; i++) {
       view[i] = s.charCodeAt(i) & 0xff; //convert to octet
     }
