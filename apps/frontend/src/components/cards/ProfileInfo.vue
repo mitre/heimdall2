a63b5092f2e60671aabd166db34c77938d7673d5
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-05-25 14:25:53 -0400

    Add Profile Inputs Tab

--- apps/frontend/src/components/cards/ProfileInfo.vue
+++ apps/frontend/src/components/cards/ProfileInfo.vue
@@ -1,42 +1,74 @@
 <template>
-  <v-scroll-y-transition mode="out-in">
-    <div
-      v-if="!profile"
-      class="title grey--text text--lighten-1 font-weight-light"
-      style="align-self: center"
-    >
-      Select a Profile
-    </div>
-    <v-card v-else :key="profile.id" flat>
-      <v-card-title>
-        <div class="mb-2">{{ profile.data.title }}</div>
-      </v-card-title>
-      <v-divider />
-      <v-row class="text-left pa-4" dense data-cy="profileInfoFields">
-        <v-col cols="12">
-          <div v-if="from_file">
-            <strong>From File:</strong> {{ from_file }}
+  <v-tabs>
+    <v-tab href="#tab-profileInfo"> Profile Info </v-tab>
+    <v-tab href="#tab-inputs"> Profile Inputs </v-tab>
+    <v-tab-item value="tab-profileInfo">
+      <v-scroll-y-transition mode="out-in">
+        <div
+          v-if="!profile"
+          class="title grey--text text--lighten-1 font-weight-light"
+          style="align-self: center"
+        >
+          Select a Profile
+        </div>
+        <v-card v-else :key="profile.id" flat>
+          <v-card-title>
+            <div class="mb-2">{{ profile.data.title }}</div>
+          </v-card-title>
+          <v-divider />
+          <v-row class="text-left pa-4" dense data-cy="profileInfoFields">
+            <v-col cols="12">
+              <div v-if="from_file">
+                <strong>From File:</strong> {{ from_file }}
+              </div>
+              <div v-if="version"><strong>Version:</strong> {{ version }}</div>
+              <div v-if="sha256_hash">
+                <strong>SHA256 Hash:</strong> {{ sha256_hash }}
+              </div>
+              <div v-if="maintainer">
+                <strong>Maintainer:</strong> {{ maintainer }}
+              </div>
+              <div v-if="copyright">
+                <strong>Copyright:</strong> {{ copyright }}
+              </div>
+              <div v-if="copyright_email">
+                <strong>Copyright Email:</strong> {{ copyright_email }}
+              </div>
+              <div v-if="control_count">
+                <strong>Control Count:</strong> {{ control_count }}
+              </div>
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-scroll-y-transition>
+    </v-tab-item>
+    <v-tab-item value="tab-inputs">
+      <v-scroll-y-transition mode="out-in">
+        <div
+          v-if="!profile"
+          class="title grey--text text--lighten-1 font-weight-light"
+          style="align-self: center"
+        >
+          Select a Profile
+        </div>
+        <v-card v-else :key="profile.id" flat>
+          <v-card-title>
+            <div class="mb-2">Inputs for {{ profile.data.title }}</div>
+          </v-card-title>
+          <div v-if="inputs.length !== 0">
+            <v-data-table :headers="headers" :items="inputs"
+              ><template #[`item.options`]="{item}">
+                {{ item.options.value }}
+              </template>
+            </v-data-table>
           </div>
-          <div v-if="version"><strong>Version:</strong> {{ version }}</div>
-          <div v-if="sha256_hash">
-            <strong>SHA256 Hash:</strong> {{ sha256_hash }}
+          <div v-else>
+            <v-card-text> No inputs found. </v-card-text>
           </div>
-          <div v-if="maintainer">
-            <strong>Maintainer:</strong> {{ maintainer }}
-          </div>
-          <div v-if="copyright">
-            <strong>Copyright:</strong> {{ copyright }}
-          </div>
-          <div v-if="copyright_email">
-            <strong>Copyright Email:</strong> {{ copyright_email }}
-          </div>
-          <div v-if="control_count">
-            <strong>Control Count:</strong> {{ control_count }}
-          </div>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-scroll-y-transition>
+        </v-card>
+      </v-scroll-y-transition>
+    </v-tab-item>
+  </v-tabs>
 </template>
 
 <script lang="ts">
@@ -48,6 +80,13 @@ import {Prop} from 'vue-property-decorator';
 import _ from 'lodash';
 import TagRow from '@/components/global/tags/TagRow.vue';
 
+interface Attribute {
+  name?: string;
+  options?: {
+    value?: unknown;
+  }
+}
+
 @Component({
   components: {
     TagRow
@@ -55,6 +94,16 @@ import TagRow from '@/components/global/tags/TagRow.vue';
 })
 export default class ProfileInfo extends Vue {
   @Prop({required: false}) readonly profile: SourcedContextualizedProfile | undefined;
+
+  headers: Object[] = [
+    {
+      text: 'Name',
+      align: 'start',
+      sortable: true,
+      value: 'name'
+    },
+    {text: 'Value', value: 'options', sortable: true}
+  ];
 
   get from_file(): string | undefined {
     return _.get(this.profile, 'sourced_from.from_file.filename');
@@ -82,6 +131,10 @@ export default class ProfileInfo extends Vue {
 
   get control_count(): string | undefined {
     return _.get(this.profile, 'data.controls').length;
+  }
+
+  get inputs(): Attribute[] {
+    return _.get(this.profile, 'data.attributes');
   }
 }
 </script>
