a5e7830ae610f6d32c520a9aa60834bcf89c6b3c
Sam Sunvold <sunvoldsj@vcu.edu>
2021-06-02 12:36:34 -0400

    Setup backend for displaying users in a group
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

---
+++ apps/backend/src/group-users/group-users.service.ts
@@ -0,0 +1,37 @@
+import {
+    Injectable,
+  } from '@nestjs/common';
+  import {InjectModel} from '@nestjs/sequelize';
+  import {User} from '../users/user.model';
+  import {UsersService} from '../users/users.service';
+  import {GroupUser} from './group-user.model';
+
+  @Injectable()
+  export class GroupUsersService {
+    constructor(
+      @InjectModel(GroupUser)
+      private readonly groupUserModel: typeof GroupUser,
+      private readonly usersService: UsersService,
+    ) {}
+
+    async findUsersInGroup(groupId: string): Promise<User[]> {
+      const users = await this.groupUserModel.findAll<GroupUser>({
+          where: {groupId: groupId}
+        });
+
+      const userArray = await this.returnUsersInGroup(users);
+
+      return userArray;
+    }
+
+    async returnUsersInGroup(groupUsersArray: GroupUser[]): Promise<User[]> {
+      let userArray: User[] = [];
+
+      for(let i = 0; i < groupUsersArray.length; i++) {
+        let user: User = await this.usersService.findById(groupUsersArray[i].userId);
+        userArray.push(user);
+      }
+
+      return userArray;
+    }
+  }
