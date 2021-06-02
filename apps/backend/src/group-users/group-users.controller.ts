97e9cd1e49cb68e63443afe97dda10715de99431
Sam Sunvold <sunvoldsj@vcu.edu>
2021-06-02 12:36:34 -0400

    Setup backend for displaying users in a group
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Ran lint
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Fixed SonarCloud errors
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Fixed lint warnings
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>

---
+++ apps/backend/src/group-users/group-users.controller.ts
@@ -0,0 +1,18 @@
+import {Controller, Get, Param, Request, UseGuards} from '@nestjs/common';
+import {JwtAuthGuard} from '../guards/jwt-auth.guard';
+import {User} from '../users/user.model';
+import {GroupUsersService} from './group-users.service';
+
+@Controller('groupusers')
+@UseGuards(JwtAuthGuard)
+export class GroupUsersController {
+  constructor(private readonly groupUsersService: GroupUsersService) {}
+
+  @Get(':id')
+  async findAllGroupUsers(
+    @Request() request: {user: User},
+    @Param('id') groupId: string
+  ): Promise<User[]> {
+    return this.groupUsersService.findUsersInGroup(groupId);
+  }
+}
--- apps/backend/src/group-users/group-users.module.ts
+++ apps/backend/src/group-users/group-users.module.ts
@@ -1,8 +1,15 @@
 import {Module} from '@nestjs/common';
 import {SequelizeModule} from '@nestjs/sequelize';
+import {AuthzModule} from '../authz/authz.module';
+import {UsersModule} from '../users/users.module';
 import {GroupUser} from './group-user.model';
+import {GroupUsersController} from './group-users.controller';
+import {GroupUsersService} from './group-users.service';
 
 @Module({
-  imports: [SequelizeModule.forFeature([GroupUser])]
+  imports: [SequelizeModule.forFeature([GroupUser]), AuthzModule, UsersModule],
+  providers: [GroupUsersService],
+  controllers: [GroupUsersController],
+  exports: [GroupUsersService]
 })
 export class GroupUsersModule {}
