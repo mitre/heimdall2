4c14a1e199db4d216cb697d14cfa557ddb520457
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-06-03 13:53:43 -0400

    Move to Mustache.js, respect filters in HTML export

--- apps/frontend/src/components/global/ExportHTMLModal.vue
+++ apps/frontend/src/components/global/ExportHTMLModal.vue
@@ -50,12 +50,46 @@ import _ from 'lodash';
 import {StatusCountModule} from '../../store/status_counts';
 import {EvaluationFile, ProfileFile} from '../../store/report_intake';
 import {ContextualizedControl} from 'inspecjs/dist/context';
-import {HDFControlSegment} from 'inspecjs';
+import Mustache from 'mustache';
 
 interface Detail {
   name: string;
   value: string;
   class?: string;
+}
+
+interface FileInfo {
+  filename: string;
+  toolVersion: string;
+  platform: string;
+  duration: string;
+}
+
+interface ControlSet {
+  filename: string;
+  fileID: string;
+  controls: (ContextualizedControl & {details: Detail[]})[];
+}
+
+interface Statistics {
+  passed: number;
+  failed: number;
+  notApplicable: number;
+  notReviewed: number;
+  passedTests: number;
+  passingTestsFailedControl: number;
+  failedTests: number;
+  totalTests: number;
+}
+
+interface OutputData {
+  style: string;
+  files: FileInfo[];
+  statistics: Statistics;
+  controlSets: ControlSet[];
+  showControlSets: boolean;
+  showCode: boolean;
+  exportType: string;
 }
 
 @Component({
@@ -71,18 +105,42 @@ export default class ExportHTMLModal extends Vue {
   showingModal = false;
   exportType = 'executive';
   description = 'Profile Info\nStatuses\nCompliance Donuts';
+  outputData: OutputData = {
+    style: '',
+    statistics: {
+      passed: StatusCountModule.countOf(this.filter, 'Passed'),
+      failed: StatusCountModule.countOf(this.filter, 'Failed'),
+      notApplicable: StatusCountModule.countOf(this.filter, 'Not Applicable'),
+      notReviewed: StatusCountModule.countOf(this.filter, 'Not Reviewed'),
+      passedTests: StatusCountModule.countOf(this.filter, 'PassedTests'),
+      passingTestsFailedControl: StatusCountModule.countOf(this.filter, 'PassingTestsFailedControl'),
+      failedTests: StatusCountModule.countOf(this.filter, 'FailedTests'),
+      totalTests: StatusCountModule.countOf(this.filter, 'PassingTestsFailedControl') + StatusCountModule.countOf(this.filter, 'FailedTests')
+    },
+    files: [],
+    controlSets: [],
+    showControlSets: false,
+    showCode: false,
+    exportType: ''
+  };
 
   @Watch('exportType')
   onFileChanged(newValue: string, _oldValue: string) {
     switch (newValue) {
       case 'executive':
         this.description = "Profile Info\nStatuses\nCompliance Donuts";
+        this.outputData.showControlSets = false;
+        this.outputData.showCode = false;
         break;
       case 'manager':
         this.description = "Profile Info\nStatuses\nCompliance Donuts\nTest Results and Details";
+        this.outputData.showControlSets = true;
+        this.outputData.showCode = false;
         break;
       case 'administrator':
         this.description = "Profile Info\nStatuses\nCompliance Donuts\nTest Results and Details\nTest Code";
+        this.outputData.showControlSets = true;
+        this.outputData.showCode = true;
         break
     }
   }
@@ -97,80 +155,33 @@ export default class ExportHTMLModal extends Vue {
     this.showingModal = true;
   }
 
-  /* Creates HTML Element Fragments */
-  createFragment(exportWindow: Window, htmlStr: string): DocumentFragment {
-    var frag = exportWindow.document.createDocumentFragment(),
-        temp = exportWindow.document.createElement('div');
-    temp.innerHTML = htmlStr;
-    while (temp.firstChild) {
-        frag.appendChild(temp.firstChild);
+  addFiledata(file: EvaluationFile | ProfileFile) {
+    this.outputData.files.push({
+      filename: file.filename,
+      toolVersion: _.get(file, 'evaluation.data.version'),
+      platform: _.get(file, 'evaluation.data.platform.name'),
+      duration: _.get(file, 'evaluation.data.statistics.duration')
+    })
+    this.outputData.exportType = _.capitalize(this.exportType)
+    const controls = FilteredDataModule.controls({...this.filter, fromFile: [file.unique_id]});
+    const controlRoots: ContextualizedControl[] = []
+    const hitIds = new Set();
+    // Convert them into rows
+    for (const ctrl of controls) {
+      const root = ctrl.root;
+      if (!hitIds.has(root.hdf.wraps.id)) {
+        hitIds.add(root.hdf.wraps.id);
+        controlRoots.push(root);
+      }
     }
-    return frag;
+    this.outputData.controlSets.push({
+      filename: file.filename,
+      fileID: file.unique_id,
+      controls: controlRoots.map((control) => this.addDetails(control))
+    })
   }
 
-  /* Adds profile info */
-  createResultsSetElement(template: HTMLTemplateElement, file: EvaluationFile | ProfileFile): Node {
-    const templateNode: Node = template.content.cloneNode(true);
-    // Add the filename
-    const fileNameElement = document.createElement("span")
-    fileNameElement.innerHTML = `Filename: <strong>${file.filename}</strong><br />`
-    templateNode.childNodes[1].appendChild(fileNameElement)
-    // This info is only defined if we're exporting a result, not a profile
-    if(this.fileType === 'results'){
-      // Add the tool version
-      const toolVersionElement = document.createElement("span")
-      toolVersionElement.innerHTML = `Tool Version: <strong>${_.get(file, 'evaluation.data.version')}</strong><br />`
-      templateNode.childNodes[1].appendChild(toolVersionElement)
-      // Add the run platform
-      const platformElement = document.createElement("span")
-      platformElement.innerHTML = `Platform: <strong>${_.get(file, 'evaluation.data.platform.name') + _.get(file, 'evaluation.data.platform.release')}</strong><br />`
-      templateNode.childNodes[1].appendChild(platformElement)
-      // Add the run duration
-      const durationElement = document.createElement("span")
-      durationElement.innerHTML = `Duration: <strong>${_.get(file, 'evaluation.data.statistics.duration')}</strong>`
-      templateNode.childNodes[1].appendChild(durationElement)
-    }
-    return templateNode
-  }
-
-  /* Adds status count cards */
-  createStatusCardsTemplate(exportWindow: Window): void {
-    const passedCountElement = exportWindow.document.getElementById('passedCount')
-    const passedCountSubtitleElement = exportWindow.document.getElementById('passedCountSubtitle')
-    const failedCountElement = exportWindow.document.getElementById('failedCount')
-    const failedCountSubtitleElement = exportWindow.document.getElementById('failedCountSubtitle')
-    const naCountElement = exportWindow.document.getElementById('naCount')
-    const nrCountElement = exportWindow.document.getElementById('nrCount')
-    if(passedCountElement && passedCountSubtitleElement){
-      passedCountElement.textContent = `Passed: ${StatusCountModule.countOf(this.filter, 'Passed')}`
-      passedCountSubtitleElement.textContent = `${StatusCountModule.countOf(this.filter, 'PassedTests')} individual checks passed`
-    }
-    if (failedCountElement && failedCountSubtitleElement) {
-      failedCountElement.textContent = `Failed: ${StatusCountModule.countOf(this.filter, 'Failed')}`
-      failedCountSubtitleElement.textContent = `${StatusCountModule.countOf(
-          this.filter,
-          'PassingTestsFailedControl'
-        )} individual checks passed, ${StatusCountModule.countOf(
-          this.filter,
-          'FailedTests'
-        )} failed out of ${StatusCountModule.countOf(
-          this.filter,
-          'PassingTestsFailedControl'
-        ) + StatusCountModule.countOf(
-          this.filter,
-          'FailedTests'
-        )} total checks`
-    }
-    if (naCountElement) {
-      naCountElement.textContent = `Not Applicable: ${StatusCountModule.countOf(this.filter, 'Not Applicable')}`
-    }
-    if (nrCountElement) {
-      nrCountElement.textContent = `Not Reviewed: ${StatusCountModule.countOf(this.filter, 'Not Reviewed')}`
-    }
-  }
-
-  /* Copies the complianceCards from the results view */
-  copyComplianceCards(exportWindow: Window) {
+   copyComplianceCards(exportWindow: Window) {
     // Grab the html from Heimdall, and then grab the container that the html will be put into
     const statusCardHTML = document.getElementById('statusCounts')?.innerHTML
     const statusCountCardContainer = exportWindow.document.getElementById('statusCountCard')
@@ -192,121 +203,8 @@ export default class ExportHTMLModal extends Vue {
     }
   }
 
-  /* Creates a table row row for the control results table */
-  createControlResultRow(result: HDFControlSegment): HTMLTableRowElement {
-    const maxWidth = "50px" // This max width isn't actually represented in the final version, but it is needed to enforce line breaking
-    const wordWrap = "break-word";
-    const controlTestResultElement = document.createElement("tr")
-    // Create TD elements for each status, test description, and result
-    const controlStatusTableDataElement = document.createElement("td");
-    const controlStatusTableNode = document.createTextNode(result.status);
-    const controlTestTableDataElement = document.createElement("td");
-    const controlTestTableNode = document.createTextNode(result.code_desc);
-    const controlResultTableDataElement = document.createElement("td");
-    const controlResultTableNode = document.createTextNode((result.message || result.skip_message || ''))
-    // Append the text nodes to their respective TR
-    controlStatusTableDataElement.appendChild(controlStatusTableNode)
-    controlTestTableDataElement.appendChild(controlTestTableNode)
-    controlResultTableDataElement.appendChild(controlResultTableNode)
-    // Apply a css fix to ensure long strings don't overlap other text
-    controlStatusTableDataElement.style.maxWidth = maxWidth;
-    controlStatusTableDataElement.style.wordWrap = wordWrap;
-    controlTestTableDataElement.style.maxWidth = maxWidth;
-    controlTestTableDataElement.style.wordWrap = wordWrap;
-    controlResultTableDataElement.style.maxWidth = maxWidth;
-    controlResultTableDataElement.style.wordWrap = wordWrap;
-    // Put each TD inside the TR
-    controlTestResultElement.appendChild(controlStatusTableDataElement)
-    controlTestResultElement.appendChild(controlTestTableDataElement)
-    controlTestResultElement.appendChild(controlResultTableDataElement)
-    return controlTestResultElement
-  }
-
-  createDetailRow(detail: Detail): HTMLTableRowElement {
-    const maxWidth = "50px" // This max width isn't actually represented in the final version, but it is needed to enforce line breaking
-    const wordWrap = "break-word";
-    // Create a table row for each detail
-    const detailRow = document.createElement("tr")
-    // Create the detail name column
-    const detailNameTableDataElement = document.createElement("td");
-    const detailNameTableNode = document.createTextNode(detail.name)
-    detailNameTableDataElement.style.maxWidth = maxWidth;
-    detailNameTableDataElement.style.wordWrap = wordWrap;
-    detailNameTableDataElement.appendChild(detailNameTableNode);
-    // Create the detail value column
-    const detailValueTableDataElement = document.createElement("td")
-    const detailValueTableNode = document.createTextNode(detail.value)
-    detailValueTableDataElement.style.maxWidth = maxWidth;
-    detailValueTableDataElement.style.wordWrap = wordWrap;
-    detailValueTableDataElement.appendChild(detailValueTableNode)
-    // Put the columns into the table row
-    detailRow.appendChild(detailNameTableDataElement)
-    detailRow.appendChild(detailValueTableDataElement)
-    return detailRow
-  }
-
-  /* Adds control sets and controls */
-  createControlSet(controlSetTemplate: HTMLTemplateElement, controlTemplate: HTMLTemplateElement, file: EvaluationFile | ProfileFile): Node {
-    const templateNode: Node = controlSetTemplate.content.cloneNode(true);
-    const templateHTML = controlSetTemplate.innerHTML
-    const controlSetElement = document.createElement("div")
-    templateNode.textContent = ""
-    controlSetElement.innerHTML = templateHTML.replace(/{{ filename }}/g, file.filename);
-    controlSetElement.innerHTML = controlSetElement.innerHTML.replace(/{{ fileID }}/g, file.unique_id);
-    templateNode.appendChild(controlSetElement)
-
-    const controls = FilteredDataModule.controls({fromFile: [file.unique_id]});
-    const hitIds = new Set();
-    // Convert them into rows
-    for (const ctrl of controls) {
-      const root = ctrl.root;
-      if (!hitIds.has(root.hdf.wraps.id)) {
-        hitIds.add(root.hdf.wraps.id);
-        const controlTemplateNode = controlTemplate.content.cloneNode();
-        const controlTemplateHTML = controlTemplate.innerHTML;
-        const controlElement = document.createElement("div")
-        controlTemplateNode.textContent = "";
-        const controlresults = root.hdf.segments;
-
-        // Replace the template strings with the actual data
-        controlElement.innerHTML = controlTemplateHTML
-            .replace('{{ controlId }}', root.hdf.wraps.id)
-            .replace(/{{ controlTitle }}/g, root.hdf.wraps.title || '')
-            .replace(/{{ controlDescription }}/g, (root.data.desc || 'No description').trim())
-        let deleteControlTestResultTable = false;
-        if(this.fileType === 'results') {
-          controlresults?.forEach((result) => {
-            // Create a table row for each result
-            const controlTableResultRow = this.createControlResultRow(result)
-            // Append the new row to to the control test result table
-            controlElement.childNodes[1].childNodes[3].childNodes[1].parentElement?.querySelector("#testResultsTable")?.appendChild(controlTableResultRow)
-          })
-        } else {
-          // Handle controls without any results (e.g exporting a profile or a run error)
-          // We just delete the table to give more room for the details table
-          deleteControlTestResultTable = true;
-        }
-        this.getDetails(root).forEach((detail) => {
-          // Create the HTML row for the detail
-          const detailRow = this.createDetailRow(detail)
-          // Append that to the details table
-          controlElement.childNodes[1].childNodes[3].childNodes[1].parentElement?.querySelector('#controlDetailsTable')?.appendChild(detailRow)
-        })
-        // Add control code if were are making an administrator report
-        const controlCode = this.exportType === 'administrator' ? root.full_code : ''
-        controlElement.innerHTML = controlElement.innerHTML.replace(/{{ code }}/g, controlCode)
-        // Once we've added all our details, we can remove the control test results table if determined earlier
-        if(deleteControlTestResultTable) {
-          controlElement.childNodes[1].childNodes[3].childNodes[1].childNodes[1].childNodes[1].remove()
-        }
-        templateNode.childNodes[0].childNodes[1].childNodes[1].childNodes[3].appendChild(controlElement)
-      }
-    }
-    return templateNode
-  }
-
-  getDetails(control: ContextualizedControl): Detail[] {
-    return [
+    addDetails(control: ContextualizedControl): ContextualizedControl & {details: Detail[]} {
+      return {...control, full_code: control.full_code, details: [
       {
         name: 'Control',
         value: control.data.id
@@ -351,7 +249,7 @@ export default class ExportHTMLModal extends Vue {
         name: 'Fix Text',
         value: control.hdf.descriptions.fix || control.data.tags.fix
       }
-    ].filter((v) => v.value); // Get rid of nulls
+    ].filter((v) => v.value)}
   }
 
   exportHTML(): void {
@@ -359,54 +257,30 @@ export default class ExportHTMLModal extends Vue {
       return SnackbarModule.failure('No files have been loaded.')
     }
     // Download the template
-    axios.get(`/static/export/${this.exportType}.html`).then(({data}) => {
-      // Create a new window where we will build the template
-      const exportWindow = window.open('', '_blank');
-      exportWindow?.focus()
-      // If we successfully created the window
-      if (exportWindow) {
-        // Write the initial template
-        exportWindow.document.write(data);
-        // Add the bootstrap framework
-        axios.get(`/static/export/style.css`).then((response) => {
-          const modifiedStyle = response.data
-            .replace(/\#dc3545/g, "#f34335") // bg-danger
-            .replace(/\#198754/g, "#4cb04f") // bg-success
-            .replace(/\#0dcaf0/g, "#03a9f4") // bg-info
-            .replace(/\#ffc107/g, "#fe9900") // bg-warning
+    axios.get(`/static/export/template.html`).then(({data}) => {
+      this.filter.fromFile.forEach(async (fileId) => {
+        const file = InspecDataModule.allFiles.find(
+          (f) => f.unique_id === fileId
+        );
+        if (file) {
+          this.addFiledata(file)
+        }
+      })
+      axios.get(`/static/export/style.css`).then((response) => {
+        const modifiedStyle = response.data
+          .replace(/\#dc3545/g, "#f34335") // bg-danger
+          .replace(/\#198754/g, "#4cb04f") // bg-success
+          .replace(/\#0dcaf0/g, "#03a9f4") // bg-info
+          .replace(/\#ffc107/g, "#fe9900") // bg-warning
+        const exportWindow = window.open('', '_blank');
+        if(exportWindow) {
+          exportWindow.document.write(Mustache.render(data, this.outputData))
           exportWindow.document.head.insertAdjacentHTML("beforeend", `<style>${modifiedStyle}</style>`)
-        })
-        // Get the template for results sets
-        const profileInfos = exportWindow.document.getElementById('profileInfos');
-        const profileInfoTemplate = exportWindow.document.getElementsByTagName('template')[0];
-        // Get the template for the control set accordion and control sets
-        const resultsSetContainer = exportWindow.document.getElementById('resultSetsContainer');
-        const resultsSetAccordionTemplate = exportWindow.document.getElementsByTagName('template')[1];
-        const controlTemplate = exportWindow.document.getElementsByTagName('template')[2];
-        let copiedComplianceCards = false;
-
-        // For each result set, add to
-        this.filter.fromFile.forEach(async (fileId) => {
-          const file = InspecDataModule.allFiles.find(
-            (f) => f.unique_id === fileId
-          );
-          if (file) {
-            profileInfos?.appendChild(this.createResultsSetElement(profileInfoTemplate, file));
-            this.createStatusCardsTemplate(exportWindow)
-            // Only copy compliance cards once
-            if(!copiedComplianceCards) {
-              this.copyComplianceCards(exportWindow)
-              copiedComplianceCards = true
-            }
-            resultsSetContainer?.appendChild(this.createControlSet(resultsSetAccordionTemplate, controlTemplate, file))
-          }
-        })
-        exportWindow.document.close();
-        exportWindow.focus();
-      } // User has popups disabled
-      else {
-        SnackbarModule.failure('Please allow pop-up tabs to export to HTML.')
-      }
+          this.copyComplianceCards(exportWindow)
+        } else {
+          SnackbarModule.failure('Please allow pop-up tabs to export to HTML.')
+        }
+      })
     })
     this.closeModal()
   }
