db29ca556b1f14e51264995db5b0fef1b7d10663
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-06-22 12:38:09 -0400

    Remove password force click, try clearing password input before typing

--- test/support/pages/RegistrationPage.ts
+++ test/support/pages/RegistrationPage.ts
@@ -3,17 +3,14 @@ import {CreateUserDto} from '../../../apps/backend/src/users/dto/create-user.dto
 export default class RegistrationPage {
   register(user: CreateUserDto): void {
     this.registerNoSubmit(user);
-    /*
-     * We have to force click the button here since the password complexity pipe hasn't always returned yet
-     */
-    cy.get('#register').click({force: true});
+    cy.get('#register').click();
   }
   registerNoSubmit(user: CreateUserDto): void {
     cy.visit('/signup');
     cy.get('input[name=firstName]').type(user.firstName);
     cy.get('input[name=lastName]').type(user.lastName);
     cy.get('input[name=email]').type(user.email);
-    cy.get('input[name=password]').type(user.password);
+    cy.get('input[name=password]').clear().type(user.password);
     cy.get('input[name=passwordConfirmation]').type(user.passwordConfirmation);
   }
 }
