f163c6452c8eb7e4c7343fba2394bf81176c1a8b
Kyle Fagan <kfagan@mitre.org>
2021-06-28 14:06:23 -0700

    AwsConfigMapping class

---
+++ libs/heimdall_tools/src/mappings/AwsConfigMapping.ts
@@ -0,0 +1,18 @@
+import parse from 'csv-parse/';
+import fs from 'fs';
+
+class AwsConfigMapping {
+  data: AwsConfigMappingItem[];
+
+  constructor(csvDataPath: string) {
+    let data = new Array()
+    parse(fs.readFileSync(csvDataPath, {encoding: 'utf-8'}), {skip_empty_lines: true}, function(err, line) {
+      if(!err ===undefined) {
+        throw err
+      } else {
+        data.push(new AwsConfigMappingItem(line))
+      }
+    })
+    this.data = data;
+  }
+}
