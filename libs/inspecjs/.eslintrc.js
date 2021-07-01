a08ad4f3efcf88d1475a6f5926583d158852e78e
Robert Clark <rbclark@mitre.org>
2021-07-01 16:53:04 -0400

    Cleanup eslint config and remove duplicated packages from package.json

--- libs/inspecjs/.eslintrc.js
+++ libs/inspecjs/.eslintrc.js
@@ -6,8 +6,9 @@ module.exports =  {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends:  [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'prettier',
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules:  {
