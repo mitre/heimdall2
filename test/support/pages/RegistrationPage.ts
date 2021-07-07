fefc2aab82ea38a029a8cf28bc56342e80362588
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-07-07 10:30:03 -0400

    Wait for checkPasswordComplexity request to complete

--- test/support/pages/RegistrationPage.ts
+++ test/support/pages/RegistrationPage.ts
@@ -3,15 +3,20 @@ import {CreateUserDto} from '../../../apps/backend/src/users/dto/create-user.dto
 export default class RegistrationPage {
   register(user: CreateUserDto): void {
     this.registerNoSubmit(user);
-    cy.get('#register', {timeout: 1000}).should('not.be.disabled');
-    cy.get('#register').click();
+    cy.get('#register').click({force: true});
   }
   registerNoSubmit(user: CreateUserDto): void {
     cy.visit('/signup');
     cy.get('input[name=firstName]').type(user.firstName);
     cy.get('input[name=lastName]').type(user.lastName);
     cy.get('input[name=email]').type(user.email);
+    cy.intercept('POST', '*/check-password-complexity').as(
+      'checkPasswordComplexity'
+    );
     cy.get('input[name=password]').type(user.password);
+    cy.wait('@checkPasswordComplexity')
+      .its('response.statusCode')
+      .should('equal', 201);
     cy.get('input[name=passwordConfirmation]').type(user.passwordConfirmation);
   }
 }
