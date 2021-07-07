efbaee6d5ea47cbb84867bcb22dcd260929789bb
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-07-07 09:58:01 -0400

    Match whitespace and parenthesis when cleaning NIST ID

--- apps/frontend/src/components/cards/treemap/Treemap.vue
+++ apps/frontend/src/components/cards/treemap/Treemap.vue
@@ -94,7 +94,7 @@ export default class Treemap extends Vue {
     contextualizedControls.forEach((cc) => {
       for (const tag of cc.root.hdf.parsed_nist_tags) {
         // Changes if we're showing the sub-category, e.g for "AC-4 (a)": showSubcategory ? 'AC-4' : 'AC'
-        const nistID = showSubcategory ? tag.raw_text?.substring(0, 5).replace(/\s/, '') : tag.raw_text?.substring(0, 2)
+        const nistID = showSubcategory ? tag.raw_text?.substring(0, 5).replace(/\s|\(|\)/, '') : tag.raw_text?.substring(0, 2)
         if(exactMatch && nistID?.indexOf(exactMatch) === -1) {
           continue;
         }
