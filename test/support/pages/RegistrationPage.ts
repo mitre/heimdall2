da68e360ea3dede8de80c38d1f6eac72a884e499
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-07-07 12:43:09 -0400

    Wait until #register is not disabled

--- test/support/pages/RegistrationPage.ts
+++ test/support/pages/RegistrationPage.ts
@@ -3,20 +3,15 @@ import {CreateUserDto} from '../../../apps/backend/src/users/dto/create-user.dto
 export default class RegistrationPage {
   register(user: CreateUserDto): void {
     this.registerNoSubmit(user);
-    cy.get('#register').click({force: true});
+    cy.waitUntil(() => cy.get('#register').should('not.be.disabled'));
+    cy.get('#register').click();
   }
   registerNoSubmit(user: CreateUserDto): void {
     cy.visit('/signup');
     cy.get('input[name=firstName]').type(user.firstName);
     cy.get('input[name=lastName]').type(user.lastName);
     cy.get('input[name=email]').type(user.email);
-    cy.intercept('POST', '*/check-password-complexity').as(
-      'checkPasswordComplexity'
-    );
     cy.get('input[name=password]').type(user.password);
-    cy.wait('@checkPasswordComplexity')
-      .its('response.statusCode')
-      .should('equal', 201);
     cy.get('input[name=passwordConfirmation]').type(user.passwordConfirmation);
   }
 }
