8274fc35892edae45e12724beb36f165c94835e3
Sam Sunvold <60075714+samuelsunvold@users.noreply.github.com>
2021-07-12 19:04:16 -0400

    Display group users on 'My Groups' page (#1436)
    * Setup backend for displaying users in a group
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Ran lint
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Fixed SonarCloud errors
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Fixed lint warnings
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    * Removed unnecessary backend controller/service I previously added and updated the display members UI
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    * Hide total members chip if all members have a chip showing already
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Ran lint
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Fixed Sonar errors
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    * PR changes
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Fixed Sonar issue
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Removed unused function
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    * PR changes
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    * Created new component that displays users in a group
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Changed class name/removed apps/backend/dist
    
    Signed-off-by: Sam Sunvold <sunvoldsj@vcu.edu>
    
    Co-authored-by: Robert Clark <rbclark@mitre.org>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ apps/frontend/src/components/global/groups/GroupUsers.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-dialog v-model="dialogDisplayUsers" max-width="700px">
+    <v-card class="rounded-t-0">
+      <v-card-title
+        data-cy="groupModalTitle"
+        class="headline mitreSecondaryBlue"
+        primary-title
+        >{{ 'Members' }}</v-card-title
+      >
+      <v-card-text>
+        <Users v-model="selectedGroupUsers" :editable="false" />
+      </v-card-text>
+      <v-card-actions>
+        <v-col class="text-right">
+          <v-btn color="primary" text @click="dialogDisplayUsers = false"
+            >Close</v-btn
+          >
+        </v-col>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import {ISlimUser} from '@heimdall/interfaces';
+import {Prop, VModel} from 'vue-property-decorator';
+import Users from '@/components/global/groups/Users.vue';
+
+@Component({
+  validations: {},
+  components: {
+    Users
+  }
+})
+export default class GroupUsers extends Vue {
+  @VModel({type: Array, required: true}) selectedGroupUsers!: ISlimUser[];
+  @Prop({type: Boolean, default: false}) readonly dialogDisplayUsers!: boolean;
+
+}
+</script>
