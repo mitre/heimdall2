2f6b54c2c04c50834614080f68d6b0b4e8939255
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-07-22 15:30:10 -0400

    Log Database Queries

--- apps/backend/src/database/database.module.ts
+++ apps/backend/src/database/database.module.ts
@@ -1,8 +1,19 @@
 import {Module} from '@nestjs/common';
 import {SequelizeModule} from '@nestjs/sequelize';
+import winston from 'winston';
 import {ConfigModule} from '../config/config.module';
 import {ConfigService} from '../config/config.service';
 import {DatabaseService} from './database.service';
+
+const logger = winston.createLogger({
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'MMM-DD-YYYY HH:mm:ss Z'
+    }),
+    winston.format.printf((info) => `[${[info.timestamp]}] ${info.message}`)
+  )
+});
 
 function getSynchronize(configService: ConfigService): boolean {
   const nodeEnvironment = configService.get('NODE_ENV');
@@ -10,6 +21,20 @@ function getSynchronize(configService: ConfigService): boolean {
     throw new TypeError('NODE_ENV is not set and must be provided.');
   } else {
     return nodeEnvironment === 'test' ? false : true;
+  }
+}
+
+function logQuery(sql: string, connection: any) {
+  if (connection.type === 'INSERT' || connection.type === 'UPDATE') {
+    const matches = sql.match(/\$\d/gm);
+    if (matches) {
+      matches.forEach((placeholder, index) => {
+        sql = sql.replace(placeholder, `"${connection.bind[index]}"`);
+      });
+    }
+    logger.info({message: sql});
+  } else {
+    logger.info({message: sql});
   }
 }
 
@@ -22,7 +47,9 @@ function getSynchronize(configService: ConfigService): boolean {
         ...configService.getDbConfig(),
         autoLoadModels: true,
         synchronize: getSynchronize(configService),
-        logging: false,
+        logging: (sql, connection) => {
+          logQuery(sql, connection);
+        },
         pool: {
           max: 5,
           min: 0,
