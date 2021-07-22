f16f74089bb4f85a56b32aa2cf3033fb99412fca
sesheikholeslam <sheikholeslam@mitre.org>
2021-07-22 09:21:40 -0400

    Moves the inspec schema generation to a quicktype shell script file

--- libs/inspecjs/scripts/update_schema.sh
+++ libs/inspecjs/scripts/update_schema.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # This script (will) automatically updates the parsers in "generated-parsers" based on the current inspec schema output.
-
 # Show every line as it executes
 # set -x
 # Halt immediately on error
 set -e
-
 # Ensure we were given an argument
 # [[ -n $1 ]] || { echo "Must supply a version number" >&2; exit 1; }
 # set VERSION=$1
@@ -13,19 +11,16 @@ VERSION="v_1_0"
 
 # Declare our schemas - those that we will ask inspec to generate
 declare -a SCHEMAS=('exec-json' 'exec-jsonmin' 'profile-json')
-
 # Make the schemas for each version we want
-mkdir -p './work/schemas'
+# mkdir -p './work/schemas'
 for SCHEMA in ${SCHEMAS[@]}
 do
     echo Generating $SCHEMA
-    echo '(TEMPORARILY DISABLED)'
-    # inspec schema $SCHEMA > work/schemas/$VERSION/$SCHEMA.json;
+    # echo '(TEMPORARILY DISABLED)'
+    inspec schema $SCHEMA > schemas/$SCHEMA.json --chef-license=accept-silent
     # We just copy
-    cp -R './schemas/' './work/schemas/'
-
+    # cp -R './schemas/' './work/schemas/'
 done
-
 # Quicktype each
 echo "Generating types"
 mkdir -p "./work/interfaces"
@@ -34,18 +29,16 @@ for SCHEMA in ${SCHEMAS[@]}
 do
     # Establish filenames
     OUTFILE="./src/generated_parsers/$VERSION/$SCHEMA.ts"
-    SCHEMAFILE="./work/schemas/$SCHEMA.json"
+    SCHEMAFILE="./schemas/$SCHEMA.json"
     MODIFIED_SCHEMAFILE="./work/tmp_$SCHEMA.json"
 
     # Loosen the schema
     COMPAT="./scripts/schema_compat_patches"
     cat $SCHEMAFILE | . "$COMPAT/generic.sh" | . "$COMPAT/$SCHEMA.sh" | . "./scripts/null_compat_schema.sh" > $MODIFIED_SCHEMAFILE
-
     # Generate the parser
     npx quicktype -l ts -s schema --src $MODIFIED_SCHEMAFILE -o $OUTFILE --runtime-typecheck
-    # rm $MODIFIED_SCHEMAFILE
+    rm $MODIFIED_SCHEMAFILE
 done
-
 # Remove work directory trash
-rm -r './work/interfaces'
+rm -r './work'
 # rm -r './work/schemas'
