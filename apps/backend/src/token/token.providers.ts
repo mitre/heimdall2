b06ade1ac2cd05fbe6b0ae7784aa929f1738e956
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2021-08-09 16:45:12 -0400

    Limit JWT Expire Time to Two Days (#1759)
    * Limit JWT Expire Time to Two Days
    
    * Use ms to calculate maximum time, log a message if expire time has been limited
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- apps/backend/src/token/token.providers.ts
+++ apps/backend/src/token/token.providers.ts
@@ -1,10 +1,23 @@
 import {JwtModule} from '@nestjs/jwt';
 import * as crypto from 'crypto';
+import ms from 'ms';
 import {ConfigModule} from '../config/config.module';
 import {ConfigService} from '../config/config.service';
 
 export function generateDefault(): string {
   return crypto.randomBytes(64).toString('hex');
+}
+
+function limitJWTTime(time: string) {
+  const timeMs = ms(time);
+  const maxDays = ms('2d'); // limit to two days
+  if (timeMs > maxDays) {
+    // eslint-disable-next-line no-console
+    console.log('JWT Expire time has been limited to two days maximum.');
+    return maxDays;
+  } else {
+    return timeMs;
+  }
 }
 
 export const tokenProviders = [
@@ -14,7 +27,7 @@ export const tokenProviders = [
     useFactory: (configService: ConfigService) => ({
       secret: configService.get('JWT_SECRET') || generateDefault(),
       signOptions: {
-        expiresIn: configService.get('JWT_EXPIRE_TIME') || '60s'
+        expiresIn: limitJWTTime(configService.get('JWT_EXPIRE_TIME') || '60s')
       }
     })
   })
