b7914a1f746c30b66c7213222580376bb48dc0ad
Camden Moors <cmoors@mitre.org>
2021-10-14 22:10:22 -0400

    Enable Secure Cookie on OAuth Authentication Exception

--- apps/backend/src/filters/authentication-exception.filter.ts
+++ apps/backend/src/filters/authentication-exception.filter.ts
@@ -1,11 +1,15 @@
 import {ArgumentsHost, Catch, ExceptionFilter} from '@nestjs/common';
+import {ConfigService} from '../config/config.service';
 
 @Catch(Error)
 export class AuthenticationExceptionFilter implements ExceptionFilter {
+  configService = new ConfigService();
   catch(exception: Error, host: ArgumentsHost): void {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse();
-    response.cookie('authenticationError', exception.message);
+    response.cookie('authenticationError', exception.message, {
+      secure: this.configService.isInProductionMode()
+    });
     response.redirect(301, '/');
   }
 }
