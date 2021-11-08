eacfcd0fddfd8220d68c5f27ce2af23af80e3928
sesheikholeslam <sheikholeslam@mitre.org>
2021-11-07 21:14:45 -0500

    Resolve SonarCloud issue

--- test/support/verifiers/ToastVerifier.ts
+++ test/support/verifiers/ToastVerifier.ts
@@ -1,8 +1,8 @@
 export default class ToastVerifier {
   readonly snackBar = 'div[class="v-snack__content"]';
   toastTextContains(textToCheck: string): void {
-    cy.get('div[class="v-snack__content"]', {timeout: 6000}).should('exist');
-    cy.get('div[class="v-snack__content"]').should('contain', textToCheck);
+    cy.get(this.snackBar, {timeout: 6000}).should('exist');
+    cy.get(this.snackBar).should('contain', textToCheck);
   }
 
   toastTextNotContains(): void {
