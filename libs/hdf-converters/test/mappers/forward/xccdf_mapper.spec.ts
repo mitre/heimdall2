cce348c28fef4ccc0263d0063646d278f6a3e15b
Amndeep Singh Mann <amann@mitre.org>
2021-12-23 02:05:39 -0500

    added more tests based off of new samples
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/hdf-converters/test/mappers/forward/xccdf_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/xccdf_mapper.spec.ts
@@ -3,40 +3,80 @@ import {XCCDFResultsMapper} from '../../../src/xccdf-results-mapper';
 import {omitVersions} from '../../utils';
 
 describe('xccdf_mapper', () => {
-  it('SCC', () => {
-    const mapper = new XCCDFResultsMapper(
-      fs.readFileSync(
-        'sample_jsons/xccdf_results_mapper/sample_input_report/xccdf-results-scc-rhel7.xml',
-        {encoding: 'utf-8'}
-      )
-    );
-    expect(omitVersions(mapper.toHdf())).toEqual(
-      omitVersions(
-        JSON.parse(
-          fs.readFileSync(
-            'sample_jsons/xccdf_results_mapper/xccdf-scc-rhel7-hdf.json',
-            {encoding: 'utf-8'}
+  describe('SCC', () => {
+    it('rhel7', () => {
+      const mapper = new XCCDFResultsMapper(
+        fs.readFileSync(
+          'sample_jsons/xccdf_results_mapper/sample_input_report/xccdf-results-scc-rhel7.xml',
+          {encoding: 'utf-8'}
+        )
+      );
+      expect(omitVersions(mapper.toHdf())).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              'sample_jsons/xccdf_results_mapper/xccdf-scc-rhel7-hdf.json',
+              {encoding: 'utf-8'}
+            )
           )
         )
-      )
-    );
+      );
+    });
+    it('rhel8', () => {
+      const mapper = new XCCDFResultsMapper(
+        fs.readFileSync(
+          'sample_jsons/xccdf_results_mapper/sample_input_report/xccdf-results-scc-rhel8.xml',
+          {encoding: 'utf-8'}
+        )
+      );
+      expect(omitVersions(mapper.toHdf())).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              'sample_jsons/xccdf_results_mapper/xccdf-scc-rhel8-hdf.json',
+              {encoding: 'utf-8'}
+            )
+          )
+        )
+      );
+    });
   });
-  it('OpenSCAP', () => {
-    const mapper = new XCCDFResultsMapper(
-      fs.readFileSync(
-        'sample_jsons/xccdf_results_mapper/sample_input_report/xccdf-results-openscap-rhel8.xml',
-        {encoding: 'utf-8'}
-      )
-    );
-    expect(omitVersions(mapper.toHdf())).toEqual(
-      omitVersions(
-        JSON.parse(
-          fs.readFileSync(
-            'sample_jsons/xccdf_results_mapper/xccdf-openscap-rhel8-hdf.json',
-            {encoding: 'utf-8'}
+  describe('OpenSCAP', () => {
+    it('rhel7', () => {
+      const mapper = new XCCDFResultsMapper(
+        fs.readFileSync(
+          'sample_jsons/xccdf_results_mapper/sample_input_report/xccdf-results-openscap-rhel7.xml',
+          {encoding: 'utf-8'}
+        )
+      );
+      expect(omitVersions(mapper.toHdf())).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              'sample_jsons/xccdf_results_mapper/xccdf-openscap-rhel7-hdf.json',
+              {encoding: 'utf-8'}
+            )
           )
         )
-      )
-    );
+      );
+    });
+    it('rhel8', () => {
+      const mapper = new XCCDFResultsMapper(
+        fs.readFileSync(
+          'sample_jsons/xccdf_results_mapper/sample_input_report/xccdf-results-openscap-rhel8.xml',
+          {encoding: 'utf-8'}
+        )
+      );
+      expect(omitVersions(mapper.toHdf())).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              'sample_jsons/xccdf_results_mapper/xccdf-openscap-rhel8-hdf.json',
+              {encoding: 'utf-8'}
+            )
+          )
+        )
+      );
+    });
   });
 });
