9621ace0ebe6fc7c474415d6943e50a297d8a71f
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-02-21 17:32:15 -0500

    Use secrets.SPLUNK_HEC_TOKEN for SPLUNK_HEC_TOKEN

--- libs/hdf-converters/test/mappers/reverse/splunk_reverse_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/reverse/splunk_reverse_mapper.spec.ts
@@ -13,7 +13,9 @@ describe('Describe Splunk Reverse Mapper', () => {
     //The From Hdf to Asff mapper takes a HDF object and an options argument with the format of the CLI tool
     const guid = await new FromHDFToSplunkMapper(inputData).toSplunk(
       {
-        token: 'd9dfa42c-aa59-4fa3-a70a-072c32f6615d',
+        token:
+          process.env.SPLUNK_HEC_TOKEN ||
+          'SET SPLUNK_HEC_TOKEN OR THIS WILL BREAK',
         host: '127.0.0.1',
         protocol: 'http',
         port: 8088
