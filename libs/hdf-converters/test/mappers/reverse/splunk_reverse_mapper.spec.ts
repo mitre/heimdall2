91cd1a13dc79c88e18399a99abc36350fb1f91cf
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-03-07 17:23:12 -0500

    Remove waitForJob

--- libs/hdf-converters/test/mappers/reverse/splunk_reverse_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/reverse/splunk_reverse_mapper.spec.ts
@@ -1,39 +1,5 @@
-import axios from 'axios';
 import fs from 'fs';
 import {FromHDFToSplunkMapper} from '../../../src/converters-from-hdf/splunk/reverse-splunk-mapper';
-
-const splunkAuthorization = 'Basic YWRtaW46VmFsaWRfcGFzc3dvcmQh'; // Admin:Valid_password!
-
-async function waitForJob(id: string): Promise<any> {
-  const completed = await axios
-    .get(`http://127.0.0.1:8089/services/search/jobs/${id}?output_mode=json`, {
-      headers: {
-        Authorization: splunkAuthorization // Admin:Valid_password!
-      }
-    })
-    .then(({data}) => data.entry[0].content.isDone);
-  if (!completed) {
-    return waitForJob(id);
-  } else {
-    return axios
-      .get(
-        `http://127.0.0.1:8089/services/search/jobs/${id}/results?output_mode=json&count=200000`,
-        {
-          headers: {
-            Authorization: splunkAuthorization
-          }
-        }
-      )
-      .then(({data}) => {
-        return data.results.map((result: {_raw: string}) => {
-          if (typeof result._raw === 'string') {
-            return result._raw;
-          }
-          return JSON.parse(result._raw);
-        });
-      });
-  }
-}
 
 export function delay(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
