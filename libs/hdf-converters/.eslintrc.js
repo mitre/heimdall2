0945cab52ff49e7b344514bd3bdbb9ac937c2edd
Camden Moors <cmoors@mitre.org>
2022-03-22 12:28:09 -0400

    Update TSConfig to include eslintrc

--- libs/hdf-converters/.eslintrc.js
+++ libs/hdf-converters/.eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: 'tsconfig.json',
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'plugin:prettier/recommended',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  root: true,
+  env: {
+    node: true
+  },
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true
+        }
+      }
     ],
-    root: true,
-    env: {
-        node: true,
-    },
-    rules: {
-        "@typescript-eslint/naming-convention": [
-            "warn",
-            {
-                "selector": "interface",
-                "format": ["PascalCase"],
-                "custom": {
-                    "regex": "^I[A-Z]",
-                    "match": true
-                }
-            }
-        ],
-        'object-curly-spacing': 'warn',
-        '@typescript-eslint/no-explicit-any': 'off',
-    },
+    'object-curly-spacing': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off'
+  }
 };
