3b50b75c519053231bdb790f48b1428f81f7e97b
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-04-14 17:08:32 -0400

    Add tests for 3-layer overlay

--- libs/hdf-converters/test/mappers/reverse/asff_reverse_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/reverse/asff_reverse_mapper.spec.ts
@@ -3,7 +3,7 @@ import {FromHdfToAsffMapper} from '../../../src/converters-from-hdf/asff/reverse
 import {omitASFFTimes, omitASFFTitle, omitASFFVersions} from '../../utils';
 
 describe('ASFF Reverse Mapper', () => {
-  it('Successfully converts HDF into ASFF', () => {
+  it('Successfully converts a one-layer HDF into ASFF', () => {
     const inputData = JSON.parse(
       fs.readFileSync(
         'sample_jsons/asff_reverse_mapper/sample_input_report/rhel7-results.json',
@@ -21,11 +21,6 @@ describe('ASFF Reverse Mapper', () => {
 
     const profileInformation = [converted[converted.length - 1] || {}];
 
-    fs.writeFileSync(
-      'sample_jsons/asff_reverse_mapper/rhel7-results.asff.json',
-      JSON.stringify(converted, null, 2)
-    );
-
     const expectedJSON = JSON.parse(
       fs.readFileSync(
         'sample_jsons/asff_reverse_mapper/rhel7-results.asff.json',
@@ -36,6 +31,48 @@ describe('ASFF Reverse Mapper', () => {
     const expectedProfileInfo = JSON.parse(
       fs.readFileSync(
         'sample_jsons/asff_reverse_mapper/rhel7-results.asff.json.p0.json',
+        {encoding: 'utf-8'}
+      )
+    );
+
+    expect(omitASFFVersions(omitASFFTimes(omitASFFTitle(converted)))).toEqual(
+      omitASFFVersions(omitASFFTimes(omitASFFTitle(expectedJSON)))
+    );
+    expect(
+      omitASFFVersions(omitASFFTimes(omitASFFTitle(profileInformation)))
+    ).toEqual(
+      omitASFFVersions(omitASFFTimes(omitASFFTitle(expectedProfileInfo)))
+    );
+  });
+
+  it('Successfully converts a three-layer HDF into ASFF', () => {
+    const inputData = JSON.parse(
+      fs.readFileSync(
+        'sample_jsons/asff_reverse_mapper/sample_input_report/example-3-layer-overlay_03062022.json',
+        {encoding: 'utf-8'}
+      )
+    );
+
+    //The From Hdf to Asff mapper takes a HDF object and an options argument with the format of the CLI tool
+    const converted = new FromHdfToAsffMapper(inputData, {
+      input: 'example-3-layer-overlay_03062022.json',
+      awsAccountId: '12345678910',
+      target: 'reverse-proxy',
+      region: 'us-east-2'
+    }).toAsff();
+
+    const profileInformation = [converted[converted.length - 1] || {}];
+
+    const expectedJSON = JSON.parse(
+      fs.readFileSync(
+        'sample_jsons/asff_reverse_mapper/example-3-layer-overlay_03062022.asff.json',
+        {encoding: 'utf-8'}
+      )
+    );
+
+    const expectedProfileInfo = JSON.parse(
+      fs.readFileSync(
+        'sample_jsons/asff_reverse_mapper/example-3-layer-overlay_03062022.asff.json.p0.json',
         {encoding: 'utf-8'}
       )
     );
