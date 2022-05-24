d7bdbbe668afeb37f580dcb1d1400a9c7877f5f2
Joshua <jweiss@mitre.org>
2022-05-24 16:19:50 -0400

    remoed line that cased sonarcube test bug

--- libs/hdf-converters/src/veracode-mapper.ts
+++ libs/hdf-converters/src/veracode-mapper.ts
@@ -1,5 +1,4 @@
 /* eslint-disable prettier/prettier */
-import { String } from 'aws-sdk/clients/acm';
 import parser from 'fast-xml-parser';
 import {ExecJSON} from 'inspecjs';
 import _ from 'lodash';
