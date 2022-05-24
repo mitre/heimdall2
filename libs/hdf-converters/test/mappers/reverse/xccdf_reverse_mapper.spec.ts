15b3156cafac3fc1ce40a0f4bdc4f6b973109c1f
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-05-24 10:25:42 -0400

    Add tests for example-3-layer xccdf-results conversion

--- libs/hdf-converters/test/mappers/reverse/xccdf_reverse_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/reverse/xccdf_reverse_mapper.spec.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import {FromHDFToXCCDFMapper} from '../../../index';
 
 describe('XCCDF Results Reverse Mapper', () => {
-  it('Successfully converts HDF into XCCDF-Results', () => {
+  it('Successfully converts RHEL7 HDF into XCCDF-Results', () => {
     const inputData = fs.readFileSync(
       'sample_jsons/xccdf_reverse_mapper/sample_input_report/rhel7-results.json',
       {encoding: 'utf-8'}
@@ -26,6 +26,36 @@ describe('XCCDF Results Reverse Mapper', () => {
 
     const expected = fs.readFileSync(
       'sample_jsons/xccdf_reverse_mapper/rhel7-xccdf-results.xml',
+      'utf-8'
+    );
+
+    expect(converted).toEqual(expected);
+  });
+
+  it('Successfully converts a 3 layer overlay HDF into XCCDF-Results', () => {
+    const inputData = fs.readFileSync(
+      'sample_jsons/xccdf_reverse_mapper/sample_input_report/example-3-layer-overlay.json',
+      {encoding: 'utf-8'}
+    );
+    const outputTemplate = fs.readFileSync(
+      'src/converters-from-hdf/xccdf/hdf2xccdf-results-template.xml'
+    );
+    const mapper = new FromHDFToXCCDFMapper(
+      inputData,
+      outputTemplate.toString(),
+      true
+    );
+
+    const converted = mapper.toXCCDF();
+
+    // Write out to a file so we can do schema validation
+    fs.writeFileSync(
+      'sample_jsons/xccdf_reverse_mapper/output_report/example-3-layer-overlay-xccdf-results.xml',
+      converted
+    );
+
+    const expected = fs.readFileSync(
+      'sample_jsons/xccdf_reverse_mapper/example-3-layer-overlay-xccdf-results.xml',
       'utf-8'
     );
 
