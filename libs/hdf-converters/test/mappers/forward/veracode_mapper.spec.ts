625606d6a131128fd0a6ebd8c8c62c89929f9279
Joshua <jweiss@mitre.org>
2022-06-02 14:37:43 -0400

    added expect statement to veracode mapper test

--- libs/hdf-converters/test/mappers/forward/veracode_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/veracode_mapper.spec.ts
@@ -1,12 +1,21 @@
 import fs from 'fs';
 import {VeracodeMapper} from '../../../src/veracode-mapper';
-
+import {omitVersions} from '../../utils'
 describe('veracode_mapper', () => {
   it('Successfully converts Veracode reports', () => {
     const mapper = new VeracodeMapper(
       fs.readFileSync(
         'sample_jsons/veracode_mapper/sample_input_report/veracode.xml',
         {encoding: 'utf-8'}
+      )
+    );
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync('sample_jsons/veracode_mapper/veracode-hdf.json', {
+            encoding: 'utf-8'
+          })
+        )
       )
     );
   });
