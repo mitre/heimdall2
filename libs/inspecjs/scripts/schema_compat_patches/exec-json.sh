c1ddba5a6f7661887424048489026565aa4c07b2
Camden Moors <cmoors@mitre.org>
2022-06-07 09:37:24 -0400

    Add Attestations to HDF Schema

--- libs/inspecjs/scripts/schema_compat_patches/exec-json.sh
+++ libs/inspecjs/scripts/schema_compat_patches/exec-json.sh
@@ -13,6 +13,12 @@ jq '.
 # Insert Waiver Data
 | .definitions.Waiver_Data = {"type":"object","additionalProperties":true,"required":[],"properties":{"expiration_date":{"type":"string"},"justification":{"type":"string"},"message":{"type":"string"},"run":{"type":"boolean"},"skipped_due_to_waiver":{"type":["string","boolean"]}}}
 
+# Insert Attestation Data
+| .definitions.Attestation_Data = {"type":"object", "additionalProperties": true, "required": ["control_id", "explanation", "frequency", "status", "updated", "updated_by"], "properties": {"control_id": {"type":"string"}, "explanation": {"type": "string"}, "frequency": {"type": "string"}, "status": {"type": "string", "enum": ["passed", "failed"]}, "updated": {"type": "string"}, "updated_by": {"type": "string"}}}
+
 # Insert waiver_data reference
 | .definitions["Exec_JSON_Control"].properties.waiver_data = {"$ref":"#/definitions/Waiver_Data"}
+
+# Insert attestation_data reference
+| .definitions["Exec_JSON_Control"].properties.attestation_data = {"$ref":"#/definitions/Attestation_Data"}
 ' <&0
