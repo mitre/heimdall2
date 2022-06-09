762ca16319462e760ccfb7c84a74467898522a20
charleshu-mitre <chu@mitre.org>
2022-06-09 11:43:52 -0400

    Fixed unstable parsing
    Signed-off-by: charleshu-mitre <chu@mitre.org>

--- libs/hdf-converters/src/twistlock-mapper.ts
+++ libs/hdf-converters/src/twistlock-mapper.ts
@@ -127,6 +127,6 @@ export class TwistlockMapper extends BaseConverter {
     }
   };
   constructor(twistlockJson: string) {
-    super(JSON.parse(twistlockJson), true);
+    super(JSON.parse(JSON.stringify(twistlockJson)), true);
   }
 }
