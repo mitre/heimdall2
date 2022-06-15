e9fc3748789761f9ed5e429557dbe47ee2d80c8b
Ryan Lin <38306648+Rlin232@users.noreply.github.com>
2022-06-15 14:12:12 -0700

    rename to fingerprint
    Co-authored-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/hdf-converters/src/utils/fingerprinting.ts
+++ libs/hdf-converters/src/utils/fingerprinting.ts
@@ -24,7 +24,7 @@ export const fileTypeFingerprints = {
   zap: ['@generated', '@version', 'site']
 };
 
-export function guessType(guessOptions: {
+export function fingerprint(guessOptions: {
   data: string;
   filename: string;
 }): string {
