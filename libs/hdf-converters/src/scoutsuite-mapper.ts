9664c1af33f382f139cb2485d9b6de144f63cb2f
charleshu-mitre <chu@mitre.org>
2022-07-01 13:05:56 -0400

    Linting
    Signed-off-by: charleshu-mitre <chu@mitre.org>

--- libs/hdf-converters/src/scoutsuite-mapper.ts
+++ libs/hdf-converters/src/scoutsuite-mapper.ts
@@ -267,7 +267,14 @@ export class ScoutsuiteMapper extends BaseConverter {
     ],
     passthrough: {
       transformer: (data: Record<string, unknown>): Record<string, unknown> => {
-        let auxData = _.omit(data, ['account_id', 'environment', 'partition', 'provider_code', 'provider_name', 'services']);
+        const auxData = _.omit(data, [
+          'account_id',
+          'environment',
+          'partition',
+          'provider_code',
+          'provider_name',
+          'services'
+        ]);
         if (auxData instanceof Object) {
           auxData.last_run = _.pick(auxData.last_run, ['summary']);
         }
