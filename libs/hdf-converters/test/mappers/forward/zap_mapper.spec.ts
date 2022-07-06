3a15cdee8878a6e3c117df048cbb84411274fac2
Amndeep Singh Mann <amann@mitre.org>
2022-07-06 11:09:49 -0400

    Recomment writefilesync

--- libs/hdf-converters/test/mappers/forward/zap_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/zap_mapper.spec.ts
@@ -36,10 +36,10 @@ describe('zap_mapper', () => {
       'http://zero.webappsecurity.com'
     );
 
-    fs.writeFileSync(
-      'sample_jsons/zap_mapper/zap-webappsecurity-hdf.json',
-      JSON.stringify(mapper.toHdf(), null, 2)
-    );
+    // fs.writeFileSync(
+    //   'sample_jsons/zap_mapper/zap-webappsecurity-hdf.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
