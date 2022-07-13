9c117a5e61c041da778c12f157f5a3cb4dfdbc1f
charleshu-8 <chu@mitre.org>
2022-07-13 15:53:39 -0400

    Added regression testing for withRaw flagged mappers
    Signed-off-by: charleshu-8 <chu@mitre.org>

--- libs/hdf-converters/test/mappers/forward/burpsuite_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/burpsuite_mapper.spec.ts
@@ -27,3 +27,33 @@ describe('burpsuite_mapper', () => {
     );
   });
 });
+
+describe('burpsuite_mapper_withraw', () => {
+  it('Successfully converts withRaw flagged Burpsuite reports', () => {
+    const mapper = new BurpSuiteMapper(
+      fs.readFileSync(
+        'sample_jsons/burpsuite_mapper/sample_input_report/zero.webappsecurity.com.min',
+        {encoding: 'utf-8'}
+      ),
+      true
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/burpsuite_mapper/burpsuite-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/burpsuite_mapper/burpsuite-hdf-withraw.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+});
--- libs/hdf-converters/test/mappers/forward/jfrog_xray_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/jfrog_xray_mapper.spec.ts
@@ -27,3 +27,33 @@ describe('jfrog_xray_mapper', () => {
     );
   });
 });
+
+describe('jfrog_xray_mapper_withraw', () => {
+  it('Successfully converts withRaw flagged JFrog Xray data', () => {
+    const mapper = new JfrogXrayMapper(
+      fs.readFileSync(
+        'sample_jsons/jfrog_xray_mapper/sample_input_report/jfrog_xray_sample.json',
+        {encoding: 'utf-8'}
+      ),
+      true
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/jfrog_xray_mapper/jfrog-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/jfrog_xray_mapper/jfrog-hdf-withraw.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+});
