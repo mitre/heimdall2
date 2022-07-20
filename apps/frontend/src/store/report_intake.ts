c1f86f268efcb93b47b847d104ee5f90ce3f8fea
Rlin232 <rlin@mitre.org>
2022-07-20 10:53:26 -0700

    removed brackets from import
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- apps/frontend/src/store/report_intake.ts
+++ apps/frontend/src/store/report_intake.ts
@@ -47,7 +47,7 @@ import { FilteredDataModule } from './data_filters';
 import { SnackbarModule } from './snackbar';
 import { ChecklistFile, ChecklistHeader, ChecklistVuln, Stig } from '@/types/checklist/control';
 import router from '@/router';
-import { Jsonix } from 'jsonix'
+import Jsonix from 'jsonix'
 
 /** Each FileID corresponds to a unique File in this store */
 export type FileID = string;
@@ -432,7 +432,6 @@ export class InspecIntake extends VuexModule {
       router.push('/checklist');
 
     const fileID: FileID = uuid();
-    new Jsonix.Context([])
     const mapping = {
       name: 'Checklist',
       typeInfos: [{
@@ -816,7 +815,8 @@ export class InspecIntake extends VuexModule {
     }
     console.log('mapping defined')
     console.log(Jsonix)
-    const context = new Jsonix.Context([mapping])
+    // const context = new Jsonix.Context([mapping])
+    const context = {} as any
     const unmarshaller = context.createUnmarshaller()
 
     function getAttributeData(stigdata: unknown[], tag: string): string {
