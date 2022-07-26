4c6d2c016310d7674bb685ae829c9a44f5b2a9a8
Rlin232 <rlin@mitre.org>
2022-07-26 13:31:30 -0700

    Added checklist to sidebar (selection functionality still not there)
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- apps/frontend/src/components/global/Sidebar.vue
+++ apps/frontend/src/components/global/Sidebar.vue
@@ -1,49 +1,31 @@
 <template>
-  <v-navigation-drawer
-    :value="value"
-    :clipped="$vuetify.breakpoint.lgAndUp"
-    app
-    :style="{'margin-top': classification ? '5em' : '3.5em', 'z-index': 11}"
-    disable-resize-watcher
-    disable-route-watcher
-    fixed
-    temporary
-    width="600px"
-    @input="$emit('input', $event)"
-    @blur="value = false"
-  >
+  <v-navigation-drawer :value="value" :clipped="$vuetify.breakpoint.lgAndUp" app
+    :style="{ 'margin-top': classification ? '5em' : '3.5em', 'z-index': 11 }" disable-resize-watcher
+    disable-route-watcher fixed temporary width="600px" @input="$emit('input', $event)" @blur="value = false">
     <v-expansion-panels v-model="active_path" accordion>
-      <DropdownContent
-        header-text="Results"
-        :files="visible_evaluation_files"
-        :all-selected="all_evaluations_selected"
-        :enable-compare-view="true"
-        :compare-view-active="compareViewActive"
-        @toggle-all="toggle_all_evaluations"
-        @toggle-compare-view="compareView"
-        @changed-files="$emit('changed-files')"
-      />
-      <DropdownContent
-        header-text="Profiles"
-        :files="visible_profile_files"
-        :all-selected="all_profiles_selected"
-        @toggle-all="toggle_all_profiles"
-        @changed-files="$emit('changed-files')"
-      />
+      <DropdownContent header-text="Results" :files="visible_evaluation_files" :all-selected="all_evaluations_selected"
+        :enable-compare-view="true" :compare-view-active="compareViewActive" @toggle-all="toggle_all_evaluations"
+        @toggle-compare-view="compareView" @changed-files="$emit('changed-files')" />
+      <DropdownContent header-text="Profiles" :files="visible_profile_files" :all-selected="all_profiles_selected"
+        @toggle-all="toggle_all_profiles" @changed-files="$emit('changed-files')" />
+      <DropdownContent header-text="Checklists" :files="visible_checklist_files" :all-selected="all_checklists_selected"
+        @toggle-all="toggle_all_checklists" @changed-files="$emit('changed-files')" />
     </v-expansion-panels>
   </v-navigation-drawer>
 </template>
 
 <script lang="ts">
 import DropdownContent from '@/components/global/sidebaritems/DropdownContent.vue';
-import {Trinary} from '@/enums/Trinary';
+import { Trinary } from '@/enums/Trinary';
 import RouteMixin from '@/mixins/RouteMixin';
-import {FilteredDataModule} from '@/store/data_filters';
-import {InspecDataModule} from '@/store/data_store';
-import {EvaluationFile, ProfileFile} from '@/store/report_intake';
-import Component, {mixins} from 'vue-class-component';
-import {Prop} from 'vue-property-decorator';
-import {ServerModule} from '../../store/server';
+import { FilteredChecklistDataModule } from '@/store/checklist_data_filters';
+import { FilteredDataModule } from '@/store/data_filters';
+import { InspecDataModule } from '@/store/data_store';
+import { EvaluationFile, ProfileFile } from '@/store/report_intake';
+import { ChecklistFile } from '@/types/checklist/control';
+import Component, { mixins } from 'vue-class-component';
+import { Prop } from 'vue-property-decorator';
+import { ServerModule } from '../../store/server';
 
 @Component({
   components: {
@@ -51,11 +33,13 @@ import {ServerModule} from '../../store/server';
   }
 })
 export default class Sidebar extends mixins(RouteMixin) {
-  @Prop({type: Boolean}) readonly value!: boolean;
+  @Prop({ type: Boolean }) readonly value!: boolean;
 
   // open the appropriate v-expansion-panel based on current route
   get active_path() {
-    if (this.current_route === 'profiles') {
+    if (this.current_route === 'checklists') {
+      return 2;
+    } else if (this.current_route === 'profiles') {
       return 1;
     } else if (
       this.current_route === 'results' ||
@@ -68,13 +52,16 @@ export default class Sidebar extends mixins(RouteMixin) {
   }
 
   set active_path(id: number) {
-    // There are currently 2 available values that the v-modal can have,
+    // There are currently 3 available values that the v-modal can have,
     // 0 -> results view
     // 1 -> profile view
+    // 2 -> checklists view
     if (id === 0) {
       this.navigateWithNoErrors(`/results`);
     } else if (id === 1) {
       this.navigateWithNoErrors(`/profiles`);
+    } else if (id === 2) {
+      this.navigateWithNoErrors(`/checklists`);
     }
   }
 
@@ -90,12 +77,22 @@ export default class Sidebar extends mixins(RouteMixin) {
     return files.sort((a, b) => a.filename.localeCompare(b.filename));
   }
 
+  // get all visible (uploaded) checklist files
+  get visible_checklist_files(): ChecklistFile[] {
+    const files = InspecDataModule.allChecklistFiles;
+    return files.sort((a, b) => a.filename.localeCompare(b.filename));
+  }
+
   get all_evaluations_selected(): Trinary {
     return FilteredDataModule.all_evaluations_selected;
   }
 
   get all_profiles_selected(): Trinary {
     return FilteredDataModule.all_profiles_selected;
+  }
+
+  get all_checklists_selected(): Trinary {
+    return FilteredChecklistDataModule.all_checklists_selected;
   }
 
   get compareViewActive(): boolean {
@@ -114,6 +111,11 @@ export default class Sidebar extends mixins(RouteMixin) {
   // toggle the "select all" for evaluations
   toggle_all_evaluations(): void {
     FilteredDataModule.toggle_all_evaluations();
+  }
+
+  // toggle the "select all" for checklists
+  toggle_all_checklists(): void {
+    FilteredChecklistDataModule.toggle_all_checklists();
   }
 
   // toggle between the comparison view and the results view
