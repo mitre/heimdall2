33d3ed97df6975f5f6fb3ad7c9db6232904cde75
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-07-29 15:27:25 -0400

    Add search, select columns

--- apps/frontend/src/views/Checklist.vue
+++ apps/frontend/src/views/Checklist.vue
@@ -1,142 +1,219 @@
 <template>
-  <Base :show-search="true" :title="curr_title" @changed-files="evalInfo = null">
-  <!-- Topbar content - give it a search bar -->
-  <template #topbar-content>
-    <v-btn :disabled="!can_clear" @click="clear">
-      <span class="d-none d-md-inline pr-2"> Clear </span>
-      <v-icon>mdi-filter-remove</v-icon>
-    </v-btn>
-    <UploadButton />
-    <div class="text-center">
-      <v-menu>
-        <template #activator="{ on, attrs }">
-          <v-btn v-bind="attrs" class="mr-2" v-on="on">
-            <span class="d-none d-md-inline mr-2"> Export </span>
-            <v-icon> mdi-file-export </v-icon>
-          </v-btn>
-        </template>
-        <v-list class="py-0">
-          <v-list-item class="px-0">
-            <ExportCaat :filter="all_filter" />
-          </v-list-item>
-          <v-list-item v-if="is_checklist_view" class="px-0">
-            <ExportNist :filter="all_filter" />
-          </v-list-item>
-          <v-list-item v-if="is_checklist_view" class="px-0">
-            <ExportASFFModal :filter="all_filter" />
-          </v-list-item>
-          <v-list-item v-if="is_checklist_view" class="px-0">
-            <ExportCKLModal :filter="all_filter" />
-          </v-list-item>
-          <v-list-item class="px-0">
-            <ExportCSVModal :filter="all_filter" />
-          </v-list-item>
-          <v-list-item v-if="is_checklist_view" class="px-0">
-            <ExportHTMLModal :filter="all_filter" :file-type="current_route_name" />
-          </v-list-item>
-          <v-list-item v-if="is_checklist_view" class="px-0">
-            <ExportSplunkModal />
-          </v-list-item>
-          <v-list-item class="px-0">
-            <ExportJson />
-          </v-list-item>
-          <v-list-item class="px-0">
-            <ExportXCCDFResults :filter="all_filter" :is-result-view="is_checklist_view" />
-          </v-list-item>
-        </v-list>
-      </v-menu>
-    </div>
-  </template>
-  <template #main-content>
-    <v-container fluid grid-list-md pt-0 pa-2>
-      <v-container fluid>
-        <v-row class="mb-6">
-          <v-col v-for="n in 4" :v-bind="n" :cols="1">
-            <v-switch inset label="red" color="red" value="red" hide-details />
+  <Base
+    :show-search="true"
+    :title="curr_title"
+    @changed-files="evalInfo = null"
+  >
+    <!-- Topbar content - give it a search bar -->
+    <template #topbar-content>
+      <v-btn :disabled="!can_clear" @click="clear">
+        <span class="d-none d-md-inline pr-2"> Clear </span>
+        <v-icon>mdi-filter-remove</v-icon>
+      </v-btn>
+      <UploadButton />
+      <div class="text-center">
+        <v-menu>
+          <template #activator="{on, attrs}">
+            <v-btn v-bind="attrs" class="mr-2" v-on="on">
+              <span class="d-none d-md-inline mr-2"> Export </span>
+              <v-icon> mdi-file-export </v-icon>
+            </v-btn>
+          </template>
+          <v-list class="py-0">
+            <v-list-item class="px-0">
+              <ExportCaat :filter="all_filter" />
+            </v-list-item>
+            <v-list-item v-if="is_checklist_view" class="px-0">
+              <ExportNist :filter="all_filter" />
+            </v-list-item>
+            <v-list-item v-if="is_checklist_view" class="px-0">
+              <ExportASFFModal :filter="all_filter" />
+            </v-list-item>
+            <v-list-item v-if="is_checklist_view" class="px-0">
+              <ExportCKLModal :filter="all_filter" />
+            </v-list-item>
+            <v-list-item class="px-0">
+              <ExportCSVModal :filter="all_filter" />
+            </v-list-item>
+            <v-list-item v-if="is_checklist_view" class="px-0">
+              <ExportHTMLModal
+                :filter="all_filter"
+                :file-type="current_route_name"
+              />
+            </v-list-item>
+            <v-list-item v-if="is_checklist_view" class="px-0">
+              <ExportSplunkModal />
+            </v-list-item>
+            <v-list-item class="px-0">
+              <ExportJson />
+            </v-list-item>
+            <v-list-item class="px-0">
+              <ExportXCCDFResults
+                :filter="all_filter"
+                :is-result-view="is_checklist_view"
+              />
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+    </template>
+    <template #main-content>
+      <v-container fluid grid-list-md pt-0 pa-2>
+        <v-container fluid>
+          <v-row class="mb-2">
+            <v-col v-for="n in 4" :v-bind="n" :cols="1">
+              <v-switch
+                inset
+                label="red"
+                color="red"
+                value="red"
+                hide-details
+              />
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-row>
+          <v-col xs="4" :cols="4" height="100%">
+            <!-- Data Table -->
+            <v-card>
+              <v-card-title class="mt-0 pt-0"
+                ><div class="mt-n4 pt-n6">Rules</div>
+                <v-spacer class="mt-0 pt-0" />
+                <v-select
+                  v-model="selectedHeaders"
+                  :items="headersList"
+                  label="Select Columns"
+                  class="mt-4 pt-0"
+                  multiple
+                  outlined
+                  return-object
+                  :style="{width: '300px'}"
+                  dense
+                >
+                  <template #selection="{item, index}">
+                    <v-chip v-if="index < 3" small>
+                      <span>{{ item.text }}</span>
+                    </v-chip>
+                    <span v-if="index === 3" class="grey--text caption"
+                      >(+{{ selectedHeaders.length - 2 }} others)</span
+                    >
+                  </template>
+                </v-select>
+              </v-card-title>
+              <v-card-text>
+                <v-data-table
+                  disable-pagination
+                  dense
+                  fixed-header
+                  :items="rules"
+                  :headers="headers"
+                  :search="searchValue"
+                  hide-default-footer
+                  class="overflow-y-auto"
+                  height="70vh"
+                  @click:row="showRule"
+                >
+                  <template #[`item.ruleVersion`]="{item}">
+                    {{ truncate(shortStigId(item.ruleVersion), 20) }}
+                  </template>
+                  <template #[`item.ruleId`]="{item}">
+                    {{ truncate(shortRuleId(item.ruleId), 20) }}
+                  </template>
+                  <template #[`item.vulnNum`]="{item}">
+                    {{ truncate(item.vulnNum, 20) }}
+                  </template>
+                  <template #[`item.groupTitle`]="{item}">
+                    {{ truncate(item.groupTitle, 20) }}
+                  </template>
+                  <template #[`item.ruleTitle`]="{item}">
+                    {{ truncate(item.ruleTitle, 100) }}
+                  </template>
+                  <template #[`item.cciRef`]="{item}">
+                    {{ truncate(item.cciRef, 13) }}
+                  </template>
+                </v-data-table>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <!-- Rule Data -->
+          <v-col xs="4" :cols="8">
+            <v-card height="40%" class="mt-n16 mb-2">
+              <v-card-text>
+                <v-row>
+                  <v-col :cols="2">
+                    <v-select
+                      v-model="selectedRule.status"
+                      label="Status"
+                      :items="[
+                        'Not_Reviewed',
+                        'Open',
+                        'NotAFinding',
+                        'Not_Applicable'
+                      ]"
+                    />
+                    <v-select
+                      v-model="selectedRule.severityOverride"
+                      label="Severity Override"
+                      :items="['CAT I', 'CAT II', 'CAT III']"
+                    />
+                  </v-col>
+                  <v-col :cols="10">
+                    <strong>Finding Details: </strong><br />
+                    <v-textarea
+                      v-model="selectedRule.findingDetails"
+                      solo
+                      outlined
+                      dense
+                      no-resize
+                      height="12vh"
+                    />
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col>
+                    <v-card class="mt-n12" color="transparent">
+                      <strong>Comments: </strong>
+                      <v-textarea
+                        v-model="selectedRule.comments"
+                        solo
+                        outlined
+                        dense
+                        no-resize
+                        height="12vh"
+                      />
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+            <v-card height="50.5vh" class="overflow-auto">
+              <v-card-title>Selected Rule</v-card-title>
+              <div v-if="selectedRule.vulnNum !== ''">
+                <v-card-text>
+                  <strong>Rule Title: </strong><br />
+                  {{ selectedRule.ruleTitle }}<br /><br />
+                  <strong>Discussion: </strong><br />
+                  {{ selectedRule.vulnDiscuss }}<br /><br />
+                  <strong>Check Text: </strong><br />
+                  {{ selectedRule.checkContent }}<br /><br />
+                  <strong>Fix Text: </strong><br />
+                  {{ selectedRule.fixText }}<br /><br />
+                </v-card-text>
+                <v-card-subtitle class="text-center"
+                  >References</v-card-subtitle
+                >
+                <v-card-text>
+                  <strong>CCI: </strong>{{ selectedRule.cciRef }}<br /><br />
+                </v-card-text>
+              </div>
+              <div v-else>
+                <v-card-text>No rule selected.</v-card-text>
+              </div>
+            </v-card>
           </v-col>
         </v-row>
       </v-container>
-      <v-row>
-        <v-col xs="4" :cols="4" height="100%">
-          <!-- Data Table -->
-          <v-card>
-            <v-card-title>Rules</v-card-title>
-            <v-card-text>
-              <v-data-table disable-pagination dense fixed-header :items="rules" :headers="headers" hide-default-footer
-                class="overflow-y-auto" height="70vh" @click:row="showRule">
-                <template #[`item.ruleVersion`]="{ item }">
-                  {{ truncate(shortStigId(item.ruleVersion), 20) }}
-                </template>
-                <template #[`item.ruleId`]="{ item }">
-                  {{ truncate(shortRuleId(item.ruleId), 20) }}
-                </template>
-                <template #[`item.vulnNum`]="{ item }">
-                  {{ truncate(item.vulnNum, 20) }}
-                </template>
-                <template #[`item.groupTitle`]="{ item }">
-                  {{ truncate(item.groupTitle, 20) }}
-                </template>
-
-              </v-data-table>
-            </v-card-text>
-          </v-card>
-        </v-col>
-        <!-- Rule Data -->
-        <v-col xs="4" :cols="8">
-          <v-card height="40%" class="mt-n16 mb-2">
-            <v-card-text>
-              <v-row>
-                <v-col :cols="2">
-                  <v-select v-model="selectedRule.status" label="Status" :items="[
-                    'Not_Reviewed',
-                    'Open',
-                    'NotAFinding',
-                    'Not_Applicable'
-                  ]" />
-                  <v-select v-model="selectedRule.severityOverride" label="Severity Override"
-                    :items="['CAT I', 'CAT II', 'CAT III']" />
-                </v-col>
-                <v-col :cols="10">
-                  <strong>Finding Details: </strong><br />
-                  <v-textarea v-model="selectedRule.findingDetails" solo outlined dense no-resize height="12vh" />
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <v-card class="mt-n12" color="transparent">
-                    <strong>Comments: </strong>
-                    <v-textarea v-model="selectedRule.comments" solo outlined dense no-resize height="12vh" />
-                  </v-card>
-                </v-col>
-              </v-row>
-            </v-card-text>
-          </v-card>
-          <v-card height="50.5vh" class="overflow-auto">
-            <v-card-title>Selected Rule</v-card-title>
-            <div v-if="selectedRule.vulnNum !== ''">
-              <v-card-text>
-                <strong>Rule Title: </strong><br />
-                {{ selectedRule.ruleTitle }}<br /><br />
-                <strong>Discussion: </strong><br />
-                {{ selectedRule.vulnDiscuss }}<br /><br />
-                <strong>Check Text: </strong><br />
-                {{ selectedRule.checkContent }}<br /><br />
-                <strong>Fix Text: </strong><br />
-                {{ selectedRule.fixText }}<br /><br />
-              </v-card-text>
-              <v-card-subtitle class="text-center">References</v-card-subtitle>
-              <v-card-text>
-                <strong>CCI: </strong>{{ selectedRule.cciRef }}<br /><br />
-              </v-card-text>
-            </div>
-            <div v-else>
-              <v-card-text>No rule selected.</v-card-text>
-            </div>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-container>
-  </template>
+    </template>
   </Base>
 </template>
 
@@ -144,22 +221,22 @@
 import Base from './Base.vue';
 import Component from 'vue-class-component';
 import RouteMixin from '@/mixins/RouteMixin';
-import { SearchModule } from '@/store/search';
+import {SearchModule} from '@/store/search';
 import {
   ExtendedControlStatus,
   Filter,
   FilteredDataModule
 } from '@/store/data_filters';
-import { FilteredChecklistDataModule } from '@/store/checklist_data_filters';
+import {FilteredChecklistDataModule} from '@/store/checklist_data_filters';
 import {
   FileID,
   SourcedContextualizedEvaluation,
   SourcedContextualizedProfile
 } from '@/store/report_intake';
-import { capitalize } from 'lodash';
-import { compare_times } from '../utilities/delta_util';
-import { Severity } from 'inspecjs';
-import { StatusCountModule } from '@/store/status_counts';
+import {capitalize} from 'lodash';
+import {compare_times} from '../utilities/delta_util';
+import {Severity} from 'inspecjs';
+import {StatusCountModule} from '@/store/status_counts';
 import UploadButton from '@/components/generic/UploadButton.vue';
 import StatusCardRow from '@/components/cards/StatusCardRow.vue';
 import StatusChart from '@/components/cards/StatusChart.vue';
@@ -172,8 +249,8 @@ import ExportJson from '@/components/global/ExportJson.vue';
 import ExportNist from '@/components/global/ExportNist.vue';
 import ExportSplunkModal from '@/components/global/ExportSplunkModal.vue';
 import ExportXCCDFResults from '@/components/global/ExportXCCDFResults.vue';
-import { ChecklistVuln } from '../types/checklist/control';
-import { InspecDataModule } from '@/store/data_store';
+import {ChecklistVuln} from '../types/checklist/control';
+import {InspecDataModule} from '@/store/data_store';
 import _ from 'lodash';
 // import SwitchTable from '@/components/cards/SwitchTable.vue';
 
@@ -235,19 +312,45 @@ export default class Checklist extends RouteMixin {
     cciRef: ''
   };
 
+  selectedHeaders: {text: string; value: string; width: string}[] = [
+    {text: 'Status', value: 'status', width: '100px'},
+    {text: 'STIG ID', value: 'ruleVersion', width: '100px'},
+    {text: 'Rule ID', value: 'ruleId', width: '100px'},
+    {text: 'Vul ID', value: 'vulnNum', width: '100px'},
+    {text: 'Group Name', value: 'groupTitle', width: '110px'},
+    {text: 'CCIs', value: 'cciRef', width: '110px'}
+  ];
+
+  headersList = [
+    {text: 'Status', value: 'status', width: '100px'},
+    {text: 'STIG ID', value: 'ruleVersion', width: '100px'},
+    {text: 'Rule ID', value: 'ruleId', width: '100px'},
+    {text: 'Vul ID', value: 'vulnNum', width: '100px'},
+    {text: 'Group Name', value: 'groupTitle', width: '110px'},
+    {text: 'Rule Title', value: 'ruleTitle', width: '500px'},
+    {text: 'CCIs', value: 'cciRef', width: '110px'}
+  ];
+
+  hiddenRows = [
+    {value: 'groupTitle', align: ' d-none'},
+    {value: 'ruleTitle', align: ' d-none'}
+  ];
+
   evalInfo:
     | SourcedContextualizedEvaluation
     | SourcedContextualizedProfile
     | null = null;
 
-  truncate(value: string, length: number) {
-    return _.truncate(value, { omission: '...', length: length })
+  truncate(value: string, length: number, omission = '...') {
+    return _.truncate(value, {omission: omission, length: length});
   }
+
   shortRuleId(ruleId: string) {
-    return ruleId.split('r')[0] || ruleId
+    return ruleId.split('r')[0] || ruleId;
   }
+
   shortStigId(stigId: string) {
-    return stigId.split('-').slice(0, 2).join('-')
+    return stigId.split('-').slice(0, 2).join('-');
   }
 
   /**
@@ -306,6 +409,22 @@ export default class Checklist extends RouteMixin {
     return FilteredChecklistDataModule.allFiles.find(
       (f) => f.uniqueId === fileID
     );
+  }
+
+  get searchValue(): string {
+    return SearchModule.searchTerm;
+  }
+
+  get headers() {
+    const selectedHeadersList = this.selectedHeaders.map(
+      (header) => header.text
+    );
+    return [
+      ...this.headersList.filter((header) =>
+        selectedHeadersList.includes(header.text)
+      ),
+      ...this.hiddenRows
+    ];
   }
 
   /**
@@ -388,16 +507,6 @@ export default class Checklist extends RouteMixin {
 
   get waivedProfilesExist(): boolean {
     return StatusCountModule.countOf(this.all_filter, 'Waived') >= 1;
-  }
-
-  get headers() {
-    return [
-      { text: 'Status', value: 'status', width: "100px", align: ' d-none' },
-      { text: 'STIG ID', value: 'ruleVersion', width: "100px", align: ' d-none' },
-      { text: 'Rule ID', value: 'ruleId', width: "100px", align: ' d-none' },
-      { text: 'Vul ID', value: 'vulnNum', width: "100px", align: ' d-none' },
-      { text: 'Rule Name', value: 'groupTitle', width: "100px", align: ' d-none' }
-    ];
   }
 
   get rules() {
