516b179bdf5fd44f3b1a6e166f0c9d88b34ab629
Eugene Aronne <34140975+ejaronne@users.noreply.github.com>
2022-08-17 16:59:53 -0400

    try 214k asff cap

--- libs/hdf-converters/src/converters-from-hdf/asff/reverse-asff-mapper.ts
+++ libs/hdf-converters/src/converters-from-hdf/asff/reverse-asff-mapper.ts
@@ -202,7 +202,8 @@ export class FromHdfToAsffMapper extends FromHdfBaseConverter {
           // length > 239000 && // commenting out to test smaller size
           // length > 119000 && // 240000/2=120000; 120000-1k=119000 commenting out to narrow down maximum size that will load
           // length > 179000 && // 240000*.75=180000; 180000-1k=179000 commenting out to narrow down maximum size that will load
-          length > 209000 && // 240000*.875=210000; 210000-1k=209000
+          // length > 209000 && // 240000*.875=210000; 210000-1k=209000
+          length > 214000 && 
           (finding.FindingProviderFields.Types as string[]).length > 0
         ) {
           // left 1KB buffer space in case anything weird happens and also to have plenty of space for our warning message to be appended
@@ -212,7 +213,8 @@ export class FromHdfToAsffMapper extends FromHdfBaseConverter {
         // if (length > 239000) {
         // if (length > 119000) {
         // if (length > 179000) {
-        if (length > 209000) {
+        // if (length > 209000) {
+        if (length > 214000) {
           // throw new Error('Finding could not be reduced to less than 240KB');
           throw new Error('Finding could not be reduced to less than 120KB');
         }
