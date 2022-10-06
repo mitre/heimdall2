744b96a450f37e5c4fafbf307414154818d8a2eb
Christopher Hinchey <chinchey@mitre.org>
2022-10-06 18:02:52 -0400

    Updated footer
    Signed-off-by: Christopher Hinchey <chinchey@mitre.org>

--- apps/frontend/src/components/global/Footer.vue
+++ apps/frontend/src/components/global/Footer.vue
@@ -3,6 +3,17 @@
     <span class="bar-visible--text">
       The MITRE Corporation &copy; 2018-{{ new Date().getFullYear() }}
     </span>
+    <v-spacer />
+    <a href="https://www.netlify.com">
+      <img
+        :src="require('@/assets/netlify-light.svg')"
+        style="width: 1.5rem; height: 1.5rem; vertical-align: middle"
+        class="mr-4"
+      />
+    </a>
+    <a href="https://github.com/mitre/mitre-saf" style="text-decoration: none">
+      <v-icon medium color="white">mdi-github</v-icon>
+    </a>
   </v-footer>
 </template>
 
