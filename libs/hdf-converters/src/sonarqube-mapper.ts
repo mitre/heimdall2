f11f22c6fcc0e134676609b8e90d535b741001ef
blake <blake.burton@caemilusa.com>
2022-10-17 12:54:01 -0400

    only pull open vulnerabilities for sonarqube2hdf as closed vulnerabilities

--- libs/hdf-converters/src/sonarqube-mapper.ts
+++ libs/hdf-converters/src/sonarqube-mapper.ts
@@ -130,6 +130,7 @@ export class SonarQubeResults {
           params: {
             componentKeys: this.projectId,
             types: 'VULNERABILITY',
+            statuses: 'OPEN,REOPENED',
             p: page,
             ...(this.branchName && {branch: this.branchName}),
             ...(this.pullRequestID && {pullRequest: this.pullRequestID})
