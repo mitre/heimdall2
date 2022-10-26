cb80c2a73a777db72c97e1e7ea0e5215e9fd88bb
Camden Moors <66680985+camdenmoors@users.noreply.github.com>
2022-10-26 15:53:38 -0400

    Lint

--- apps/backend/src/apikeys/apikey.controller.ts
+++ apps/backend/src/apikeys/apikey.controller.ts
@@ -60,7 +60,9 @@ export class ApiKeyController {
     const abac = this.authz.abac.createForUser(request.user);
     const user = createApiKeyDto.userId
       ? await this.usersService.findById(createApiKeyDto.userId)
-      : createApiKeyDto.userEmail ? await this.usersService.findByEmail(createApiKeyDto.userEmail) : request.user;
+      : createApiKeyDto.userEmail
+      ? await this.usersService.findByEmail(createApiKeyDto.userEmail)
+      : request.user;
     ForbiddenError.from(abac).throwUnlessCan(Action.Update, user);
     if (request.user.creationMethod === 'local') {
       await this.authnService.testPassword(createApiKeyDto, request.user);
