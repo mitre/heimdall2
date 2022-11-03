896ae97228fe341d4f8e1932ba9b79654e5fd035
Amndeep Singh Mann <amann@mitre.org>
2022-11-02 23:08:14 -0400

    vulnerabilities can apparently also be null instead of just an array so handling that situation
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/hdf-converters/src/twistlock-mapper.ts
+++ libs/hdf-converters/src/twistlock-mapper.ts
@@ -91,6 +91,7 @@ export class TwistlockMapper extends BaseConverter {
           {
             path: 'vulnerabilities',
             key: 'id',
+            pathTransform: (value) => Array.isArray(value) ? value : [],
             tags: {
               nist: DEFAULT_UPDATE_REMEDIATION_NIST_TAGS,
               cci: getCCIsForNISTTags(DEFAULT_UPDATE_REMEDIATION_NIST_TAGS),
