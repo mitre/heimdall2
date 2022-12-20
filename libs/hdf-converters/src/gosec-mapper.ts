7e9f4dc5c3aa83d1185ffeaa4b54c5908673e1a4
Joshua Weiss <89481736+jweissm@users.noreply.github.com>
2022-12-20 16:35:40 -0500

    Gosecmapper (#3838)
    * added licesnces to passthrough
    
    * made some small fixes to formatting
    
    * ran linter
    
    * addded list of control ids to licences
    
    * fixed nist tags
    
    * ran linter,  fixed some property errors
    
    * preliminary gosec mapper
    
    * added nist tages
    
    * got rid of commented out code
    
    * ran linter
    
    * added gosec to fingerprinting ,exports to index.ts
    
    * fixed lint issues
    
    * made rec changes
    
    * removed else if for gosec
    
    * ran linter
    
    * edited varaibles edited passthrough
    
    * GOSEC instead of GoSec, fixed Golang errors
    
    * got rid of line 42 as recommended
    
    * updated hdf file
    
    * got rid of extraneous comma
    
    * added gosec to report intake
    
    * fixed formatting
    
    Co-authored-by: Amndeep Singh Mann <amann@mitre.org>

---
+++ libs/hdf-converters/src/gosec-mapper.ts
@@ -0,0 +1,99 @@
+import {ExecJSON} from 'inspecjs';
+import _ from 'lodash';
+import {version as HeimdallToolsVersion} from '../package.json';
+import {BaseConverter, ILookupPath, MappedTransform} from './base-converter';
+import {CweNistMapping} from './mappings/CweNistMapping';
+
+const CWE_NIST_MAPPING = new CweNistMapping();
+const DEFAULT_NIST_TAG = ['SI-2', 'RA-5'];
+
+function nistTag(input: Record<string, unknown>): string[] {
+  const cwe = [`${_.get(input, 'id')}`];
+  console.log(cwe);
+  return CWE_NIST_MAPPING.nistFilter(cwe, DEFAULT_NIST_TAG);
+}
+
+function formatMessage(input: Record<string, unknown>): string {
+  return `${_.get(input, 'file')}, line:${_.get(input, 'line')}, column:${_.get(
+    input,
+    'column'
+  )}`;
+}
+
+export class GoSecMapper extends BaseConverter {
+  withRaw: boolean;
+
+  mappings: MappedTransform<
+    ExecJSON.Execution & {passthrough: unknown},
+    ILookupPath
+  > = {
+    passthrough: {
+      transformer: (data: Record<string, unknown>): Record<string, unknown> => {
+        return {
+          auxiliary_data: [
+            {
+              name: 'Gosec',
+              data: {
+                'Golang errors': _.get(data, 'Golang errors')
+              }
+            }
+          ],
+          ...(this.withRaw && {raw: data})
+        };
+      }
+    },
+    platform: {
+      name: 'Heimdall Tools',
+      release: HeimdallToolsVersion
+    },
+    version: HeimdallToolsVersion,
+    statistics: {},
+    profiles: [
+      {
+        name: 'Gosec scanner',
+        title: 'gosec',
+        version: {path: 'GosecVersion'},
+        supports: [],
+        attributes: [],
+        groups: [],
+        status: 'loaded',
+        controls: [
+          {
+            path: 'Issues',
+            key: 'id',
+            tags: {
+              nist: {
+                path: 'cwe',
+                transformer: nistTag
+              },
+              cwe: {path: 'cwe'},
+              nosec: {path: 'nosec'},
+              suppressions: {path: 'supressions'},
+              severity: {path: 'severity'},
+              confidence: {path: 'confidence'}
+            },
+            refs: [],
+            source_location: {},
+            title: {path: 'details'},
+            id: {path: 'rule_id'},
+            desc: '',
+            impact: 0.5,
+            results: [
+              {
+                status: ExecJSON.ControlResultStatus.Failed,
+                code_desc: {path: 'code'},
+                message: {transformer: formatMessage},
+                start_time: ''
+              }
+            ]
+          }
+        ],
+        sha256: ''
+      }
+    ]
+  };
+  constructor(gosecJson: string, withRaw = false) {
+    super(JSON.parse(gosecJson));
+    this.withRaw = withRaw;
+  }
+}
