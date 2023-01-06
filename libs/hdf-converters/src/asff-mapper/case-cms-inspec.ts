7d62849cc718c31b4e3f0f716cd3e79a1decc4ec
Amndeep Singh Mann <amann@mitre.org>
2023-01-05 22:44:20 -0500

    the v-number is sufficient to uniquely identify the control
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/hdf-converters/src/asff-mapper/case-cms-inspec.ts
+++ libs/hdf-converters/src/asff-mapper/case-cms-inspec.ts
@@ -2,7 +2,7 @@ import {encode} from 'html-entities';
 import _ from 'lodash';
 
 function findingId(finding: Record<string, unknown>): string {
-  return encode(_.get(finding, 'Id') as string);
+  return encode((_.get(finding, 'Id') as string).split('/').slice(-1)[0]);
 }
 
 export function getCMSInSpec(): Record<string, (...inputs: any) => any> {
