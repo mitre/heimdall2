7511443c2775569c994a99e72618c794b1a64086
Amndeep Singh Mann <amann@mitre.org>
2023-01-05 22:45:45 -0500

    don't need the product company + name in front of the title esp since it doesn't provide useful info since it's a custom upload which are always default/account_id
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/hdf-converters/src/asff-mapper/case-cms-inspec.ts
+++ libs/hdf-converters/src/asff-mapper/case-cms-inspec.ts
@@ -5,8 +5,13 @@ function findingId(finding: Record<string, unknown>): string {
   return encode((_.get(finding, 'Id') as string).split('/').slice(-1)[0]);
 }
 
+function doesNotHaveFindingTitlePrefix(): boolean {
+  return true;
+}
+
 export function getCMSInSpec(): Record<string, (...inputs: any) => any> {
   return {
-    findingId
+    findingId,
+    doesNotHaveFindingTitlePrefix
   };
 }
