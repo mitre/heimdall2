5f2cbd1db46c6f1e1faa286ff76c7ca716b351c6
David Markowski <dmarkowski@mitre.org>
2023-01-31 20:15:35 -0500

    adjusted tests to fit new options
    Signed-off-by: David Markowski <dmarkowski@mitre.org>

--- libs/hdf-converters/test/mappers/forward/checklist_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/checklist_mapper.spec.ts
@@ -1,13 +1,17 @@
 import fs from 'fs';
-import {ChecklistResults, checklistSupplementalInfo} from '../../../src/checklist-mapper';
+import {ExecJSON} from 'inspecjs';
+import {
+  ChecklistResults,
+  checklistSupplementalInfo
+} from '../../../src/checklist-mapper';
 import {omitVersions} from '../../utils';
 
 describe('checklist_mapper_single_stig_default', () => {
   it('Successfully converts Checklists', () => {
     const supplementalInfo: checklistSupplementalInfo = {
       filename: 'RHEL8V1R3.ckl',
-      intakeType: 'default',
-    }
+      intakeType: 'default'
+    };
     const mapper = new ChecklistResults(
       fs.readFileSync(
         'sample_jsons/checklist_mapper/sample_input_report/RHEL8V1R3.ckl',
@@ -21,47 +25,113 @@ describe('checklist_mapper_single_stig_default', () => {
       JSON.stringify(mapper.toHdf(), null, 2)
     );
 
-    expect(omitVersions(mapper.toHdf())).toEqual(
-      omitVersions(
-        JSON.parse(
-          fs.readFileSync(
-            'sample_jsons/checklist_mapper/checklist-RHEL8V1R3-hdf.json',
-            {
-              encoding: 'utf-8'
-            }
+    const results = mapper.toHdf();
+
+    expect(Array.isArray(results)).toBe(false);
+
+    if (!Array.isArray(results)) {
+      expect(omitVersions(results)).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              'sample_jsons/checklist_mapper/checklist-RHEL8V1R3-hdf.json',
+              {encoding: 'utf-8'}
+            )
           )
         )
-      )
-    );
+      );
+    }
   });
 });
 
-describe('checklist_mapper_withraw', () => {
-  it('Successfully converts withRaw flagged Checklist', () => {
-    const mapper = new ChecklistMapper(
+describe('checklist_mapper_multi_stig_wrapper', () => {
+  it('Successfully converts Checklists', () => {
+    const supplementalInfo: checklistSupplementalInfo = {
+      filename: 'three_stig_checklist.ckl',
+      intakeType: 'wrapper'
+    };
+    const mapper = new ChecklistResults(
       fs.readFileSync(
-        'sample_jsons/checklist_mapper/sample_input_report/RHEL8V1R3.ckl',
+        'sample_jsons/checklist_mapper/sample_input_report/three_stig_checklist.ckl',
         {encoding: 'utf-8'}
       ),
-      true
+      supplementalInfo
     );
 
     fs.writeFileSync(
-      'sample_jsons/checklist_mapper/checklist-RHEL8V1R3-hdf-withraw.json',
+      'sample_jsons/checklist_mapper/three_stig_checklist-hdf.json',
       JSON.stringify(mapper.toHdf(), null, 2)
     );
 
-    expect(omitVersions(mapper.toHdf())).toEqual(
-      omitVersions(
-        JSON.parse(
-          fs.readFileSync(
-            'sample_jsons/checklist_mapper/checklist-RHEL8V1R3-hdf-withraw.json',
-            {
-              encoding: 'utf-8'
-            }
+    const results = mapper.toHdf();
+
+    expect(Array.isArray(results)).toBe(false);
+
+    if (!Array.isArray(results)) {
+      expect(omitVersions(results)).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              'sample_jsons/checklist_mapper/three_stig_checklist-hdf.json',
+              {encoding: 'utf-8'}
+            )
           )
         )
-      )
+      );
+    }
+  });
+});
+
+describe('checklist_mapper_multi_stig_split', () => {
+  it('Successfully converts Checklists', () => {
+    const supplementalInfo: checklistSupplementalInfo = {
+      filename: 'three_stig_checklist.ckl',
+      intakeType: 'split'
+    };
+    const mapper = new ChecklistResults(
+      fs.readFileSync(
+        'sample_jsons/checklist_mapper/sample_input_report/three_stig_checklist.ckl',
+        {encoding: 'utf-8'}
+      ),
+      supplementalInfo
     );
+
+    const results = mapper.toHdf();
+    // if (Array.isArray(results)) {
+    //   results.forEach((result, index) => {
+    //     fs.writeFileSync(
+    //       `sample_jsons/checklist_mapper/sample_input_report/three_stig_checklist-hdf-${index + 1}.json`,
+    //       JSON.stringify(result, null, 2)
+    //     )
+    //   })
+    // }
+    const expectedSet = [
+      JSON.parse(
+        fs.readFileSync(
+          'sample_jsons/checklist_mapper/sample_input_report/three_stig_checklist-hdf-1.json',
+          {encoding: 'utf-8'}
+        )
+      ),
+      JSON.parse(
+        fs.readFileSync(
+          'sample_jsons/checklist_mapper/sample_input_report/three_stig_checklist-hdf-2.json',
+          {encoding: 'utf-8'}
+        )
+      ),
+      JSON.parse(
+        fs.readFileSync(
+          'sample_jsons/checklist_mapper/sample_input_report/three_stig_checklist-hdf-3.json',
+          {encoding: 'utf-8'}
+        )
+      )
+    ];
+
+    expect(Array.isArray(results)).toBe(true);
+
+    if (Array.isArray(results)) {
+      expect(results.map((result) => omitVersions(result))).toEqual(
+        expectedSet.map((result: ExecJSON.Execution) => omitVersions(result))
+      );
+    }
   });
 });
