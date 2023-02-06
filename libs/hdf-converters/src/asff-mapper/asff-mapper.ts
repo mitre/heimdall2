e7476475c12e804e61eb7b5d321b3077d4a99f60
Amndeep Singh Mann <amann@mitre.org>
2023-02-06 17:58:46 -0500

    would not believe that it was an array without all the extra stuff
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/hdf-converters/src/asff-mapper/asff-mapper.ts
+++ libs/hdf-converters/src/asff-mapper/asff-mapper.ts
@@ -661,14 +661,18 @@ export class ASFFMapper extends BaseConverter {
   };
 
   statusReason(finding: unknown): string | undefined {
-    return _.get(finding, 'Compliance.StatusReasons')
-      ?.map((reason: Record<string, string>) =>
-        Object.entries(reason || {}).map(([key, value]: [string, string]) => {
-          return `${encode(key)}: ${encode(value)}`;
-        })
-      )
-      .flat()
-      .join('\n');
+    const statusReasons = _.get(finding, 'Compliance.StatusReasons') as Record<string, string>[] | unknown;
+    if(statusReasons !== undefined && statusReasons !== null && _.isArray(statusReasons)) {
+      return statusReasons.map((reason: Record<string, string>) =>
+          Object.entries(reason || {}).map(([key, value]: [string, string]) => {
+            return `${encode(key)}: ${encode(value)}`;
+          })
+        )
+        .flat()
+        .join('\n');
+    } else {
+      return undefined;
+    }
   }
 
   setMappings(): void {
