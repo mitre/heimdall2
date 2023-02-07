d828cba3be879ce4e7e83c69482b80835ca4fd50
Amndeep Singh Mann <amann@mitre.org>
2023-02-07 02:28:21 -0500

    hopefully the cypress tests work now after changing the function to blindly delete all the users instead of having a where statement
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/backend/src/users/users.controller.ts
+++ apps/backend/src/users/users.controller.ts
@@ -25,7 +25,7 @@ import {LoggingInterceptor} from '../interceptors/logging.interceptor';
 import {PasswordChangePipe} from '../pipes/password-change.pipe';
 import {PasswordComplexityPipe} from '../pipes/password-complexity.pipe';
 import {PasswordsMatchPipe} from '../pipes/passwords-match.pipe';
-import {User} from '../users/user.model';
+import {User} from './user.model';
 import {CreateUserDto} from './dto/create-user.dto';
 import {DeleteUserDto} from './dto/delete-user.dto';
 import {SlimUserDto} from './dto/slim-user.dto';
@@ -151,6 +151,6 @@ export class UsersController {
   @UseGuards(TestGuard)
   @Post('/clear')
   async clear(): Promise<void> {
-    User.destroy({where: {}});
+    User.truncate();
   }
 }
