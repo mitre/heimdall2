e78a8df59b59e8ba7817702ce5561c7560aba15d
HenryXiaoHX <hxiao@ucsd.edu>
2023-02-16 13:20:51 -0500

    experiment with shim files, run lint

--- apps/frontend/src/shims-vue.d.ts
+++ apps/frontend/src/shims-vue.d.ts
@@ -1,7 +1,5 @@
 import Vue from 'vue';
-declare module '*.vue' {
-  export default Vue;
-}
+declare module '*.vue';
 declare module 'vue/types/options' {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface ComponentOptions<V extends Vue> {
