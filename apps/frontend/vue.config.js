0cea59197010ffaf5cd523dbb4cd21a718c93f2a
HenryXiaoHX <hxiao@ucsd.edu>
2023-02-28 13:57:57 -0500

    fix syntax for babel preset

--- apps/frontend/vue.config.js
+++ apps/frontend/vue.config.js
@@ -48,7 +48,7 @@ module.exports = {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env', {modules: false}]
+              presets: [['@babel/preset-env', {modules: false}]]
             }
           }
         }
