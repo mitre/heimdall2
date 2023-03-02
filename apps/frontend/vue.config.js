b8b1c4071ab5115f1b36ca9ca005a1919997f5dc
HenryXiaoHX <hxiao@ucsd.edu>
2023-03-02 18:00:46 -0500

    revert last commit, experiment different plugin

--- apps/frontend/vue.config.js
+++ apps/frontend/vue.config.js
@@ -48,8 +48,16 @@ module.exports = {
           use: {
             loader: 'babel-loader',
             options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['transform-remove-strict-mode']
+              presets: ['@babel/preset-env', {modules: 'commonjs'}],
+              plugins: [
+                [
+                  '@babel/plugin-transform-modules-commonjs',
+                  {
+                    loose: true,
+                    strictMode: false
+                  }
+                ]
+              ]
             }
           }
         }
