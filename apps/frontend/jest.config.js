35ef6680eafaa73db67641b9fbae531ace85c6d3
Amndeep Singh Mann <amann@mitre.org>
2023-04-07 15:49:57 -0400

    specify a single source of jest configuration
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/frontend/jest.config.js
+++ apps/frontend/jest.config.js
@@ -3,9 +3,10 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   testMatch: ['<rootDir>/tests/unit/*.spec.ts'],
   testPathIgnorePatterns: ['<rootDir>/src/__mocks__/*'],
-  moduleFileExtensions: ['vue', 'ts', 'tsx', 'js', 'jsx'],
+  moduleFileExtensions: ['vue', 'ts', 'tsx', 'js', 'jsx', 'json'],
   testEnvironment: 'jsdom',
   cacheDirectory: '<rootDir>/.cache/unit',
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   transform: {
     '.*\\.(vue)$': 'vue-jest',
     '^.+\\.tsx?$': 'ts-jest',
@@ -15,6 +16,7 @@ module.exports = {
   moduleNameMapper: {
     '^d3$': '<rootDir>/tests/util/d3.js',
     '^@[/](.+)': '<rootDir>/src/$1',
-    '^.+\\.(css)$': '<rootDir>/tests/util/cssTransform.js'
+    '^.+\\.(css)$': '<rootDir>/tests/util/cssTransform.js',
+    '^axios$': 'axios/dist/node/axios.cjs'
   }
 };
