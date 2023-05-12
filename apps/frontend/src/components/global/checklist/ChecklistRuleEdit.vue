f31cdcfd98137f7c3f3cf1946c3c476af5c99c9d
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-05-12 14:44:56 -0400

    Split up checklist file into components
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

---
+++ apps/frontend/src/components/global/checklist/ChecklistRuleEdit.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-card class="mt-3 pt-4" height="42vh">
+    <v-card-text>
+      <v-row>
+        <v-col>
+          <v-select
+            v-model="selectedRule.status"
+            dense
+            label="Status"
+            :items="statusItems"
+            item-text="name"
+            item-value="value"
+            :item-color="statusColor(selectedRule.status)"
+          />
+        </v-col>
+        <v-col>
+          <v-select
+            v-model="selectedRule.severityOverride"
+            dense
+            label="Severity Override"
+            item-text="name"
+            item-value="value"
+            :items="checkPossibleOverrides(selectedRule.severity)"
+            @change="promptSeverityJustification"
+          />
+        </v-col>
+      </v-row>
+      <v-row class="mt-n8">
+        <v-col>
+          <strong>Finding Details: </strong><br />
+          <v-textarea
+            v-model="selectedRule.findingDetails"
+            solo
+            outlined
+            dense
+            no-resize
+            height="12vh"
+          />
+        </v-col>
+      </v-row>
+      <v-row class="mt-n10">
+        <v-col>
+          <strong>Comments: </strong>
+          <v-textarea
+            v-model="selectedRule.comments"
+            solo
+            outlined
+            dense
+            no-resize
+            height="8vh"
+          />
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import _ from 'lodash';
+import {ChecklistVuln} from '@mitre/hdf-converters';
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component
+export default class ChecklistRuleInfoBody extends Vue {
+  @Prop({type: Object, required: true}) selectedRule!: ChecklistVuln;
+  @Prop({type: Boolean, required: true}) sheet!: Boolean;
+
+  statusItems = [
+    {name: 'Passed', value: 'Passed'},
+    {name: 'Failed', value: 'Failed'},
+    {name: 'Not Applicable', value: 'Not Applicable'},
+    {name: 'Not Reviewed', value: 'Not Reviewed'}
+  ];
+
+  severityOverrideItems = [
+    {name: 'High', value: 'high'},
+    {name: 'Medium', value: 'medium'},
+    {name: 'Low', value: 'low'},
+    {name: '(Default)', value: ''}
+  ];
+
+  statusColor(status: string | undefined) {
+    switch (status) {
+      case 'Passed':
+        return 'statusPassed';
+      case 'Not Applicable':
+        return 'statusNotApplicable';
+      case 'Failed':
+        return 'statusFailed';
+      default: // Not_Reviewed
+        return 'statusNotReviewed';
+    }
+  }
+
+  checkPossibleOverrides(severity: string) {
+    return this.severityOverrideItems.filter((item) => item.value !== severity);
+  }
+
+  promptSeverityJustification() {
+    if (this.selectedRule.severityOverride === '') {
+      this.selectedRule.severityJustification =
+        'Returning to default severity.';
+    } else {
+      this.selectedRule.severityJustification = '';
+    }
+    this.$emit('enable-sheet');
+  }
+}
+</script>
---
+++ apps/frontend/src/components/global/checklist/ChecklistRuleInfoHeader.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card height="13vh" class="overflow-y-auto">
+    <v-card-text class="text-center">
+      <h3 class="d-inline-block text-truncate mx-2" style="max-width: 100%">
+        {{ selectedRule.stigRef }}
+      </h3>
+      <v-row dense class="mt-n2 mt-xl-3">
+        <v-col :cols="4">
+          <div>
+            <span class="text-overline white--text">Vul ID: </span>
+            {{ selectedRule.vulnNum }}
+          </div>
+        </v-col>
+        <v-col :cols="4">
+          <div>
+            <span class="text-overline white--text">Rule ID: </span>
+            {{ shortRuleId(selectedRule.ruleId) }}
+          </div>
+        </v-col>
+        <v-col :cols="4">
+          <div>
+            <span class="text-overline white--text">STIG ID: </span>
+            {{ shortStigId(selectedRule.ruleVersion) }}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row dense class="mt-n2 mt-xl-2">
+        <v-col :cols="4">
+          <div>
+            <span class="text-overline white--text">Severity: </span
+            >{{ selectedRule.severity }}
+          </div>
+        </v-col>
+        <v-col :cols="4">
+          <div>
+            <span class="text-overline white--text"> Classification: </span>
+            {{ selectedRule.class }}
+          </div>
+        </v-col>
+        <v-col :cols="4">
+          <div>
+            <span class="text-overline white--text">Legacy IDs: </span
+            >{{ selectedRule.legacyId }}
+          </div>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import {ChecklistVuln} from '@mitre/hdf-converters';
+import _ from 'lodash';
+import {Component, Prop, Vue} from 'vue-property-decorator';
+
+@Component
+export default class ChecklistRuleInfoHeader extends Vue {
+  @Prop({type: Object, required: true}) readonly selectedRule!: ChecklistVuln;
+  @Prop({type: Boolean, required: true}) readonly shortIdEnabled!: Boolean;
+
+  shortRuleId(ruleId: string) {
+    if (this.shortIdEnabled) return ruleId.split('r')[0] || ruleId;
+    else return ruleId;
+  }
+
+  shortStigId(stigId: string) {
+    if (this.shortIdEnabled) return stigId.split('-').slice(0, 2).join('-');
+    else return stigId;
+  }
+}
+</script>
