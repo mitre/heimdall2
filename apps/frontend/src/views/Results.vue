019661548dddb2d7387c7d9e11c8e367455f1a14
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-05-23 16:05:55 -0400

    Changed out hamburger menu for right arrow
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- apps/frontend/src/views/Results.vue
+++ apps/frontend/src/views/Results.vue
@@ -189,8 +189,8 @@
       </span>
       <span v-else class="subtitle-2">
         No files are currently enabled for viewing. Open the
-        <v-icon class="mx-1">mdi-menu</v-icon> sidebar menu, and ensure that the
-        file(s) you wish to view are
+        <v-icon class="mx-1">mdi-arrow-right</v-icon> sidebar menu, and ensure
+        that the file(s) you wish to view are
         <v-icon class="mx-1">mdi-checkbox-marked</v-icon> checked.
       </span>
     </v-snackbar>
