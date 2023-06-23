587282d95610b920b33f6f681d22bd8a6f4718ce
Amndeep Singh Mann <amann@mitre.org>
2023-06-23 01:54:59 -0400

    make the nist family part of the regexes only match against valid nist control families
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/inspecjs/src/nist.ts
+++ libs/inspecjs/src/nist.ts
@@ -2,9 +2,10 @@ import {ControlStatus} from './compat_wrappers';
 import {ALL_NIST_CONTROL_NUMBERS, ALL_NIST_FAMILIES} from './raw_nist';
 
 // Regexes.
-const NIST_FAMILY_RE = /^[A-Z]{2}$/;
+// Matches against only currently existing NIST Control families instead of allowing any two adjacent uppercase letters
+const NIST_FAMILY_RE = /^(?:A[CPRTU]|C[AMP]|D[IM]|I[APR]|M[AP]|P[ELMS]|RA|S[ACEI]|TR|U[LM])$/;
 // Limit length of children to avoid potential DoS on malicious NIST Control strings
-const NIST_CONTROL_RE = /^([A-Z]{2})-([0-9]+)(.{0,60})$/;
+const NIST_CONTROL_RE = /^(A[CPRTU]|C[AMP]|D[IM]|I[APR]|M[AP]|P[ELMS]|RA|S[ACEI]|TR|U[LM])-([0-9]+)(.{0,60})$/;
 const SPEC_SPLITTER = /[\s\(\)\.]+/; // Includes all whitespace, periods, and parenthesis
 const REV_RE = /^rev[\s_.]+(\d+)$/i; // Matches Rev_5 etc
 type ParseNist = NistControl | NistRevision | null;
