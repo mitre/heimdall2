6f5f1394f971d6610692d7b7674cbcadf1f7863f
Rlin232 <rlin@mitre.org>
2023-07-11 14:46:24 -0700

    Instantialize groups in service mock
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- apps/backend/test/constants/groups-test.constant.ts
+++ apps/backend/test/constants/groups-test.constant.ts
@@ -23,15 +23,15 @@ export const UPDATE_GROUP: CreateGroupDto = {
 export const GROUPS_SERVICE_MOCK = {
   async findAll(): Promise<Group[]> { return []; },
   async count(): Promise<number> { return 1 },
-  async findByPkBang(id: string): Promise<Group> { return new Group },
+  async findByPkBang(id: string): Promise<Group> { return new Group() },
   async findByIds(id: string[]): Promise<Group[]> { return [] },
   async addUserToGroup(group: Group, user: User, role: string): Promise<void> { },
   async updateGroupUserRole(group: Group, updateGroupUser: UpdateGroupUserRoleDto): Promise<GroupUser | undefined> { return undefined },
-  async removeUserFromGroup(group: Group, user: User): Promise<Group> { return new Group },
+  async removeUserFromGroup(group: Group, user: User): Promise<Group> { return new Group() },
   async setDefaultToOwner(): Promise<void> { },
   async addEvaluationToGroup(group: Group, evaluation: Evaluation): Promise<void> { },
-  async removeEvaluationFromGroup(group: Group, evaluation: Evaluation): Promise<Group> { return new Group },
-  async create(createGroupDto: CreateGroupDto): Promise<Group> { return new Group },
-  async update(groupToUpdate: Group, groupDto: CreateGroupDto): Promise<Group> { return new Group },
-  async remove(groupToDelete: Group): Promise<Group> { return new Group }
+  async removeEvaluationFromGroup(group: Group, evaluation: Evaluation): Promise<Group> { return new Group() },
+  async create(createGroupDto: CreateGroupDto): Promise<Group> { return new Group() },
+  async update(groupToUpdate: Group, groupDto: CreateGroupDto): Promise<Group> { return new Group() },
+  async remove(groupToDelete: Group): Promise<Group> { return new Group() }
 };
