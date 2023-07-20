21a29eaa948d7ea82be3b53789ca3c692b98db50
Charles Hu <chu@mitre.org>
2023-07-20 14:39:25 -0400

    Added Netsparker smaple file
    Signed-off-by: Charles Hu <chu@mitre.org>

--- libs/hdf-converters/test/mappers/forward/netsparker_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/netsparker_mapper.spec.ts
@@ -1,4 +1,4 @@
-/*import fs from 'fs';
+import fs from 'fs';
 import {NetsparkerMapper} from '../../../src/netsparker-mapper';
 import {omitVersions} from '../../utils';
 
@@ -6,22 +6,25 @@ describe('netsparker_mapper_check', () => {
   it('Successfully converts Netsparker scan targeted at a local/cloned repository data', () => {
     const mapper = new NetsparkerMapper(
       fs.readFileSync(
-        'sample_jsons/netsparker_mapper/sample_input_report/{HERE}',
+        'sample_jsons/netsparker_mapper/sample_input_report/sample-netsparker-invicti.xml',
         {encoding: 'utf-8'}
       )
     );
 
     // fs.writeFileSync(
-    //   'sample_jsons/netsparker_mapper/sample_input_report/{HERE}',
+    //   'sample_jsons/netsparker_mapper/netsparker-hdf.json',
     //   JSON.stringify(mapper.toHdf(), null, 2)
     // );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
         JSON.parse(
-          fs.readFileSync('sample_jsons/netsparker_mapper/{HERE}', {
-            encoding: 'utf-8'
-          })
+          fs.readFileSync(
+            'sample_jsons/netsparker_mapper/netsparker-hdf.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
         )
       )
     );
@@ -32,26 +35,28 @@ describe('netsparker_mapper_check_withraw', () => {
   it('Successfully converts withRaw flagged Netsparker scan', () => {
     const mapper = new NetsparkerMapper(
       fs.readFileSync(
-        'sample_jsons/netsparker_mapper/sample_input_report/{HERE}',
+        'sample_jsons/netsparker_mapper/sample_input_report/sample-netsparker-invicti.xml',
         {encoding: 'utf-8'}
       ),
       true
     );
 
     // fs.writeFileSync(
-    //   'sample_jsons/netsparker_mapper/{HERE}',
+    //   'sample_jsons/netsparker_mapper/netsparker-hdf-withraw.json',
     //   JSON.stringify(mapper.toHdf(), null, 2)
     // );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
         JSON.parse(
-          fs.readFileSync('sample_jsons/netsparker_mapper/{HERE}', {
-            encoding: 'utf-8'
-          })
+          fs.readFileSync(
+            'sample_jsons/netsparker_mapper/netsparker-hdf-withraw.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
         )
       )
     );
   });
 });
-*/
