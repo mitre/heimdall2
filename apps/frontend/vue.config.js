7f20a6a566e03ebfe19e8407fb736fbadb09b32b
Charles Hu <chu@mitre.org>
2023-07-26 17:00:30 -0400

    Remove jQuery dependency
    Signed-off-by: Charles Hu <chu@mitre.org>

--- apps/frontend/vue.config.js
+++ apps/frontend/vue.config.js
@@ -14,8 +14,6 @@ const issues = parsed.issues || '';
 
 // This grabs the js/css to allow for HTML export
 const files = {
-  [require.resolve('jquery/dist/jquery.min.js')]:
-    'public/static/export/jquery.min.js',
   [require.resolve('tw-elements/dist/js/tw-elements.umd.min.js')]:
     'public/static/export/tw-elements.min.js'
 };
