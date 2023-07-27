36a9ae22552f8d6fa0fa1b63f23dda2f99e245e9
Rlin232 <rlin@mitre.org>
2023-07-27 08:47:02 -0700

    Fixed inconsistent naming conventions and minor typos in migration script
    Signed-off-by: Rlin232 <rlin@mitre.org>

--- apps/backend/migrations/20230725092535-change-group-names-to-unique.js
+++ apps/backend/migrations/20230725092535-change-group-names-to-unique.js
@@ -7,8 +7,8 @@ module.exports = {
     LANGUAGE plpgsql
     AS $$
     DECLARE
-      check_name boolean := true;
-      offSet_cnt INTEGER :=1;
+      check_name BOOLEAN := true;
+      offset_cnt INTEGER := 1;
       processing_group TEXT := '';
       rec_group RECORD;
       dup_names NO SCROLL CURSOR FOR
@@ -22,27 +22,27 @@ module.exports = {
       LOOP
         -- retrieve a row of data from the result set (cursor) into the rec_group variable
         FETCH dup_names INTO rec_group;
-      -- exit when no more records are available in the resul set
+      -- exit when no more records are available in the result set
         EXIT WHEN NOT FOUND;
 
       -- Skip the first duplicate name (we only want to update subsequent dup names)
         IF rec_group.name = processing_group THEN
         -- Determine the next non-duplicate name
           WHILE check_name = true LOOP
-            IF (SELECT EXISTS (SELECT name FROM "Groups" WHERE name = CONCAT(rec_group.name, '-', offSet_cnt))::int = 1) THEN
-              offSet_cnt := offSet_cnt + 1;
+            IF (SELECT EXISTS (SELECT name FROM "Groups" WHERE name = CONCAT(rec_group.name, '-', offset_cnt))::int = 1) THEN
+              offset_cnt := offset_cnt + 1;
             ELSE
               check_name := false;
             END IF;
           END LOOP;
           -- Uncomment the next line when debugging
-          -- RAISE NOTICE 'Updating % id % to %', rec_group.name, rec_group.id, CONCAT(rec_group.name, '-', offSet_cnt);
+          -- RAISE NOTICE 'Updating % id % to %', rec_group.name, rec_group.id, CONCAT(rec_group.name, '-', offset_cnt);
 
         -- Update the table (comment when debugging)
-          UPDATE "Groups" SET name = CONCAT(rec_group.name, '-', offSet_cnt) WHERE id = rec_group.id;
+          UPDATE "Groups" SET name = CONCAT(rec_group.name, '-', offset_cnt) WHERE id = rec_group.id;
 
         -- reset the controlling variables
-          offSet_cnt :=1;
+          offset_cnt :=1;
           check_name = true;
       -- Set the group name flag to the name we are processing
         ELSE
@@ -69,6 +69,8 @@ module.exports = {
     return queryInterface.sequelize.transaction((t) => {
       return Promise.all([
         queryInterface.changeColumn('Groups', 'name', {
+          type: Sequelize.STRING,
+          allowNull: false,
           unique: false
         }, { transaction: t })
       ])
