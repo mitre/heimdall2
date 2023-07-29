78a1f8ed5e92977cf7c74ca01ec1f9f6fcb1ac95
Nikhil Sadasivan <nikhilsk97@gmail.com>
2023-07-29 15:31:47 +0530

    Added tags and controlstatus.failed to results. Need to fix impact scores next.

--- libs/hdf-converters/src/grype-mapper.ts
+++ libs/hdf-converters/src/grype-mapper.ts
@@ -1,8 +1,14 @@
 import {ExecJSON} from 'inspecjs';
 import {version as HeimdallToolsVersion} from '../package.json';
 import {BaseConverter, ILookupPath, MappedTransform} from './base-converter';
+import {CweNistMapping} from './mappings/CweNistMapping';
+import {
+  DEFAULT_STATIC_CODE_ANALYSIS_NIST_TAGS,
+  getCCIsForNISTTags
+} from './utils/global';
 
 const PROFILE_NAME = 'GRYPE';
+const CWE_NIST_MAPPING = new CweNistMapping();
 
 function convertToControlMapping(): MappedTransform<
   ExecJSON.Control,
@@ -11,15 +17,30 @@ function convertToControlMapping(): MappedTransform<
   return {
     id: {path: 'vulnerability.id'},
     title: {path: 'artifact.name'},
-    desc: {path: 'relatedVulnerabilities[0].description'}, //does relatedvulnerabilities always have the same cve as the vulnerability as the first element? check this
-    impact: {path: ''}, //to do
-    refs: [],
-    tags: {}, // to do
+    desc: {path: 'vulnerability.description'},
+    impact: {path: 'relatedVulnerabilities[0].cvss.metrics.impactScore'}, //to do
+    refs: [], //to do, available in postgres.json as relatedVunerabilites.urls and vulnerability.dataSource
+    tags: {
+      cci: getCCIsForNISTTags(nistTag()),
+      nist: nistTag()
+    }, // to do
     source_location: {
       ref: {path: 'artifact.locations[0].path'} //multiple locations can be provided by grype, but only one goes into hdf
     },
-    results: []
+    results: [
+      {
+        status: ExecJSON.ControlResultStatus.Failed,
+        code_desc: {path: 'vulnerability.description'}, //need to fix this to return data from matchDetails (probably just matched by 'matcher' and type of match given by 'type')
+        start_time: {path: 'descriptor.timestamp'}
+      }
+    ]
   };
+}
+function nistTag(): string[] {
+  return CWE_NIST_MAPPING.nistFilter(
+    [],
+    DEFAULT_STATIC_CODE_ANALYSIS_NIST_TAGS
+  );
 }
 export class GrypeMapper extends BaseConverter {
   mappings: MappedTransform<
