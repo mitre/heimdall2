336cfb257a16d36b7e47ea76d802a399e4424cbe
Charles Hu <chu@mitre.org>
2023-08-01 12:22:14 -0400

    Login test overhaul
    Signed-off-by: Charles Hu <chu@mitre.org>

--- test/integration/login.cy.ts
+++ test/integration/login.cy.ts
@@ -1,3 +1,5 @@
+/*eslint-disable cypress/no-async-tests*/
+
 import {
   BAD_LDAP_AUTHENTICATION,
   BAD_LOGIN_AUTHENTICATION,
@@ -19,20 +21,20 @@ context('Login', () => {
   const toastVerifier = new ToastVerifier();
   const userModalVerifier = new UserModalVerifier();
 
-  // Run before each test
-  beforeEach(() => {
-    cy.register(CREATE_USER_DTO_TEST_OBJ);
-    cy.visit('/login');
-  });
-
   // The test
   describe('Login Form', () => {
-    it('authenticates a user with valid credentials', () => {
+    it('authenticates a user with valid credentials', async () => {
+      cy.register(CREATE_USER_DTO_TEST_OBJ);
+      cy.visit('/login');
+
       loginPageVerifier.loginFormPresent();
       cy.login(LOGIN_AUTHENTICATION);
       toastVerifier.toastTextContains('You have successfully signed in.');
     });
-    it('authenticates a github oauth user', () => {
+    it('authenticates a github oauth user', async () => {
+      cy.register(CREATE_USER_DTO_TEST_OBJ);
+      cy.visit('/login');
+
       loginPageVerifier.loginFormPresent();
       loginPage.loginOauth('github');
       // Open the user modal
@@ -40,13 +42,19 @@ context('Login', () => {
       // Make sure all the fields exist
       userModalVerifier.verifyFieldsExist();
     });
-    it('authenticates an ldap user with valid credentials', () => {
+    it('authenticates an ldap user with valid credentials', async () => {
+      cy.register(CREATE_USER_DTO_TEST_OBJ);
+      cy.visit('/login');
+
       loginPage.switchToLDAPAuth();
       loginPageVerifier.ldapLoginFormPresent();
       loginPage.ldapLogin(LDAP_AUTHENTICATION);
       toastVerifier.toastTextContains('You have successfully signed in.');
     });
-    it('authenticates an oidc user', () => {
+    it('authenticates an oidc user', async () => {
+      cy.register(CREATE_USER_DTO_TEST_OBJ);
+      cy.visit('/login');
+
       loginPage.loginOauth('oidc');
       // Open the user modal
       dropdown.openUserModal();
@@ -55,8 +63,11 @@ context('Login', () => {
     });
 
     describe('Authentication Failures', () => {
-      // Ignore 401 errors on authentication failure tests
-      beforeEach(() => {
+      it('fails to authenticate a user with invalid credentials', async () => {
+        cy.register(CREATE_USER_DTO_TEST_OBJ);
+        cy.visit('/login');
+
+        // Ignore 401 errors on authentication failure tests
         cy.on('uncaught:exception', (err) => {
           expect(err.response.status).to.equal(401);
 
@@ -64,13 +75,23 @@ context('Login', () => {
           // failing this test
           return false;
         });
-      });
 
-      it('fails to authenticate a user with invalid credentials', () => {
         cy.login(BAD_LOGIN_AUTHENTICATION);
         toastVerifier.toastTextContains('Incorrect Username or Password');
       });
-      it('fails to authenticate an ldap user with invalid credentials', () => {
+      it('fails to authenticate an ldap user with invalid credentials', async () => {
+        cy.register(CREATE_USER_DTO_TEST_OBJ);
+        cy.visit('/login');
+
+        // Ignore 401 errors on authentication failure tests
+        cy.on('uncaught:exception', (err) => {
+          expect(err.response.status).to.equal(401);
+
+          // return false to prevent the error from
+          // failing this test
+          return false;
+        });
+
         loginPage.switchToLDAPAuth();
         loginPage.ldapLogin(BAD_LDAP_AUTHENTICATION);
         toastVerifier.toastTextContains('Unauthorized');
@@ -79,7 +100,10 @@ context('Login', () => {
   });
 
   describe('Logout Button', () => {
-    it('sucessfully logs a user out', () => {
+    it('sucessfully logs a user out', async () => {
+      cy.register(CREATE_USER_DTO_TEST_OBJ);
+      cy.visit('/login');
+
       cy.login(LOGIN_AUTHENTICATION);
       dropdown.logout();
       loginPageVerifier.loginFormPresent();
