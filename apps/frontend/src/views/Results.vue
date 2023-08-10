2e0d2921eab01f357f14bd0b2b2ad7e24e83131e
Charles Hu <chu@mitre.org>
2023-08-10 16:08:01 -0400

    Deconflict HTML export
    Signed-off-by: Charles Hu <chu@mitre.org>

--- apps/frontend/src/views/Results.vue
+++ apps/frontend/src/views/Results.vue
@@ -36,6 +36,12 @@
               <ExportCSVModal :filter="all_filter" />
             </v-list-item>
             <v-list-item v-if="is_result_view" class="px-0">
+              <ExportHTMLModal
+                :filter="all_filter"
+                :file-type="current_route_name"
+              />
+            </v-list-item>
+            <v-list-item v-if="is_result_view" class="px-0">
               <ExportSplunkModal />
             </v-list-item>
             <v-list-item class="px-0">
@@ -204,6 +210,7 @@ import ExportASFFModal from '@/components/global/ExportASFFModal.vue';
 import ExportCaat from '@/components/global/ExportCaat.vue';
 import ExportCKLModal from '@/components/global/ExportCKLModal.vue';
 import ExportCSVModal from '@/components/global/ExportCSVModal.vue';
+import ExportHTMLModal from '@/components/global/ExportHTMLModal.vue';
 import ExportJson from '@/components/global/ExportJson.vue';
 import ExportNist from '@/components/global/ExportNist.vue';
 import ExportSplunkModal from '@/components/global/ExportSplunkModal.vue';
@@ -251,6 +258,7 @@ import {compare_times} from '../utilities/delta_util';
     ExportJson,
     ExportXCCDFResults,
     ExportCKLModal,
+    ExportHTMLModal,
     EvaluationInfo,
     ExportSplunkModal,
     ProfileData,
