e92209d828081dc7a486fe0874098e4c5954c78f
Charles Hu <chu@mitre.org>
2023-08-24 12:39:58 -0400

    Test frontend shim change
    Signed-off-by: Charles Hu <chu@mitre.org>

--- apps/frontend/src/shims-vue.d.ts
+++ apps/frontend/src/shims-vue.d.ts
@@ -1,10 +1,4 @@
-import Vue from 'vue';
 declare module '*.vue' {
+  import Vue from 'vue';
   export default Vue;
-}
-declare module 'vue/types/options' {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface ComponentOptions<V extends Vue> {
-    validations?: {[x: string]: object};
-  }
 }
