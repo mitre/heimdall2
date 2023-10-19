01ac6186cb38402a0972c0b415be43fc411999ca
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-10-19 12:58:20 -0400

    Added complex search for searching mapping
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- apps/backend/src/echo/echo.service.ts
+++ apps/backend/src/echo/echo.service.ts
@@ -1,110 +1,69 @@
 import {Injectable, NotFoundException} from '@nestjs/common';
 import fs from 'fs';
 import path from 'path';
-import {AnyProfile} from '../../../../libs/inspecjs/src/fileparse';
+import {searchOverallJsonMapping, SearchTerm} from './utils/complexSearch';
 // import {ApiKey} from './apikey.model';
 // import {APIKeyDto} from './dto/apikey.dto';
 
 @Injectable()
 export class EchoService {
-  // TODO: Fix typing below to be profileJsonsKeyValueMapping
-
-  private readonly profileJsons: {[key: string]: AnyProfile} =
+  private readonly profileJsons: {[key: string]: unknown} =
     generateOverallJsonMapping();
+  private readonly ttpMapping = mapping;
 
-  // TODO: Fix unknown below to be profileJsonsKeyValueMapping
   findControlByAttackPatternName(attackPatternName: string): {
-    [key: string]: AnyProfile;
+    [key: string]: {[key: string]: string};
   } {
-    console.log('Testing to see if this works');
     const profileContent = this.profileJsons;
-    fs.writeFileSync('./testingObj.json', JSON.stringify(this.profileJsons));
+
     if (profileContent === null) {
-      throw new NotFoundException("Couldn't find profile with given name");
+      throw new NotFoundException(
+        "Couldn't find profile with given name or no relevant controls exist"
+      );
     } else {
-      return profileContent;
+      return searchOverallJsonMapping(
+        this.ttpMapping[attackPatternName],
+        this.profileJsons
+      );
     }
   }
 }
 
 export const generateOverallJsonMapping = () => {
-  let allProfiles: {[key: string]: AnyProfile} = {};
+  let allProfiles: {[key: string]: unknown} = {};
   const jsonsInDir = fs
-    .readdirSync('./data/baselineProfiles')
+    .readdirSync('./data/baselineProfiles/')
     .filter((file) => path.extname(file) === '.json');
 
   jsonsInDir.forEach((file) => {
     const fileData = fs.readFileSync(
-      path.join('./data/baselineProfiles', file)
+      path.join('./data/baselineProfiles/', file)
     );
     const json = JSON.parse(fileData.toString());
+    json['github_url'] =
+      'https://github.com/mitre/' + file.replace('.json', '');
     allProfiles[json.name] = json;
   });
   return allProfiles;
 };
 
-// type Operator = "AND" | "OR";
-
-// interface KeyTerm {
-//   term: string;
-//   operator: Operator;
-//   attributes: string[];
-// }
-
-// interface Attributes {
-//   [attribute: string]: string;
-// }
-
-// interface Data {
-//   [key: string]: Attributes;
-// }
-
-// function searchStrings(data: Data, keyTerms: KeyTerm[]): string[] {
-//   const keys = Object.keys(data);
-
-//   return keys.filter((key) => {
-//     return keyTerms.reduce((acc: boolean, keyTerm: KeyTerm, index: number) => {
-//       const isPresent = keyTerm.attributes.some((attribute) => {
-//         const attributeValue = data[key][attribute];
-//         return attributeValue
-//           ? attributeValue.toLowerCase().includes(keyTerm.term.toLowerCase())
-//           : false;
-//       });
-
-//       if (index === 0) {
-//         return isPresent;
-//       } else if (keyTerm.operator === "AND") {
-//         return acc && isPresent;
-//       } else {
-//         return acc || isPresent;
-//       }
-//     }, false);
-//   });
-// }
-
-// const data: Data = {
-//   item1: {
-//     title: "This is a test sentence",
-//     description: "A simple test",
-//   },
-//   item2: {
-//     title: "Another test sentence",
-//     description: "Another simple test",
-//   },
-//   item3: {
-//     title: "Yet another test",
-//     description: "A different test",
-//   },
-//   item4: {
-//     title: "Something completely different",
-//     description: "Not a test",
-//   },
-// };
-
-// const keyTerms: KeyTerm[] = [
-//   { term: "test", operator: "OR", attributes: ["title", "description"] },
-//   { term: "sentence", operator: "AND", attributes: ["title"] },
-//   { term: "Another", operator: "OR", attributes: ["description"] },
-// ];
-
-// console.log(searchStrings(data, keyTerms));
+export const mapping: {
+  [key: string]: SearchTerm[];
+} = {
+  'T1548.003': [
+    {term: 'timestamp_timeout', type: 'OR'},
+    {term: 'tty_tickets', type: 'OR'},
+    {term: 'sudoers', type: 'AND'}
+  ],
+  T1134: [
+    {term: 'token', type: 'AND'},
+    {term: 'rights', type: 'AND'},
+    {term: 'create', type: 'AND'}
+  ],
+  'T1543.003': [
+    {term: 'drivers', type: 'AND'},
+    {term: 'windows', type: 'AND'},
+    {term: 'enforce', type: 'AND'}
+  ],
+  'T1547.009': [{term: 'Create symbolic link', type: 'OR'}]
+};
