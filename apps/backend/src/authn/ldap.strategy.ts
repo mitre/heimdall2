985478fb9feebbc823698fc365d3c2b0a7c76e9f
Amndeep Singh Mann <amann@mitre.org>
2023-10-26 18:54:11 -0400

    added support for LDAPS and appropriate envvars
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/backend/src/authn/ldap.strategy.ts
+++ apps/backend/src/authn/ldap.strategy.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@nestjs/common';
 import {PassportStrategy} from '@nestjs/passport';
 import {Request} from 'express';
+import * as fs from 'fs';
 import * as _ from 'lodash';
 import Strategy from 'passport-ldapauth';
 import {ConfigService} from '../config/config.service';
@@ -8,10 +9,70 @@ import {AuthnService} from './authn.service';
 
 @Injectable()
 export class LDAPStrategy extends PassportStrategy(Strategy, 'ldap') {
+  static getSSLConfig(configService: ConfigService) {
+    if (
+      !configService.get('LDAP_SSL') ||
+      configService.get('LDAP_SSL')?.toLowerCase() === 'false'
+    ) {
+      return false;
+    }
+
+    let sslKey, sslCert, sslCA;
+
+    if (typeof configService.get('LDAP_SSL_KEY') === 'string') {
+      if (configService.get('LDAP_SSL_KEY')?.indexOf('-BEGIN') !== -1) {
+        sslKey = configService.get('LDAP_SSL_KEY');
+      } else {
+        // Verify file exists
+        if (fs.statSync(configService.get('LDAP_SSL_KEY')!).isFile()) {
+          sslKey = fs.readFileSync(configService.get('LDAP_SSL_KEY')!);
+        } else {
+          throw new Error('SSL Key file does not exist');
+        }
+      }
+    }
+
+    if (typeof configService.get('LDAP_SSL_CERT') === 'string') {
+      if (configService.get('LDAP_SSL_CERT')?.indexOf('-BEGIN') !== -1) {
+        sslCert = configService.get('LDAP_SSL_CERT');
+      } else {
+        // Verify file exists
+        if (fs.statSync(configService.get('LDAP_SSL_CERT')!).isFile()) {
+          sslCert = fs.readFileSync(configService.get('LDAP_SSL_CERT')!);
+        } else {
+          throw new Error('SSL Cert file does not exist');
+        }
+      }
+    }
+
+    if (typeof configService.get('LDAP_SSL_CA') === 'string') {
+      if (configService.get('LDAP_SSL_CA')?.indexOf('-BEGIN') !== -1) {
+        sslCA = configService.get('LDAP_SSL_CA');
+      } else {
+        // Verify file exists
+        if (fs.statSync(configService.get('LDAP_SSL_CA')!).isFile()) {
+          sslCA = fs.readFileSync(configService.get('LDAP_SSL_CA')!);
+        } else {
+          throw new Error('SSL CA file does not exist');
+        }
+      }
+    }
+
+    return {
+      rejectUnauthorized:
+        configService.get('LDAP_SSL_INSECURE') &&
+        configService.get('LDAP_SSL_INSECURE')?.toLowerCase() !== 'true',
+      key: sslKey,
+      cert: sslCert,
+      ca: sslCA
+    };
+  }
+
   constructor(
     private readonly authnService: AuthnService,
     private readonly configService: ConfigService
   ) {
+    const sslConfig = LDAPStrategy.getSSLConfig(configService);
     super(
       {
         passReqToCallback: true,
@@ -25,7 +86,15 @@ export class LDAPStrategy extends PassportStrategy(Strategy, 'ldap') {
           searchFilter:
             configService.get('LDAP_SEARCHFILTER') ||
             '(sAMAccountName={{username}})',
-          passReqToCallback: true
+          passReqToCallback: true,
+          ...(sslConfig && {
+            tlsOptions: {
+              rejectUnauthorized: sslConfig.rejectUnauthorized,
+              ca: sslConfig.ca,
+              cert: sslConfig.cert,
+              key: sslConfig.key
+            }
+          })
         }
       },
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
