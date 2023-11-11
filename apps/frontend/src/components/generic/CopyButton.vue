0becbba93e6a8a67f9f6bbdfb1882e57bcb100e1
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-11-10 23:06:58 -0500

    Possible copy workaround for http
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- apps/frontend/src/components/generic/CopyButton.vue
+++ apps/frontend/src/components/generic/CopyButton.vue
@@ -16,11 +16,34 @@ export default class CopyButton extends Vue {
 
   async copy() {
     try {
-      navigator.clipboard.writeText(this.text);
+      if (navigator.clipboard) {
+        // If https, use the new API secure copy
+        navigator.clipboard.writeText(this.text);
+      } else {
+        // Otherwise fallback to the old API
+        this.unsecuredCopyToClipboard(this.text);
+      }
       SnackbarModule.notify('Text copied to your clipboard');
     } catch (e) {
       SnackbarModule.failure(`Failed to copy to your clipboard: ${e}`);
     }
+  }
+
+  unsecuredCopyToClipboard(text: string) {
+    const tempTextArea = document.createElement('textarea');
+    tempTextArea.value = text;
+
+    // Set the style of the textarea to make it invisible
+    tempTextArea.style.opacity = '0';
+    tempTextArea.style.pointerEvents = 'none';
+    tempTextArea.style.position = 'absolute';
+    tempTextArea.style.left = '-9999px';
+
+    document.body.appendChild(tempTextArea);
+    tempTextArea.focus();
+    tempTextArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempTextArea);
   }
 }
 </script>
