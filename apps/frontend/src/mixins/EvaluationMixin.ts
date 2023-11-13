d3941b2a904bf8a978650d6531ceda8a277fc7d8
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-11-13 13:08:49 -0500

    Fix private groups being shown
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- apps/frontend/src/mixins/EvaluationMixin.ts
+++ apps/frontend/src/mixins/EvaluationMixin.ts
@@ -1,3 +1,4 @@
+import {ServerModule} from '@/store/server';
 import {IGroup} from '@heimdall/interfaces';
 import {Component, Vue} from 'vue-property-decorator';
 import {IVuetifyItems} from '../utilities/helper_util';
@@ -5,10 +6,15 @@ import {IVuetifyItems} from '../utilities/helper_util';
 @Component({})
 export default class EvaluationMixin extends Vue {
   convertGroupsToIVuetifyItems(groups: IGroup[] | undefined): IVuetifyItems[] {
+    const currentUserEmail = ServerModule.userInfo.email;
     if (!groups) {
       return [];
     }
-    return groups.map((group) => {
+    const validatedGroups = groups.filter((group) =>
+      group.users.some((user) => user.email === currentUserEmail)
+    );
+
+    return validatedGroups.map((group) => {
       return {
         text: group.name,
         value: group.id
