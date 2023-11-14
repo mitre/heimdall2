430fc729d7566fcb0effa3b78fd59c81d88d45e8
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-11-14 02:22:06 -0500

    Use id instead of email
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- apps/frontend/src/mixins/EvaluationMixin.ts
+++ apps/frontend/src/mixins/EvaluationMixin.ts
@@ -6,12 +6,12 @@ import {IVuetifyItems} from '../utilities/helper_util';
 @Component({})
 export default class EvaluationMixin extends Vue {
   convertGroupsToIVuetifyItems(groups: IGroup[] | undefined): IVuetifyItems[] {
-    const currentUserEmail = ServerModule.userInfo.email;
+    const currentUserId = ServerModule.userInfo.id;
     if (!groups) {
       return [];
     }
     const validatedGroups = groups.filter((group) =>
-      group.users.some((user) => user.email === currentUserEmail)
+      group.users.some((user) => user.id === currentUserId)
     );
 
     return validatedGroups.map((group) => {
