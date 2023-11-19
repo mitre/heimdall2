23c553556096c7673f24c9a3d171a0a95eb6e2c2
Christopher Andrew Hinchey <chinchey@mitre.org>
2023-11-19 00:06:33 -0500

    Changed to use myGroup from store
    Signed-off-by: Christopher Andrew Hinchey <chinchey@mitre.org>

--- apps/frontend/src/mixins/EvaluationMixin.ts
+++ apps/frontend/src/mixins/EvaluationMixin.ts
@@ -1,4 +1,4 @@
-import {ServerModule} from '@/store/server';
+import {GroupsModule} from '@/store/groups';
 import {IGroup} from '@heimdall/interfaces';
 import {Component, Vue} from 'vue-property-decorator';
 import {IVuetifyItems} from '../utilities/helper_util';
@@ -6,15 +6,11 @@ import {IVuetifyItems} from '../utilities/helper_util';
 @Component({})
 export default class EvaluationMixin extends Vue {
   convertGroupsToIVuetifyItems(groups: IGroup[] | undefined): IVuetifyItems[] {
-    const currentUserId = ServerModule.userInfo.id;
     if (!groups) {
       return [];
     }
-    const validatedGroups = groups.filter((group) =>
-      group.users.some((user) => user.id === currentUserId)
-    );
 
-    return validatedGroups.map((group) => {
+    return GroupsModule.myGroups.map((group) => {
       return {
         text: group.name,
         value: group.id
