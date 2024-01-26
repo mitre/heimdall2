dcadd9c16460eb98ea7b90e735e5ce745b5e0fba
Amndeep Singh Mann <amann@mitre.org>
2024-01-26 11:16:15 -0500

    overwrite dodcerts if they're already there - only relevant if you do a docker-compose up without having done a down before hand
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- certs/dodcerts.sh
+++ certs/dodcerts.sh
@@ -2,4 +2,4 @@ yum install -y unzip
 url='https://public.cyber.mil/pki-pke/pkipke-document-library/'
 bundle=$(curl $url | grep -ioP '(?<=").*dod.zip(?=")')
 curl -o dodcerts.zip "$bundle"
-unzip -l dodcerts.zip | grep -ioP '.*\s+\K.*.pem' | xargs -I{} unzip -j dodcerts.zip {} -d /etc/pki/ca-trust/source/anchors/
+unzip -l dodcerts.zip | grep -ioP '.*\s+\K.*.pem' | xargs -I{} unzip -o -j dodcerts.zip {} -d /etc/pki/ca-trust/source/anchors/
