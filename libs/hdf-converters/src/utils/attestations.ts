686a5cbf7db7a8403201cbf0afe45ebe93ef6bfe
Daniel Medina <dmedina@mitre.org>
2024-03-26 12:27:44 -0400

    add comment to explain regex frequency input

--- libs/hdf-converters/src/utils/attestations.ts
+++ libs/hdf-converters/src/utils/attestations.ts
@@ -47,6 +47,7 @@ export function advanceDate(
       break;
     default: {
       // a number followed by d/w/m/y, with or without spaces in between
+      // 10 character limit on number of digits and characters to prevent security issues with regex
       const re = /(\d{1,10}(?:.\d{0,10})?)(\s{0,10})([a-z])/;
       const match = re.exec(frequency);
 
