8c17d1e70aa2ac628bdfbf5f12e3eabe3d067c60
Charles Hu <106095667+charleshu-8@users.noreply.github.com>
2024-07-02 14:18:07 -0400

    GoSec mapper impact fix (#5952)
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- libs/hdf-converters/test/mappers/forward/gosec_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/gosec_mapper.spec.ts
@@ -1,6 +1,7 @@
 import fs from 'fs';
 import {GoSecMapper} from '../../../src/gosec-mapper';
 import {omitVersions} from '../../utils';
+
 describe('gosec_mapper', () => {
   it('Successfully converts gosec reports', () => {
     const mapper = new GoSecMapper(
@@ -10,10 +11,42 @@ describe('gosec_mapper', () => {
       )
     );
 
+    // fs.writeFileSync(
+    //   'sample_jsons/gosec_mapper/gosec-hdf.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
         JSON.parse(
           fs.readFileSync('sample_jsons/gosec_mapper/gosec-hdf.json', {
+            encoding: 'utf-8'
+          })
+        )
+      )
+    );
+  });
+});
+
+describe('gosec_mapper_withraw', () => {
+  it('Successfully converts withRaw flagged gosec reports', () => {
+    const mapper = new GoSecMapper(
+      fs.readFileSync(
+        'sample_jsons/gosec_mapper/sample_input_report/Grype_gosec_results.json',
+        {encoding: 'utf-8'}
+      ),
+      true
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/gosec_mapper/gosec-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync('sample_jsons/gosec_mapper/gosec-hdf-withraw.json', {
             encoding: 'utf-8'
           })
         )
