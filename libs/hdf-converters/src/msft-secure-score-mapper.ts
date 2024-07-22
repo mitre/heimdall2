f4f5120b32c65fe126cbb7721dd2db68cea6e37e
David Arllen <darllen@mitre.org>
2024-07-22 09:46:25 -0400

    Revert "console debugging"
    This reverts commit 8a003c8b466b2b6e99de586250e704fd4fe50491.

--- libs/hdf-converters/src/msft-secure-score-mapper.ts
+++ libs/hdf-converters/src/msft-secure-score-mapper.ts
@@ -20,7 +20,6 @@ export class MsftSecureScoreMapper extends BaseConverter {
   }
 
   private getProfiles(controlName: string): SecureScoreControlProfile[] {
-    console.log('filtering profiles');
     return this.profiles.filter((p) => p.id === controlName);
   }
 
@@ -56,8 +55,6 @@ export class MsftSecureScoreMapper extends BaseConverter {
             },
             title: {
               transformer: (d: ControlScore) => {
-                console.log('filtering profiles to get title');
-
                 const titles = this.getProfiles(d.controlName || '')
                   .filter((p) => p.title !== undefined)
                   .map((p) => p.title);
@@ -126,7 +123,6 @@ export class MsftSecureScoreMapper extends BaseConverter {
               userImpacts: {
                 transformer: (d: ControlScore) => {
                   // return userImpacts from the profile document where its id matches the controlName
-                  console.log('filter profiles to get userImpact');
                   return this.getProfiles(d.controlName || '')
                     .filter((p) => p.userImpact !== undefined)
                     .map((p) => p.userImpact);
