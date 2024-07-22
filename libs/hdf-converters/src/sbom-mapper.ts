d08b9f25c33ec5e17c62eace22a75596ea89b566
Charles Hu <computerscience@verizon.net>
2024-07-22 13:56:13 -0400

    Logical density change
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- libs/hdf-converters/src/sbom-mapper.ts
+++ libs/hdf-converters/src/sbom-mapper.ts
@@ -33,15 +33,17 @@ export class SBOMResults {
   }
 
   generateIntermediary(data: Record<string, unknown>) {
-    if (
-      _.has(data, 'vulnerabilities') &&
-      data.vulnerabilities instanceof Array &&
-      data.components instanceof Array
-    ) {
-      for (let vulnerability of data.vulnerabilities) {
+    if (_.has(data, 'vulnerabilities')) {
+      for (let vulnerability of data.vulnerabilities as (Record<
+        string,
+        unknown
+      > & {affects: (Object & {ref: string})[]})[]) {
         for (const id of vulnerability.affects) {
           const components = [];
-          for (const component of data.components) {
+          for (const component of data.components as Record<
+            string,
+            unknown
+          >[]) {
             if (_.get(component, 'bom-ref') === id.ref) {
               components.push(component);
             }
