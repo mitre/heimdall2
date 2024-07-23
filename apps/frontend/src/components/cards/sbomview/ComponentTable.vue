f73acb69110dd57e96c283c2da570f84fabaaa19
Kaden Emley <kemley@mitre.org>
2024-07-23 11:24:30 -0400

    built out the UI to view the additional component details
    Signed-off-by: Kaden Emley <kemley@mitre.org>

--- apps/frontend/src/components/cards/sbomview/ComponentTable.vue
+++ apps/frontend/src/components/cards/sbomview/ComponentTable.vue
@@ -1,43 +1,99 @@
 <template>
   <v-container fluid class="font-weight-bold">
     <div
-      ref="controlTableTitle"
+      ref="componentTableTitle"
       :class="
         $vuetify.breakpoint.smAndDown
-          ? 'control-table-title'
-          : 'pinned-header control-table-title'
+          ? 'component-table-title'
+          : 'pinned-header component-table-title'
       "
-      :style="controlTableTitleStyle"
     >
-      <!-- Toolbar -->
-
-      <!-- Header. This should mirror the structure of ControlRowHeader -->
     </div>
 
     <!-- Body -->
     <v-row>
       <v-col>
         <v-data-table
-          :items="raw_items"
+          :items="items"
           :headers="headers"
           :items-per-page="-1"
           :item-key="'bom-ref'"
           show-expand
+          single-expand
           hide-default-footer
         >
+        <!--The single expand is to hide the tabs syncing together when multiple components are open-->
           <template #top>
             <v-card-title class="pb-0">Component View Data</v-card-title>
           </template>
           <template #expanded-item="{headers, item}">
-            <td class="m-10 p-10" :colspan="headers.length">
-              More info about {{ item.name }} <br />
-              Properties {{ item.properties }} <br />
-              <span v-for="reference in item.externalReferences">
-                <a href="reference.url">{{ reference.type }}</a
-                ><br />
-              </span>
-              Purl: {{ item.purl }} Licenses: {{ item.licenses }}
-            </td>
+          <td class="m-10 p-10" :colspan="headers.length">
+            <v-tabs v-model="tabs">
+              <v-tab>General Properties</v-tab>
+              <v-tab>References</v-tab>
+              <v-tab>Licenses</v-tab>
+            </v-tabs>
+            <v-tabs-items v-model="tabs">
+              <!--General Properties Tab-->
+              <v-tab-item>
+                <v-simple-table dense>
+                  <template #default>
+                    <thead>
+                      <tr>
+                        <th>Property</th><th>Value</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="key in stringFields" :colspan="headers.length">
+                        <td v-if="key in item">{{ key }}</td>
+                        <td v-if="key in item">{{ item[key] }}</td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </v-tab-item>
+
+              <!--References Tab-->
+              <v-tab-item>
+                <v-simple-table dense>
+                  <template #default>
+                    <thead>
+                      <tr>
+                        <th>Property</th><th>Value</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr v-for="reference in item.externalReferences" :colspan="headers.length">
+                        <td>{{ reference.type }}</td>
+                        <td><a :href="reference.url" target="_blank">{{ reference.url }}</a></td>
+                      </tr>
+                    </tbody>
+                  </template>
+                </v-simple-table>
+              </v-tab-item>
+
+              <!--Licenses Tab-->
+              <v-tab-item>
+                <span v-for="license in item.licenses">
+                  <v-simple-table dense>
+                    <template #default>
+                      <thead>
+                        <tr>
+                          <th>Property</th><th>Value</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr v-for="(value, key) in license.license" :colspan="headers.length">
+                          <td v-if="typeof(value) === 'string'">{{ key }}</td>
+                          <td v-if="typeof(value) === 'string'">{{  value }}</td>
+                        </tr>
+                      </tbody>
+                    </template>
+                </v-simple-table>
+                </span>
+              </v-tab-item>
+            </v-tabs-items>
+          </td>
           </template>
         </v-data-table>
       </v-col>
@@ -46,15 +102,7 @@
 </template>
 
 <script lang="ts">
-import ControlRowDetails from '@/components/cards/controltable/ControlRowDetails.vue';
-import ControlRowHeader from '@/components/cards/controltable/ControlRowHeader.vue';
-import ResponsiveRowSwitch from '@/components/cards/controltable/ResponsiveRowSwitch.vue';
-import ColumnHeader, {Sort} from '@/components/generic/ColumnHeader.vue';
-import {Filter, FilteredData, FilteredDataModule} from '@/store/data_filters';
-import {HeightsModule} from '@/store/heights';
-import {getControlRunTime} from '@/utilities/delta_util';
-import {control_unique_key} from '@/utilities/format_util';
-import {ContextualizedControl, severities} from 'inspecjs';
+import {Filter, FilteredDataModule} from '@/store/data_filters';
 import * as _ from 'lodash';
 import Vue from 'vue';
 import Component from 'vue-class-component';
@@ -79,37 +127,12 @@ interface Passthrough {
 
 @Component({
   components: {
-    ControlRowHeader,
-    ControlRowDetails,
-    ColumnHeader,
-    ResponsiveRowSwitch
   }
 })
 export default class ComponentTable extends Vue {
   @Ref('controlTableTitle') readonly controlTableTitle!: Element;
   @Prop({type: Object, required: true}) readonly filter!: Filter;
-
-  // Whether to allow multiple expansions
-  singleExpand = true;
-
-  // If the currently selected tab should sync
-  syncTabs = false;
-  syncTab = 'tab-test';
-
-  // List of currently expanded options. If unique id is in here, it is expanded
-  expanded: string[] = [];
-
-  // Sorts
-  sortId: Sort = 'none';
-  sortStatus: Sort = 'none';
-  sortSet: Sort = 'none';
-  sortSeverity: Sort = 'none';
-  sortRunTime: Sort = 'none';
-
-  // Used for viewed/unviewed controls.
-  viewedControlIds: string[] = [];
-  displayUnviewedControls = true;
-
+  
   headers = [
     {
       text: 'Component Name',
@@ -141,133 +164,34 @@ export default class ComponentTable extends Vue {
     }
   ];
 
-  get numOfViewed() {
-    return this.raw_items.filter((elem) =>
-      this.viewedControlIds.some((id) => elem.control.data.id === id)
-    ).length;
+  tabs = {
+    tab: null,
+    items: [
+      'strings', 'provenance', 'references', 'etc'
+    ]
   }
 
-  toggleControlViewed(control: ContextualizedControl) {
-    const alreadyViewed = this.viewedControlIds.indexOf(control.data.id);
-    // If the control hasn't been marked as viewed yet, mark it as viewed.
-    if (alreadyViewed === -1) {
-      this.viewedControlIds.push(control.data.id);
-    }
-    // Else, remove it from the view controls array.
-    else {
-      this.viewedControlIds.splice(alreadyViewed, 1);
-    }
-  }
+  stringFields = [
+    'type',
+    'mime-type',
+    'publisher',
+    'group',
+    'name',
+    'version',
+    'description',
+    'scope',
+    'copyright',
+    'cpe',
+    'purl'
+  ]
 
-  mounted() {
-    this.onResize();
-  }
-
-  onResize() {
-    // Allow the page to settle before checking the controlTableHeader height
-    // (this is what $nextTick is supposed to do but it's firing too quickly)
-    setTimeout(() => {
-      HeightsModule.setControlTableHeaderHeight(
-        this.controlTableTitle?.clientHeight
-      );
-    }, 2000);
-  }
-
-  /** Callback to handle setting a new sort */
-  set_sort(column: string, newSort: Sort) {
-    this.sortId = 'none';
-    this.sortSet = 'none';
-    this.sortStatus = 'none';
-    this.sortSeverity = 'none';
-    this.sortRunTime = 'none';
-    switch (column) {
-      case 'id':
-        this.sortId = newSort;
-        break;
-      case 'status':
-        this.sortStatus = newSort;
-        break;
-      case 'set':
-        this.sortSet = newSort;
-        break;
-      case 'severity':
-        this.sortSeverity = newSort;
-        break;
-      case 'runTime':
-        this.sortRunTime = newSort;
-        break;
-    }
-  }
-
-  get controlTableTitleStyle() {
-    return {top: `${HeightsModule.topbarHeight}px`};
-  }
-
-  get controlRowPinOffset() {
-    // There is ~10px of padding being added which makes the ControlRowHeader look out of place
-    return {top: `${this.topOfPage - 10}px`};
-  }
-
-  // The top of the page, relative to the topbar and the title bar
-  get topOfPage() {
-    return HeightsModule.topbarHeight + HeightsModule.controlTableHeaderHeight;
-  }
-
-  async updateTab(tab: string) {
-    this.syncTab = tab;
-  }
-
-  /** Toggles the given expansion of a control details panel */
-  toggle(key: string) {
-    if (this.singleExpand) {
-      // Check if key already there
-      const had = this.expanded.includes(key);
-
-      // Clear
-      this.expanded = [];
-
-      // If key is new, add it
-      if (!had) {
-        this.expanded.push(key);
-        this.jump_to_key(key);
-      }
-    } else {
-      // Add or remove it from the set, as appropriate. Shortcut this by only adding if delete fails
-      const i = this.expanded.indexOf(key);
-      if (i < 0) {
-        this.expanded.push(key);
-        this.jump_to_key(key);
-      } else {
-        this.expanded.splice(i, 1);
-      }
-    }
-  }
-
-  jump_to_key(key: string) {
-    if (!this.$vuetify.breakpoint.smAndDown) {
-      this.$nextTick(() => {
-        this.$vuetify.goTo(`#${this.striptoChars(key)}`, {
-          offset: this.topOfPage,
-          duration: 300
-        });
-      });
-    }
-  }
-
-  striptoChars(key: string) {
-    return key.replace(/[^a-z0-9]/gi, '');
-  }
-
-  /** Return items as key, value pairs */
-  get raw_items(): any[] {
+  get items(): any[] {
     const evaluations = FilteredDataModule.evaluations(
       FilteredDataModule.selectedEvaluationIds
     );
-    console.log(evaluations);
     const aux_datas: Passthrough['auxiliary_data'][] = evaluations.map((e) =>
       _.get(e, 'data.passthrough.auxiliary_data', [])
     );
-    console.log(aux_datas);
     let sboms: SBOMComponent[] = [];
     for (const auxes of aux_datas) {
       for (const a of auxes) {
@@ -276,7 +200,6 @@ export default class ComponentTable extends Vue {
         }
       }
     }
-    console.log(sboms);
     return sboms;
   }
 }
