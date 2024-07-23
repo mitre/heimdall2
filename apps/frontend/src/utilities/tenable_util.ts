f9c454cada7f8da1306067a91dd7e6aabe0560ed
Amndeep Singh Mann <amann@mitre.org>
2024-07-23 01:57:09 -0400

    swapped adm-zip out for zip.js
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/frontend/src/utilities/tenable_util.ts
+++ apps/frontend/src/utilities/tenable_util.ts
@@ -1,6 +1,14 @@
-import Zip from 'adm-zip';
 import axios, {AxiosInstance} from 'axios';
 import {createWinstonLogger} from '../../../../libs/hdf-converters/src/utils/global';
+
+let zip: any;
+try {
+  (async () => {
+    zip = await import('@zip.js/zip.js');
+  })();
+} catch (e) {
+  console.log(`zip module import failed: ${e}`);
+}
 
 /** represents the information of the current used */
 export interface AuthInfo {
@@ -44,7 +52,7 @@ export class TenableUtil {
 
   async loginToTenable(): Promise<boolean> {
     logger.info(`Connecting to Tenable Client`);
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       setTimeout(
         () =>
           reject(
@@ -56,32 +64,25 @@ export class TenableUtil {
       );
 
       try {
-        this.axios_instance({
+        const response = await this.axios_instance({
           method: 'get',
           url: '/rest/currentUser'
-        })
-          .then((response) => {
-            resolve(response.request.finished);
-          })
-          .catch((error) => {
-            try {
-              if (error.code == 'ENOTFOUND') {
-                reject(
-                  `Host: ${this.hostConfig.host_url} not found, check the Host Name (URL) or the network`
-                );
-              } else if (error.response.data.error_code == 74) {
-                reject('Incorrect Access or Secret key');
-              } else {
-                reject(error.response.data.error_msg);
-              }
-            } catch (e) {
-              reject(
-                `Possible network connection blocked by CORS policy. Received error: ${error}`
-              );
-            }
-          });
-      } catch (e) {
-        reject(`Unknown error: ${e}`);
+        });
+        resolve(response.request.finished);
+      } catch (error) {
+        if (error?.code === 'ENOTFOUND') {
+          reject(
+            `Host: ${this.hostConfig.host_url} not found, check the Host Name (URL) or the network`
+          );
+        } else if (error?.response?.data?.error_code === 74) {
+          reject('Incorrect Access or Secret key');
+        } else if (error?.response?.data?.error_msg) {
+          reject(error.response.data.error_msg);
+        } else {
+          reject(
+            `Possible network connection blocked by CORS policy. Received error: ${error}`
+          );
+        }
       }
     });
   }
@@ -93,7 +94,7 @@ export class TenableUtil {
    */
   async getScans(startTime: number, endTime: number): Promise<[]> {
     logger.info(`Getting scans from Tenable Client`);
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       setTimeout(
         () =>
           reject(
@@ -105,22 +106,21 @@ export class TenableUtil {
       );
 
       try {
-        this.axios_instance({
+        const response = await this.axios_instance({
           method: 'get',
           url: `/rest/scanResult?fields=name,description,details,scannedIPs,totalChecks,startTime,finishTime,status&startTime=${startTime}&endTime=${endTime}`
-        })
-          .then((response) => {
-            resolve(response.data.response.usable);
-          })
-          .catch((error) => {
-            if (error.response.data.error_code == 74) {
-              reject('Incorrect Access or Secret key');
-            } else {
-              reject(error.response.data.error_msg);
-            }
-          });
-      } catch (e) {
-        reject(e);
+        });
+        resolve(response.data.response.usable);
+      } catch (error) {
+        if (error?.response?.data?.error_code === 74) {
+          reject('Incorrect Access or Secret key');
+        } else if (error?.response?.data?.error_msg) {
+          reject(error.response.data.error_msg);
+        } else {
+          reject(
+            `Possible network connection blocked by CORS policy. Received error: ${error}`
+          );
+        }
       }
     });
   }
@@ -135,7 +135,7 @@ export class TenableUtil {
    */
   async getVulnerabilities(scanId: string): Promise<string> {
     logger.info(`Getting vulnerabilities from Tenable Client`);
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
       setTimeout(
         () =>
           reject(
@@ -147,24 +147,18 @@ export class TenableUtil {
       );
 
       try {
-        this.axios_instance({
+        const response = await this.axios_instance({
           method: 'post',
           url: `rest/scanResult/${scanId}/download?downloadType=v2`,
           responseType: 'arraybuffer'
-        })
-          .then((response) => {
-            // Unzip response in memory
-            try {
-              const zip = new Zip(Buffer.from(response.data));
-              const zipEntries = zip.getEntries();
-              resolve(zip.readAsText(zipEntries[0]));
-            } catch (unzipErr) {
-              reject(unzipErr);
-            }
-          })
-          .catch((error) => {
-            reject(error);
-          });
+        });
+        const zipReader = new zip.ZipReader(
+          new zip.BlobReader(Buffer.from(response.data))
+        );
+        const zipEntries = await zipReader.getEntries();
+        const contents = await zipEntries[0].getData(new zip.TextWriter());
+        await zipReader.close();
+        resolve(contents);
       } catch (e) {
         reject(e);
       }
