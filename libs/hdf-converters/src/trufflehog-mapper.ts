76f8571e84b0f980b5a03643a51a3aee418a263e
andytang99 <atang@mitre.org>
2024-07-26 15:45:57 +0000

    Final commit

--- libs/hdf-converters/src/trufflehog-mapper.ts
+++ libs/hdf-converters/src/trufflehog-mapper.ts
@@ -1,11 +1,7 @@
 import {ExecJSON} from 'inspecjs';
 import _ from 'lodash';
 import {version as HeimdallToolsVersion} from '../package.json';
-import {
-  BaseConverter,
-  ILookupPath,
-  MappedTransform
-} from './base-converter';
+import {BaseConverter, ILookupPath, MappedTransform} from './base-converter';
 
 export class TrufflehogMapper extends BaseConverter {
   withRaw: boolean;
@@ -16,66 +12,69 @@ export class TrufflehogMapper extends BaseConverter {
   > = {
     platform: {
       name: 'Heimdall Tools',
-      release: HeimdallToolsVersion,
+      release: HeimdallToolsVersion
     },
     version: HeimdallToolsVersion,
-    statistics: {
-    },
+    statistics: {},
     profiles: [
       {
-        name: {path: 'wrapper[0].SourceName'},              
-        title: {path: 'wrapper[0].SourceName'},           
-        supports: [],          
-        attributes: [],        
-        depends: [],           
-        groups: [],            
-        status: 'loaded',      
+        name: {path: 'wrapper[0].SourceName'},
+        title: {path: 'wrapper[0].SourceName'},
+        supports: [],
+        attributes: [],
+        groups: [],
+        status: 'loaded',
         controls: [
           {
-            path:'wrapper',
+            path: 'wrapper',
             key: 'id',
             tags: {
-                nist: 'IA-5(7)',
-                cci: [
-                  'CCI-004069',
-                  'CCI-000202',
-                  'CCI-000203',
-                  'CCI-002367'
-                ],
-                severity: 'medium'
-            },                 
-            refs: [],             
-            source_location: {},  
+              nist: 'IA-5(7)',
+              cci: ['CCI-004069', 'CCI-000202', 'CCI-000203', 'CCI-002367'],
+              severity: 'medium'
+            },
+            refs: [],
+            source_location: {},
             title: {
-                transformer: (data: Record<string, unknown>): string => {
-                    return _.get(data, 'DetectorName') + '_' + _.get(data, 'DecoderName');
-                }
-            },          
+              transformer: (data: Record<string, unknown>): string => {
+                return (
+                  _.get(data, 'DetectorName') + '_' + _.get(data, 'DecoderName')
+                );
+              }
+            },
             id: {
-                transformer: (data: Record<string, unknown>): string => {
-                    return _.get(data, 'DetectorType') + '_' + _.get(data, 'DecoderName');
-                }
-            },               
+              transformer: (data: Record<string, unknown>): string => {
+                return (
+                  _.get(data, 'DetectorType') + '_' + _.get(data, 'DecoderName')
+                );
+              }
+            },
             desc: {
-                transformer: (data: Record<string, unknown>): string => {
-                    return 'Found ' + _.get(data, 'DetectorName') + ' secret using ' + _.get(data, 'DecoderName') + ' decoder'
-                }
-            },           
-            impact: 0.5,                      
+              transformer: (data: Record<string, unknown>): string => {
+                return (
+                  'Found ' +
+                  _.get(data, 'DetectorName') +
+                  ' secret using ' +
+                  _.get(data, 'DecoderName') +
+                  ' decoder'
+                );
+              }
+            },
+            impact: 0.5,
             results: [
               {
-                status: ExecJSON.ControlResultStatus.Failed,  
+                status: ExecJSON.ControlResultStatus.Failed,
                 code_desc: {
                   transformer: (data: Record<string, unknown>): string => {
                     return `${JSON.stringify(_.get(data, 'SourceMetadata'), null, 2)}`;
                   }
-                },                                
+                },
                 message: {
-                    transformer: (data: Record<string, unknown>): string => {
-                      return `${JSON.stringify(_.omit(data, 'SourceMetadata'), null, 2)}`;
-                    }
-                },                                                            
-                start_time: ''                                
+                  transformer: (data: Record<string, unknown>): string => {
+                    return `${JSON.stringify(_.omit(data, 'SourceMetadata'), null, 2)}`;
+                  }
+                },
+                start_time: ''
               }
             ]
           }
@@ -93,6 +92,6 @@ export class TrufflehogMapper extends BaseConverter {
   };
   constructor(exportJson: string, withRaw = false) {
     super({wrapper: JSON.parse(exportJson)}, true);
-    this.withRaw = withRaw
+    this.withRaw = withRaw;
   }
 }
--- libs/hdf-converters/test/mappers/forward/trufflehog_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/trufflehog_mapper.spec.ts
@@ -11,10 +11,10 @@ describe('trufflehog_mapper', () => {
       )
     );
 
-      // fs.writeFileSync(
-      //   'sample_jsons/trufflehog_mapper/trufflehog-hdf.json',
-      //   JSON.stringify(mapper.toHdf(), null, 2)
-      // );
+    // fs.writeFileSync(
+    //   'sample_jsons/trufflehog_mapper/trufflehog-hdf.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
@@ -41,10 +41,10 @@ describe('trufflehog_mapper_withraw', () => {
       true
     );
 
-      // fs.writeFileSync(
-      //   'sample_jsons/trufflehog_mapper/trufflehog-hdf-withraw.json',
-      //   JSON.stringify(mapper.toHdf(), null, 2)
-      // );
+    // fs.writeFileSync(
+    //   'sample_jsons/trufflehog_mapper/trufflehog-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
@@ -70,10 +70,10 @@ describe('trufflehog_docker_mapper', () => {
       )
     );
 
-      // fs.writeFileSync(
-      //   'sample_jsons/trufflehog_mapper/trufflehog-docker-hdf.json',
-      //   JSON.stringify(mapper.toHdf(), null, 2)
-      // );
+    // fs.writeFileSync(
+    //   'sample_jsons/trufflehog_mapper/trufflehog-docker-hdf.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
@@ -100,16 +100,75 @@ describe('trufflehog_docker_mapper_withraw', () => {
       true
     );
 
-      // fs.writeFileSync(
-      //   'sample_jsons/trufflehog_mapper/trufflehog-docker-hdf-withraw.json',
-      //   JSON.stringify(mapper.toHdf(), null, 2)
-      // );
+    // fs.writeFileSync(
+    //   'sample_jsons/trufflehog_mapper/trufflehog-docker-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
         JSON.parse(
           fs.readFileSync(
             'sample_jsons/trufflehog_mapper/trufflehog-docker-hdf-withraw.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+});
+
+describe('trufflehog_saf_example_mapper', () => {
+  it('Successfully converts trufflehog targeted at a local/cloned repository data', () => {
+    const mapper = new TrufflehogMapper(
+      fs.readFileSync(
+        'sample_jsons/trufflehog_mapper/sample_input_report/trufflehog_saf_example.json',
+        {encoding: 'utf-8'}
+      )
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/trufflehog_mapper/trufflehog-saf-hdf.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/trufflehog_mapper/trufflehog-saf-hdf.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+});
+
+describe('trufflehog_saf_example_mapper_withraw', () => {
+  it('Successfully converts withraw flagged trufflehog targeted at a local/cloned repository data', () => {
+    const mapper = new TrufflehogMapper(
+      fs.readFileSync(
+        'sample_jsons/trufflehog_mapper/sample_input_report/trufflehog_saf_example.json',
+        {encoding: 'utf-8'}
+      ),
+      true
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/trufflehog_mapper/trufflehog-saf-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/trufflehog_mapper/trufflehog-saf-hdf-withraw.json',
             {
               encoding: 'utf-8'
             }
