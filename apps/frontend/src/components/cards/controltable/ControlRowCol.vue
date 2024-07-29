11bd36103ed1f30de7cd22a7116ff6b4acbdd748
Charles Hu <computerscience@verizon.net>
2024-07-29 16:45:48 -0400

    Linting
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- apps/frontend/src/components/cards/controltable/ControlRowCol.vue
+++ apps/frontend/src/components/cards/controltable/ControlRowCol.vue
@@ -79,11 +79,13 @@ export default class ControlRowCol extends mixins(HtmlSanitizeMixin) {
     // Check if either `skip_message` or `message` exist
     // If one but not the other exists, display the individual message
     // Otherwise display both messages in a joint string
-    return this.result.skip_message
-      ? this.result.message
+    if (this.result.skip_message) {
+      return this.result.message
         ? `-Message-\n${this.result.message}\n\n-Skip Message-\n${this.result.skip_message}`
-        : this.result.skip_message
-      : this.result.message;
+        : this.result.skip_message;
+    } else {
+      return this.result.message;
+    }
   }
 }
 </script>
