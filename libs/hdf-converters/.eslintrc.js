41e4effcb4292a8c39ecab8dc9ade113f4c095c0
Charles Hu <computerscience@verizon.net>
2024-08-19 12:22:00 -0400

    Linting rules exception for unused vars prefixed with _
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- libs/hdf-converters/.eslintrc.js
+++ libs/hdf-converters/.eslintrc.js
@@ -1,38 +1,39 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        project: 'tsconfig.json',
-        sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint/eslint-plugin'],
-    extends: [
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'plugin:prettier/recommended',
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  root: true,
+  env: {
+    node: true
+  },
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: {
+          regex: '^I[A-Z]',
+          match: true
+        }
+      }
     ],
-    root: true,
-    env: {
-        node: true,
-    },
-    rules: {
-        "@typescript-eslint/naming-convention": [
-            "warn",
-            {
-                "selector": "interface",
-                "format": ["PascalCase"],
-                "custom": {
-                    "regex": "^I[A-Z]",
-                    "match": true
-                }
-            }
-        ],
-        'object-curly-spacing': 'warn',
-        '@typescript-eslint/no-explicit-any': 'off',
-        "no-unused-vars": [
-            "error", {
-                "argsIgnorePattern": "^_"
-            }
-        ]
-    },
+    'object-curly-spacing': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
 };
