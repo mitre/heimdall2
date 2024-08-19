148d43879d9de700b94092c651940b549e98ee76
Charles Hu <computerscience@verizon.net>
2024-08-19 09:04:47 -0400

    Added additional tests
    Signed-off-by: Charles Hu <computerscience@verizon.net>

--- libs/hdf-converters/test/mappers/forward/cyclonedx_sbom_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/cyclonedx_sbom_mapper.spec.ts
@@ -290,3 +290,125 @@ describe('sbom_mapper_vex', () => {
     );
   });
 });
+
+describe('sbom_mapper_syft_alpine_container', () => {
+  it('Successfully converts SBOM data', () => {
+    const mapper = new CycloneDXSBOMResults(
+      fs.readFileSync(
+        'sample_jsons/cyclonedx_sbom_mapper/sample_input_report/syft-scan-alpine-container.json',
+        {
+          encoding: 'utf-8'
+        }
+      )
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/cyclonedx_sbom_mapper/sbom-syft-alpine-container-hdf.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/cyclonedx_sbom_mapper/sbom-syft-alpine-container-hdf.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+
+  it('Successfully converts withraw flagged SBOM data', () => {
+    const mapper = new CycloneDXSBOMResults(
+      fs.readFileSync(
+        'sample_jsons/cyclonedx_sbom_mapper/sample_input_report/syft-scan-alpine-container.json',
+        {
+          encoding: 'utf-8'
+        }
+      ),
+      true
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/cyclonedx_sbom_mapper/sbom-syft-alpine-container-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/cyclonedx_sbom_mapper/sbom-syft-alpine-container-hdf-withraw.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+});
+
+describe('sbom_mapper_converted_spdx', () => {
+  it('Successfully converts SBOM data', () => {
+    const mapper = new CycloneDXSBOMResults(
+      fs.readFileSync(
+        'sample_jsons/cyclonedx_sbom_mapper/sample_input_report/spdx-to-cyclonedx.json',
+        {
+          encoding: 'utf-8'
+        }
+      )
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/cyclonedx_sbom_mapper/sbom-converted-spdx-hdf.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/cyclonedx_sbom_mapper/sbom-converted-spdx-hdf.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+
+  it('Successfully converts withraw flagged SBOM data', () => {
+    const mapper = new CycloneDXSBOMResults(
+      fs.readFileSync(
+        'sample_jsons/cyclonedx_sbom_mapper/sample_input_report/spdx-to-cyclonedx.json',
+        {
+          encoding: 'utf-8'
+        }
+      ),
+      true
+    );
+
+    // fs.writeFileSync(
+    //   'sample_jsons/cyclonedx_sbom_mapper/sbom-converted-spdx-hdf-withraw.json',
+    //   JSON.stringify(mapper.toHdf(), null, 2)
+    // );
+
+    expect(omitVersions(mapper.toHdf())).toEqual(
+      omitVersions(
+        JSON.parse(
+          fs.readFileSync(
+            'sample_jsons/cyclonedx_sbom_mapper/sbom-converted-spdx-hdf-withraw.json',
+            {
+              encoding: 'utf-8'
+            }
+          )
+        )
+      )
+    );
+  });
+});
