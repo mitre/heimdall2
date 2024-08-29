efbb3ffba0f8bb7bbe9dd1b0203cefd428a656c4
andytang99 <atang@mitre.org>
2024-08-29 12:48:52 +0000

    pushing changes

--- libs/hdf-converters/src/anchore-grype-mapper.ts
+++ libs/hdf-converters/src/anchore-grype-mapper.ts
@@ -8,83 +8,183 @@ import {
   MappedTransform
 } from './base-converter';
 
+const IMPACT_MAPPING: Map<string, number> = new Map([
+  ['critical', 0.9],
+  ['high', 0.7],
+  ['medium', 0.5],
+  ['low', 0.3],
+  ['negligible', 0.0],
+  ['unknown severity', 0.5]
+]);
+
 export class AnchoreGrypeMapper extends BaseConverter {
   withRaw: boolean;
+  metadata: Record<string, unknown>;
 
-  mappings: MappedTransform<
+  mapping(): MappedTransform<
     ExecJSON.Execution & {passthrough: unknown},
     ILookupPath
-  > = {
-    platform: {
-      name: 'Heimdall Tools',
-      release: HeimdallToolsVersion,
-      target_id: null  //Insert data
-    },
-    version: HeimdallToolsVersion,
-    statistics: {
-      duration: null  //Insert data
-    },
-    profiles: [
-      {
-        name: 'Anchore - Grype',              
-        title: 'Anchore Grype Matches',           
-        version: null,         //Insert data
-        maintainer: null,      //Insert data
-        summary: null,         //Insert data
-        license: null,         //Insert data
-        copyright: null,       //Insert data
-        copyright_email: null, //Insert data
-        supports: [],          //Insert data
-        attributes: [],        //Insert data
-        depends: [],           //Insert data
-        groups: [],            //Insert data
-        status: 'loaded',      //Insert data
-        controls: [
-          {
-            path: 'vulnerability',
-            key: {
-              transformer: (data: Record<string, unknown>): string => {
-                  return 'grype/' + _.get(data, 'id');
-              }
-            },
-            tags: {},             //Insert data
-            descriptions: [],     //Insert data
-            refs: [],             //Insert data
-            source_location: {},  //Insert data
-            title: {
-              transformer: (data: Record<string, unknown>): string => {
-                  return 'Grype found a vulnerability to ' + _.get(data, 'id');
-              }
-            },
-            id: '',               //Insert data
-            desc: null,           //Insert data
-            impact: 0,            //Insert data
-            code: null,           //Insert data
-            results: [
-              {
-                status: ExecJSON.ControlResultStatus.Failed,  //Insert data
-                code_desc: '',                                //Insert data
-                message: null,                                //Insert data
-                run_time: null,                               //Insert data
-                start_time: ''                                //Insert data
-              }
-            ]
-          }
-        ],
-        sha256: ''
+  > {
+    return {
+      platform: {
+        name: 'Heimdall Tools',
+        release: HeimdallToolsVersion,
+        target_id: null 
+      },
+      version: HeimdallToolsVersion,
+      statistics: {
+        duration: null 
+      },
+      profiles: [
+        {
+          name: 'Anchore - Grype',
+          title: 'Anchore Grype Matches',
+          version: _.get(
+            this.metadata,
+            'descriptor.version'
+          ) as string, 
+          maintainer: null, 
+          summary: null, 
+          license: null, 
+          copyright: null, 
+          copyright_email: null, 
+          supports: [], 
+          attributes: [], 
+          depends: [], 
+          groups: [], 
+          status: 'loaded', 
+          controls: [
+            {
+              path: 'wrapper',
+              key: 'id',
+              tags: {
+                nist: ['SA-11', 'RA-5'],
+                cveid: {path: 'vulnerability.id'}
+              }, 
+              descriptions: [
+                {
+                  data: {
+                    path: 'vulnerability.fix',
+                    transformer: (data: Record<string, unknown>): string =>
+                      data.state == 'fixed'
+                        ? `vulnerability is ${_.get(data, 'state')} for versions ${(_.get(data, 'versions') as string[]).join(', ')}`
+                        : `vulnerability is not known to be fixed in any versions`
+                  },
+                  label: 'fix'
+                },
+                {
+                  data: {
+                    path: 'relatedVulnerabilities',
+                    transformer: (data: Record<string, unknown>): string =>
+                      `${JSON.stringify(_.get(data, 'cvss'), null, 2)}`
+                  },
+                  label: 'check'
+                }
+              ],
+              refs: {
+                transformer: (data: Record<string, unknown>) => {
+                  const vuln_urls = _.get(data, 'vulnerability.urls') as Record<
+                    string,
+                    unknown
+                  >[];
+                  const relatedVulnerabilities = _.get(
+                    data,
+                    'relatedVulnerabilities'
+                  );
+                  let relatedVulnerabilitiesUrls = [] as unknown as Record<
+                    string,
+                    unknown
+                  >[];
+                  if (relatedVulnerabilities) {
+                    relatedVulnerabilitiesUrls = (
+                      relatedVulnerabilities as Record<string, unknown>[]
+                    ).map((element) => element.urls) as Record<
+                      string,
+                      unknown
+                    >[];
+                  }
+                  return (
+                    vuln_urls.concat(
+                      ...relatedVulnerabilitiesUrls
+                    ) as unknown as Record<string, unknown>[]
+                  ).map((element) => ({url: element}));
+                }
+              } as unknown as ExecJSON.Reference[],
+              source_location: {}, 
+              title: {
+                path: 'vulnerability',
+                transformer: (data: Record<string, unknown>): string =>
+                  `Grype found a vulnerability to ${_.get(data, 'id')}`
+              },
+              id: {
+                transformer: (data: Record<string, unknown>): string =>
+                  `${_.get(this.metadata, 'source.target.userInput')}/${_.get(data, 'vulnerability.id')}`
+              },
+              desc: {
+                transformer: (data: Record<string, unknown>): string =>
+                  !(data.vulnerability as Record<string, unknown>).description
+                    ? ((
+                        data.relatedVulnerabilities as Record<string, unknown>[]
+                      ).filter(
+                        (relatedVulnerability) =>
+                          relatedVulnerability.id ===
+                          (data.vulnerability as Record<string, unknown>).id
+                      )[0].description as string)
+                    : ((data.vulnerability as Record<string, unknown>)
+                        .description as string)
+              },
+              impact: {
+                path: 'vulnerability.severity',
+                transformer: impactMapping(IMPACT_MAPPING)
+              },
+              code: {
+                transformer: (data: Record<string, unknown>): string =>
+                  `${JSON.stringify(
+                    _.omitBy(
+                      _.pick(data, ['vulnerability', 'relatedVulnerabilities']),
+                      (value) => value === null || value === ''
+                    ),
+                    null,
+                    2
+                  )}`
+              },
+              results: [
+                {
+                  status: ExecJSON.ControlResultStatus.Failed,
+                  code_desc: {
+                    transformer: (data: Record<string, unknown>): string =>
+                      `${JSON.stringify(_.get(data, 'matchDetails'), null, 2)}`
+                  },
+                  message: {
+                    transformer: (data: Record<string, unknown>): string =>
+                      `${JSON.stringify(_.get(data, 'artifact'), null, 2)}`
+                  },
+                  start_time: _.get(
+                    this.metadata,
+                    'descriptor.timestamp'
+                  ) as string
+                }
+              ]
+            }
+          ],
+          sha256: ''
+        }
+      ],
+      passthrough: {
+        transformer: (data: Record<string, any>): Record<string, unknown> => {
+          return {
+            auxiliary_data: [{name: '', data: _.omit([])}], //Insert service name and mapped fields to be removed
+            ...(this.withRaw && {raw: data})
+          };
+        }
       }
-    ],
-    passthrough: {
-      transformer: (data: Record<string, any>): Record<string, unknown> => {
-        return {
-          auxiliary_data: [{name: '', data: _.omit([])}],  //Insert service name and mapped fields to be removed
-          ...(this.withRaw && {raw: data})
-        };
-      }
-    }
-  };
+    };
+  }
   constructor(exportJson: string, withRaw = false) {
-    super(JSON.parse(exportJson), true);
-    this.withRaw = withRaw
+    const temp = JSON.parse(exportJson);
+    super({wrapper: _.get(temp, 'matches')});
+    this.metadata = _.omit(temp, 'matches');
+    this.withRaw = withRaw;
+    this.setMappings(this.mapping());
   }
 }
--- libs/hdf-converters/test/mappers/forward/anchore-grype_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/anchore-grype_mapper.spec.ts
@@ -1,26 +1,26 @@
 import fs from 'fs';
-import {SKELETONMapper} from '../../../src/anchore-grype-mapper';
+import {AnchoreGrypeMapper} from '../../../src/anchore-grype-mapper';
 import {omitVersions} from '../../utils';
 
 describe('SKELETON_mapper', () => {
   it('Successfully converts SKELETON targeted at a local/cloned repository data', () => {
-    const mapper = new SKELETONMapper(
+    const mapper = new AnchoreGrypeMapper(
       fs.readFileSync(
-        'sample_jsons/SKELETON_mapper/sample_input_report/SKELETON.json',
+        'sample_jsons/anchore_grype_mapper/sample_input_report/anchore_grype.json',
         {encoding: 'utf-8'}
       )
     );
 
-    // fs.writeFileSync(
-    //   'sample_jsons/SKELETON_mapper/SKELETON-hdf.json',
-    //   JSON.stringify(mapper.toHdf(), null, 2)
-    // );
+    fs.writeFileSync(
+      'sample_jsons/anchore_grype_mapper/anchore-grype-hdf.json',
+      JSON.stringify(mapper.toHdf(), null, 2)
+    );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
         JSON.parse(
           fs.readFileSync(
-            'sample_jsons/SKELETON_mapper/SKELETON-hdf.json',
+            'sample_jsons/anchore_grype_mapper/anchore-grype-hdf.json',
             {
               encoding: 'utf-8'
             }
@@ -31,26 +31,26 @@ describe('SKELETON_mapper', () => {
   });
 });
 
-describe('SKELETON_mapper_withraw', () => {
+describe('anchore-grype_mapper_withraw', () => {
   it('Successfully converts withraw flagged SKELETON targeted at a local/cloned repository data', () => {
-    const mapper = new SKELETONMapper(
+    const mapper = new AnchoreGrypeMapper(
       fs.readFileSync(
-        'sample_jsons/SKELETON_mapper/sample_input_report/SKELETON.json',
+        'sample_jsons/anchore_grype_mapper/sample_input_report/anchore_grype.json',
         {encoding: 'utf-8'}
       ),
       true
     );
 
-    // fs.writeFileSync(
-    //   'sample_jsons/SKELETON_mapper/SKELETON-hdf-withraw.json',
-    //   JSON.stringify(mapper.toHdf(), null, 2)
-    // );
+    fs.writeFileSync(
+      'sample_jsons/anchore_grype_mapper/anchore-grype-withraw.json',
+      JSON.stringify(mapper.toHdf(), null, 2)
+    );
 
     expect(omitVersions(mapper.toHdf())).toEqual(
       omitVersions(
         JSON.parse(
           fs.readFileSync(
-            'sample_jsons/SKELETON_mapper/SKELETON-hdf-withraw.json',
+            'sample_jsons/anchore_grype_mapper/anchore-grype-withraw.json',
             {
               encoding: 'utf-8'
             }
