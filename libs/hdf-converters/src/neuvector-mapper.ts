e2cc67e04b71250d88edd551a1f290ca3317b275
Joyce Quach <jquach@mitre.org>
2024-09-20 16:55:24 -0400

    Add source tag of CVE and edit cve tags
    Signed-off-by: Joyce Quach <jquach@mitre.org>

--- libs/hdf-converters/src/neuvector-mapper.ts
+++ libs/hdf-converters/src/neuvector-mapper.ts
@@ -10,6 +10,121 @@ import {
 import {CweNistMapping} from './mappings/CweNistMapping';
 import {DEFAULT_UPDATE_REMEDIATION_NIST_TAGS} from './utils/global';
 
+type RESTVulnerability = {
+  name: string;
+  score: number /* float32 */;
+  severity: string;
+  vectors: string;
+  description: string;
+  file_name: string;
+  package_name: string;
+  package_version: string;
+  fixed_version: string;
+  link: string;
+  score_v3: number /* float32 */;
+  vectors_v3: string;
+  published_timestamp: number /* int64 */;
+  last_modified_timestamp: number /* int64 */;
+  cpes?: string[];
+  cves?: string[];
+  feed_rating: string;
+  in_base_image?: boolean;
+  tags?: string[];
+};
+
+type RESTScanModule = {
+  name: string;
+  file: string;
+  version: string;
+  source: string;
+  cves?: RESTModuleCve[];
+  cpes?: string[];
+};
+
+type RESTModuleCve = {
+  name: string;
+  status: string;
+};
+
+type RESTBenchItem = {
+  level: string;
+  evidence?: string;
+  location?: string;
+  message: string[];
+  group?: string;
+} & RESTBenchCheck;
+
+type RESTBenchCheck = {
+  test_number: string;
+  category: string;
+  type: string;
+  profile: string;
+  scored: boolean;
+  automated: boolean;
+  description: string;
+  remediation: string;
+  tags?: string[]; // Tags provide list of compliance that related to the cis test item.
+  tags_v2?: Record<string, any /* share.TagDetails */>; // TagsV2 provide compliance details for each compliance tag
+};
+
+type RESTScanSecret = {
+  type: string; // the secret description
+  evidence: string; // found in a cloaked string
+  path: string; // file path
+  suggestion: string; // Todo:
+};
+
+type RESTScanSetIdPerm = {
+  type: string; // the set id descriptions
+  evidence: string; // file attributes
+  path: string; // file path
+};
+
+type RESTScanSignatureInfo = {
+  verifiers?: string[];
+  verification_timestamp: string;
+};
+
+type RESTScanReport = {
+  vulnerabilities: RESTVulnerability[];
+  modules?: RESTScanModule[];
+  checks?: RESTBenchItem[];
+  secrets?: RESTScanSecret[];
+  setid_perms?: RESTScanSetIdPerm[];
+  envs?: string[];
+  labels?: Record<string, string>;
+  cmds?: string[];
+  signature_data?: RESTScanSignatureInfo;
+};
+
+type RESTScanRepoReport = {
+  verdict?: string;
+  image_id: string;
+  registry: string;
+  repository: string;
+  tag: string;
+  digest: string;
+  size: number /* int64 */;
+  author: string;
+  base_os: string;
+  created_at: string;
+  cvedb_version: string;
+  cvedb_create_time: string;
+  layers: RESTScanLayer[];
+} & RESTScanReport;
+
+type RESTScanLayer = {
+  digest: string;
+  cmds: string;
+  vulnerabilities: RESTVulnerability[];
+  size: number /* int64 */;
+};
+
+type NeuvectorScanJson = {
+  report: RESTScanRepoReport;
+  error_message: string;
+};
+
 const CWE_NIST_MAPPING = new CweNistMapping();
 
 function cweTags(description: string): string[] {
@@ -42,6 +157,16 @@ const IMPACT_MAPPING: Map<string, number> = new Map([
 
 export class NeuvectorMapper extends BaseConverter {
   withRaw: boolean;
+  rawData: NeuvectorScanJson;
+  getModule: (moduleName: string) => RESTScanModule | undefined;
+
+  memoizedGetModule(): (moduleName: string) => RESTScanModule | undefined {
+    return (moduleName: string) => {
+      return this.rawData.report.modules?.find(
+        (value: RESTScanModule) => value.name === moduleName
+      );
+    };
+  }
 
   mappings: MappedTransform<
     ExecJSON.Execution & {passthrough: unknown},
@@ -76,7 +201,7 @@ export class NeuvectorMapper extends BaseConverter {
             path: 'report.vulnerabilities',
             key: 'id',
             tags: {
-              cve: {path: 'name'},
+              cve: {path: 'cves'},
               cwe: {
                 path: 'description',
                 transformer: cweTags
@@ -92,6 +217,11 @@ export class NeuvectorMapper extends BaseConverter {
               },
               severity: {
                 path: 'severity'
+              },
+              source: {
+                path: 'package_name',
+                transformer: (packageName: string) =>
+                  this.getModule(packageName)?.source
               }
             }, //Insert data
             descriptions: [], //Insert data
@@ -137,7 +267,11 @@ export class NeuvectorMapper extends BaseConverter {
     }
   };
   constructor(exportJson: string, withRaw = false) {
-    super(JSON.parse(exportJson), true);
+    const rawParams = JSON.parse(exportJson);
+    // TODO or to-answer: does it matter that collapseResults is true or false?
+    super(rawParams, true);
     this.withRaw = withRaw;
+    this.rawData = rawParams;
+    this.getModule = this.memoizedGetModule();
   }
 }
