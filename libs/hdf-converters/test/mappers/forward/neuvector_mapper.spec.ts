814cb15173951929f389b604919a059bdcaf24b2
Joyce Quach <jquach@mitre.org>
2024-09-24 11:18:01 -0400

    Add more tags, change cve tag to cves, make optional fields not show up if they are falsy
    Signed-off-by: Joyce Quach <jquach@mitre.org>

--- libs/hdf-converters/test/mappers/forward/neuvector_mapper.spec.ts
+++ libs/hdf-converters/test/mappers/forward/neuvector_mapper.spec.ts
@@ -1,59 +1,66 @@
-import fs from "fs";
-import { NeuvectorMapper } from "../../../src/neuvector-mapper";
-import { omitVersions } from "../../utils";
+import fs from 'fs';
+import {NeuvectorMapper} from '../../../src/neuvector-mapper';
+import {omitVersions} from '../../utils';
 
-describe("neuvector_mapper", () => {
-  it("Successfully converts neuvector targeted at a local/cloned repository data", () => {
-    const mapper = new NeuvectorMapper(
-      fs.readFileSync(
-        "sample_jsons/neuvector_mapper/sample_input_report/neuvector.json",
-        { encoding: "utf-8" }
-      )
-    );
+const INPUT_FILE_INDICES = [1, 2, 3];
 
-    fs.writeFileSync(
-      'sample_jsons/neuvector_mapper/neuvector-hdf.json',
-      JSON.stringify(mapper.toHdf(), null, 2)
-    );
-
-    expect(omitVersions(mapper.toHdf())).toEqual(
-      omitVersions(
-        JSON.parse(
-          fs.readFileSync("sample_jsons/neuvector_mapper/neuvector-hdf.json", {
-            encoding: "utf-8",
-          })
+describe('neuvector_mapper', () => {
+  it('Successfully converts neuvector targeted at a local/cloned repository data', () => {
+    INPUT_FILE_INDICES.map((index) => {
+      const mapper = new NeuvectorMapper(
+        fs.readFileSync(
+          `sample_jsons/neuvector_mapper/sample_input_report/neuvector-${index}.json`,
+          {encoding: 'utf-8'}
         )
-      )
-    );
+      );
+
+      fs.writeFileSync(
+        `sample_jsons/neuvector_mapper/neuvector-hdf-${index}.json`,
+        JSON.stringify(mapper.toHdf(), null, 2)
+      );
+
+      expect(omitVersions(mapper.toHdf())).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              `sample_jsons/neuvector_mapper/neuvector-hdf-${index}.json`,
+              {
+                encoding: 'utf-8'
+              }
+            )
+          )
+        )
+      );
+    });
   });
 });
 
-describe("neuvector_mapper_withraw", () => {
-  it("Successfully converts withraw flagged neuvector targeted at a local/cloned repository data", () => {
-    const mapper = new NeuvectorMapper(
-      fs.readFileSync(
-        "sample_jsons/neuvector_mapper/sample_input_report/neuvector.json",
-        { encoding: "utf-8" }
-      ),
-      true
-    );
+describe('neuvector_mapper_withraw', () => {
+  it('Successfully converts withraw flagged neuvector targeted at a local/cloned repository data', () => {
+    INPUT_FILE_INDICES.map((index) => {
+      const mapper = new NeuvectorMapper(
+        fs.readFileSync(
+          `sample_jsons/neuvector_mapper/sample_input_report/neuvector-${index}.json`,
+          {encoding: 'utf-8'}
+        ),
+        true
+      );
 
-    fs.writeFileSync(
-      'sample_jsons/neuvector_mapper/neuvector-hdf-withraw.json',
-      JSON.stringify(mapper.toHdf(), null, 2)
-    );
+      fs.writeFileSync(
+        `sample_jsons/neuvector_mapper/neuvector-hdf-withraw-${index}.json`,
+        JSON.stringify(mapper.toHdf(), null, 2)
+      );
 
-    expect(omitVersions(mapper.toHdf())).toEqual(
-      omitVersions(
-        JSON.parse(
-          fs.readFileSync(
-            "sample_jsons/neuvector_mapper/neuvector-hdf-withraw.json",
-            {
-              encoding: "utf-8",
-            }
+      expect(omitVersions(mapper.toHdf())).toEqual(
+        omitVersions(
+          JSON.parse(
+            fs.readFileSync(
+              `sample_jsons/neuvector_mapper/neuvector-hdf-withraw-${index}.json`,
+              {encoding: 'utf-8'}
+            )
           )
         )
-      )
-    );
+      );
+    });
   });
 });
