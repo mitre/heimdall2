e34d2c87942e09c64216b76acbbc9d5de89bf03e
Joyce Quach <jquach@mitre.org>
2024-11-06 16:14:49 -0500

    Revert NIST->CCI trie to Record<string, string[]>
    Signed-off-by: Joyce Quach <jquach@mitre.org>

--- libs/hdf-converters/src/mappings/CciNistMapping.ts
+++ libs/hdf-converters/src/mappings/CciNistMapping.ts
@@ -5,7 +5,6 @@ import {
 } from '../mappings/NistCciMappingData';
 import {is_control, parse_nist} from 'inspecjs';
 import {CCI_TO_NIST} from './CciNistMappingData';
-import {CCIS_KEY} from '../../data/converters/cciListXml2json';
 
 export function CCI2NIST(
   identifiers: string[],
@@ -24,35 +23,18 @@ export function NIST2CCI(
 ): string[] {
   const DEFAULT_CCI_TAGS = defaultNist2Cci || [];
 
-  const paths = identifiers
+  const ccis = identifiers
     .map(parse_nist)
     .filter(is_control)
-    .map((control) => [
-      control.subSpecifiers.slice(0, 2).join('-'),
-      ...control.subSpecifiers.slice(2)
-    ]);
-
-  const ccis = _.uniq(
-    paths.flatMap((specifiers) => {
-      const parentSpecifier = specifiers[0];
-
-      // See if the given path maps to CCIs, otherwise back up a specifier.
-      for (let i = 0; i < specifiers.length; i++) {
-        const path = [
-          parentSpecifier,
-          ...specifiers.slice(1, specifiers.length - i),
-          CCIS_KEY
-        ];
-        const ccis = _.get(NIST_TO_CCI, path);
-        if (ccis) {
-          return ccis;
-        }
-      }
-
-      // If there is no official NIST->CCI mapping for this NIST control, then check the handcrafted mapping.
-      return _.get(HANDCRAFTED_DEFAULT_NIST_TO_CCI, parentSpecifier, []);
-    })
-  );
+    .map((nist) => nist.canonize())
+    .flatMap((nist) => {
+      // Get the official NIST->CCI mapping if it exists. Otherwise, get the handcrafted mapping.
+      return _.get(
+        NIST_TO_CCI,
+        nist,
+        _.get(HANDCRAFTED_DEFAULT_NIST_TO_CCI, nist, [])
+      );
+    });
 
   return ccis.length > 0 ? ccis : DEFAULT_CCI_TAGS;
 }
