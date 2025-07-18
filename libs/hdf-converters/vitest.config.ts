d39407eebf63b1d02add7d1ad3c20fdd4d5f9bec
Amndeep Singh Mann <amann@mitre.org>
2025-07-18 00:17:27 -0400

    doubled timeout
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- libs/hdf-converters/vitest.config.ts
+++ libs/hdf-converters/vitest.config.ts
@@ -2,8 +2,8 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
   test: {
-    hookTimeout: 10000,
-    testTimeout: 10000,
+    hookTimeout: 20000,
+    testTimeout: 20000,
     root: '.'
   }
 });
