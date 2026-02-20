9e9f6cb45abbe452a7b62a6ff92cb835c1dcde16
Amndeep Singh Mann <amann@mitre.org>
2026-02-20 01:46:19 -0500

    try rootdir approach
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -2,26 +2,29 @@ import {defineConfig} from 'vitest/config';
 import vue2 from '@vitejs/plugin-vue2';
 import path from 'path';
 import svgLoader from 'vite-svg-loader';
+import {fileURLToPath} from 'url';
+
+const rootDir = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  root: rootDir,
   plugins: [
     vue2(),
     svgLoader()
   ],
   resolve: {
     aliases: [
-      {find: '@', replacement: path.resolve(__dirname, 'src')},
-      // {find: 'd3', replacement: path.resolve(__dirname, 'tests/util/d3.js')},
+      {find: '@', replacement: path.resolve(rootDir, 'src')},
+      // {find: 'd3', replacement: path.resolve(rootDir, 'tests/util/d3.js')},
       {find: 'axios', replacement: 'axios/dist/node/axios.cjs'},
       {find: 'csv-stringify/sync', replacement: 'csv-stringify/dist/cjs/sync/cjs'}
     ]
   },
   test: {
     environment: 'jsdom',
-    setupFiles: [path.resolve(__dirname, 'tests/setup.ts')],
+    setupFiles: [path.resolve(rootDir, 'tests/setup.ts')],
     include: ['tests/unit/*.spec.ts'],
     exclude: ['src/__mocks__/**', 'node_modules/**', 'dist/**'],
-    root: '.'
     // deps: { inline: ['vuetify'] }
   }
 });
