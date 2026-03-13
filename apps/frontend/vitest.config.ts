38888df22dd9dd31be9bf60022fa7a9767bbc894
Amndeep Singh Mann <amann@mitre.org>
2026-03-13 01:00:57 -0400

    make frontend vitest concurrent
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

--- apps/frontend/vitest.config.ts
+++ apps/frontend/vitest.config.ts
@@ -22,6 +22,9 @@ export default defineConfig({
     setupFiles: [path.resolve(rootDir, 'tests/setup.ts')],
     include: ['tests/unit/*.spec.ts'],
     exclude: ['src/__mocks__/**', 'node_modules/**', 'dist/**'],
-    testTimeout: 15_000
+    testTimeout: 15_000,
+    sequence: {
+      concurrent: true
+    }
   }
 });
