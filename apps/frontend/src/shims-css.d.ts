dc2ef27641e095a7f835659ed31a2fa4cb6971d5
Amndeep Singh Mann <amann@mitre.org>
2026-03-26 03:19:31 -0400

    need to declare modules for all imports including css
    Signed-off-by: Amndeep Singh Mann <amann@mitre.org>

---
+++ apps/frontend/src/shims-css.d.ts
@@ -0,0 +1,4 @@
+declare module 'prismjs/themes/*.css';
+declare module 'vue-file-agent/dist/*.css';
+declare module 'roboto-fontface/css/roboto/roboto-fontface.css';
+declare module '@mdi/font/css/materialdesignicons.css';
