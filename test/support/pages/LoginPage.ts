dd769901d9b93df5b34a3f60531065536a28ecb2
seanlongcc <seanlongcc@gmail.com>
2026-07-06 15:01:24 -0400

    test: added saml tests

--- test/support/pages/LoginPage.ts
+++ test/support/pages/LoginPage.ts
@@ -11,6 +11,19 @@ export default class LoginPage {
     cy.get('[data-cy=ldapLoginButton]').click();
   }
 
+  loginSaml(username: string): void {
+    const origin = String(
+      Cypress.env('MOCKSAML_ORIGIN') || 'http://localhost:4000',
+    );
+
+    cy.get('#saml').click({ force: true });
+    cy.origin(origin, { args: { username } }, ({ username }) => {
+      cy.get('#username').clear();
+      cy.get('#username').type(username);
+      cy.get('button[type=submit]').click();
+    });
+  }
+
   switchToLDAPAuth(): void {
     cy.get('#select-tab-ldap-login').click();
   }
